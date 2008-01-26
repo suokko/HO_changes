699f85d129ece5363f19ed0821528a3ec774b14c
aik_ho <aik_ho@b8de521a-fa13-0410-8113-fc7ffabbd59a>
2008-01-26 16:47:56 +0000

    version is now 1.398
    git-svn-id: https://ho1.svn.sourceforge.net/svnroot/ho1@31 b8de521a-fa13-0410-8113-fc7ffabbd59a

--- java/de/hattrickorganizer/gui/HOMainFrame.java
+++ java/de/hattrickorganizer/gui/HOMainFrame.java
@@ -66,7 +66,7 @@ public final class HOMainFrame
 	//~ Static fields/initializers -----------------------------------------------------------------
 
 	/** TODO Missing Parameter Documentation */
-	public static final double VERSION = 1.397d;
+	public static final double VERSION = 1.398d;
 
 	/** TODO Missing Parameter Documentation */
 	private static final boolean DEVELOPMENT = false;
