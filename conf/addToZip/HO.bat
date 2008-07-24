7e31db8283f7c7afc1ccd7ece22104e4da149553
drake79 <drake79@b8de521a-fa13-0410-8113-fc7ffabbd59a>
2008-07-24 12:33:03 +0000

    Reactivating HOLauncher
    git-svn-id: https://ho1.svn.sourceforge.net/svnroot/ho1@155 b8de521a-fa13-0410-8113-fc7ffabbd59a

--- conf/addToZip/HO.bat
+++ conf/addToZip/HO.bat
@@ -1,6 +1,5 @@
 @echo off
-REM HOLauncher is not used anymore:
-REM javaw -classpath . HOLauncher
+javaw -classpath . HOLauncher
 @start javaw -Xmx512m -jar ho.jar
 @if exist extension.bat call extension.bat
 @exit
