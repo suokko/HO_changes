ca5dcd8f8d96403a65694465463ef9bc279dff8d
drake79 <drake79@b8de521a-fa13-0410-8113-fc7ffabbd59a>
2009-02-28 02:27:20 +0000

    New passing bonus for technical def FW
    git-svn-id: https://ho1.svn.sourceforge.net/svnroot/ho1@363 b8de521a-fa13-0410-8113-fc7ffabbd59a

--- java/de/hattrickorganizer/model/Spieler.java
+++ java/de/hattrickorganizer/model/Spieler.java
@@ -2559,7 +2559,7 @@ public final class Spieler implements plugins.ISpieler {
 
         // Fix for new Defensive Attacker position
 		if (fo.getPosition()==ISpielerPosition.STURM_DEF && getSpezialitaet()==ISpieler.BALLZAUBERER) {
-			psValue *= 1.25f;
+			psValue *= 1.30f;
 		}
 
         float spValue = fo.getStandardsScaled(normalized) * RatingPredictionManager.calcPlayerStrength(this, SKILL_STANDARDS, useForm);
