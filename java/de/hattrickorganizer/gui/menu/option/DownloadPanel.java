e9461f8ed11383c4b6a0a945ec5576200475fe47
dableho <dableho@b8de521a-fa13-0410-8113-fc7ffabbd59a>
2009-03-09 19:12:42 +0000

    Added a new config option for the default state of the "download matches/schedule" checkboxes on the download dialog
    git-svn-id: https://ho1.svn.sourceforge.net/svnroot/ho1@375 b8de521a-fa13-0410-8113-fc7ffabbd59a

---
+++ java/de/hattrickorganizer/gui/menu/option/DownloadPanel.java
@@ -0,0 +1,93 @@
+package de.hattrickorganizer.gui.menu.option;
+
+
+import de.hattrickorganizer.gui.templates.ImagePanel;
+
+import java.awt.GridLayout;
+
+import javax.swing.JCheckBox;
+import javax.swing.JLabel;
+import javax.swing.event.ChangeEvent;
+
+
+/**
+ * Alle weiteren Optionen, die Keine Formeln sind
+ */
+public final class DownloadPanel extends ImagePanel
+    implements javax.swing.event.ChangeListener, java.awt.event.ItemListener
+{
+    //~ Static fields/initializers -----------------------------------------------------------------
+
+	private JCheckBox m_jchXMLDownload;
+	private JCheckBox m_jchCurrentMatchlist;    
+    private JCheckBox m_jchFixtures;
+
+    //~ Constructors -------------------------------------------------------------------------------
+
+    /**
+     * Creates a new DownloadPanel object.
+     */
+    public DownloadPanel() {
+        initComponents();
+    }
+
+    //~ Methods ------------------------------------------------------------------------------------
+
+
+    /**
+     * TODO Missing Method Documentation
+     *
+     * @param itemEvent TODO Missing Method Parameter Documentation
+     */
+    public final void itemStateChanged(java.awt.event.ItemEvent itemEvent) {        
+        gui.UserParameter.instance().xmlDownload = m_jchXMLDownload.isSelected();
+        gui.UserParameter.instance().fixtures = m_jchFixtures.isSelected();
+        gui.UserParameter.instance().currentMatchlist = m_jchCurrentMatchlist.isSelected();
+    }
+
+	public void stateChanged(ChangeEvent arg0) {
+				
+	}
+
+
+    /**
+     * TODO Missing Method Documentation
+     */
+    private void initComponents() {
+        setLayout(new GridLayout(10, 1, 4, 4));
+
+		m_jchXMLDownload = new JCheckBox(de.hattrickorganizer.model.HOVerwaltung.instance().getResource()
+																		   .getProperty("HRFDownload"));
+		m_jchXMLDownload.setToolTipText(de.hattrickorganizer.model.HOVerwaltung.instance().getResource()
+																		  .getProperty("tt_Download_XML"));
+		m_jchXMLDownload.setOpaque(false);
+		m_jchXMLDownload.setSelected(gui.UserParameter.instance().xmlDownload);
+		m_jchXMLDownload.addItemListener(this);
+		add(m_jchXMLDownload);
+
+		m_jchCurrentMatchlist = new JCheckBox(de.hattrickorganizer.model.HOVerwaltung.instance().getResource()
+																		   .getProperty("AktuellerSpielplanDownload"));
+		m_jchCurrentMatchlist.setToolTipText(de.hattrickorganizer.model.HOVerwaltung.instance().getResource()
+																		  .getProperty("tt_Download_AktuellerSpielplan"));
+		m_jchCurrentMatchlist.setOpaque(false);
+		m_jchCurrentMatchlist.setSelected(gui.UserParameter.instance().currentMatchlist);
+		m_jchCurrentMatchlist.addItemListener(this);
+		add(m_jchCurrentMatchlist);
+
+
+        m_jchFixtures = new JCheckBox(de.hattrickorganizer.model.HOVerwaltung.instance().getResource()
+                                                                           .getProperty("FixturesDownload"));
+        m_jchFixtures.setToolTipText(de.hattrickorganizer.model.HOVerwaltung.instance().getResource()
+                                                                          .getProperty("tt_Download_Ligatabelle"));
+        m_jchFixtures.setOpaque(false);
+        m_jchFixtures.setSelected(gui.UserParameter.instance().fixtures);
+        m_jchFixtures.addItemListener(this);
+        add(m_jchFixtures);
+
+
+        for(int i = 0; i < 3; i++) {
+        	add(new JLabel(""));
+        }
+    }
+
+}
