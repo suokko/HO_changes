b6494fa9be6b4fbeba3173395d6f2cf1cfec2b32
drake79 <drake79@b8de521a-fa13-0410-8113-fc7ffabbd59a>
2009-06-03 13:50:20 +0000

    git-svn-id: file:///home/coren/project/backup_ho/branches/eclipse-test@470 b8de521a-fa13-0410-8113-fc7ffabbd59a

---
+++ Plugin-SpecialEvents/src/java/hoplugins/specialEvents/ui/MultiLineToolTipUI.java
@@ -0,0 +1,91 @@
+package hoplugins.specialEvents.ui;
+
+import java.awt.Dimension;
+import java.awt.FontMetrics;
+import java.awt.Graphics;
+import java.awt.Toolkit;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.Enumeration;
+import java.util.Vector;
+
+import javax.swing.JComponent;
+import javax.swing.JToolTip;
+import javax.swing.SwingUtilities;
+import javax.swing.plaf.metal.MetalToolTipUI;
+
+class MultiLineToolTipUI extends MetalToolTipUI
+{
+
+    private String strs[];
+    private int maxWidth;
+
+    MultiLineToolTipUI()
+    {
+        maxWidth = 0;
+    }
+
+    public void paint(Graphics g, JComponent c)
+    {
+        FontMetrics metrics = Toolkit.getDefaultToolkit().getFontMetrics(g.getFont());
+        Dimension size = c.getSize();
+        g.setColor(c.getBackground());
+        g.fillRect(0, 0, size.width, size.height);
+        g.setColor(c.getForeground());
+        if(strs != null)
+        {
+            for(int i = 0; i < strs.length; i++)
+            {
+                g.drawString(strs[i], 3, metrics.getHeight() * (i + 1));
+            }
+
+        }
+    }
+
+    public Dimension getPreferredSize(JComponent c)
+    {
+        FontMetrics metrics = Toolkit.getDefaultToolkit().getFontMetrics(c.getFont());
+        String tipText = ((JToolTip)c).getTipText();
+        if(tipText == null)
+        {
+            tipText = "";
+        }
+        BufferedReader br = new BufferedReader(new StringReader(tipText));
+        int maxWidth = 0;
+        Vector v = new Vector();
+        String line;
+        try
+        {
+            while((line = br.readLine()) != null) 
+            {
+                int width = SwingUtilities.computeStringWidth(metrics, line);
+                maxWidth = maxWidth >= width ? maxWidth : width;
+                v.addElement(line);
+            }
+        }
+        catch(IOException ex)
+        {
+            ex.printStackTrace();
+        }
+        int lines = v.size();
+        if(lines < 1)
+        {
+            strs = null;
+            lines = 1;
+        } else
+        {
+            strs = new String[lines];
+            int i = 0;
+            for(Enumeration e = v.elements(); e.hasMoreElements();)
+            {
+                strs[i] = (String)e.nextElement();
+                i++;
+            }
+
+        }
+        int height = metrics.getHeight() * lines;
+        this.maxWidth = maxWidth;
+        return new Dimension(maxWidth + 6, height + 4);
+    }
+}
