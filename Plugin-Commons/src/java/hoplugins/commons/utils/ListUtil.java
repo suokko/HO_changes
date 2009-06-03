62155c33e73492b9f3da620d4a2860090832c086
drake79 <drake79@b8de521a-fa13-0410-8113-fc7ffabbd59a>
2009-06-03 13:19:44 +0000

    Eclipse SVN Project test, refs #79
    git-svn-id: file:///home/coren/project/backup_ho/branches/eclipse-test@458 b8de521a-fa13-0410-8113-fc7ffabbd59a

---
+++ Plugin-Commons/src/java/hoplugins/commons/utils/ListUtil.java
@@ -0,0 +1,38 @@
+// %1126721046260:hoplugins.commons.utils%
+package hoplugins.commons.utils;
+
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+/**
+ * List Utility class
+ *
+ * @author <a href=mailto:draghetto@users.sourceforge.net>Massimiliano Amato</a>
+ */
+public final class ListUtil {
+    /**
+     * Private default constuctor to prevent class instantiation.
+     */
+    private ListUtil() {
+    }
+
+    /**
+     * Sort the collection and returns a SortedSet
+     *
+     * @param beans Collection to be sorted
+     * @param comparator Comparator to be used
+     *
+     * @return a sorted set
+     */
+    public static SortedSet getSortedSet(Collection beans, Comparator comparator) {
+        final SortedSet set = new TreeSet(comparator);
+
+        if ((beans != null) && (beans.size() > 0)) {
+            set.addAll(beans);
+        }
+
+        return set;
+    }
+}
