5f79672e287605a5891a034eaa57586adfd33330
smaug-ho <smaug-ho@b8de521a-fa13-0410-8113-fc7ffabbd59a>
2009-09-17 07:02:22 +0000

    added a swap player feature to the lineup, refs #1.
    git-svn-id: https://ho1.svn.sourceforge.net/svnroot/ho1/trunk@633 b8de521a-fa13-0410-8113-fc7ffabbd59a

---
+++ Core/src/java/de/hattrickorganizer/gui/lineup/SwapPositionsManager.java
@@ -0,0 +1,78 @@
+package de.hattrickorganizer.gui.lineup;
+
+import plugins.ISpieler;
+import de.hattrickorganizer.gui.Updateable;
+import de.hattrickorganizer.model.Aufstellung;
+import de.hattrickorganizer.model.HOVerwaltung;
+
+public class SwapPositionsManager {
+
+	private final Updateable parentPanel;
+	private SwapPositionFeature swapCandidate = null;
+
+	public SwapPositionsManager(Updateable parentPanel) {
+		this.parentPanel = parentPanel;
+	}
+
+	boolean hasSwapCandidate() {
+		return getSwapCandidate() != null;
+	}
+
+	SwapPositionFeature getSwapCandidate() {
+		return swapCandidate;
+	}
+
+	void swapWithCandidate(SwapPositionFeature swapPositionA) {
+		SwapPositionFeature swapPositionB = getSwapCandidate();
+
+		if (positionsAreSwapable(swapPositionA, swapPositionB)) {
+			swapPositionsInLineup(swapPositionA, swapPositionB);
+			unmarkSwapCandidate();
+			updateGui(swapPositionA, swapPositionB);
+		}
+	}
+
+	private void updateGui(SwapPositionFeature swapPositionA,
+			SwapPositionFeature swapPositionB) {
+		swapPositionA.unpressButton();
+		swapPositionB.unpressButton();
+		parentPanel.update();
+	}
+
+	private boolean positionsAreSwapable(SwapPositionFeature swapPositionA,
+			SwapPositionFeature swapPositionB) {
+		return swapPositionA.getPositionsID() != swapPositionB.getPositionsID();
+	}
+
+	private void swapPositionsInLineup(SwapPositionFeature swapPositionA,
+			SwapPositionFeature swapPositionB) {
+		Aufstellung lineup = HOVerwaltung.instance().getModel()
+				.getAufstellung();
+
+		int positionA = swapPositionA.getPositionsID();
+		int positionB = swapPositionB.getPositionsID();
+		ISpieler playerA = lineup.getPlayerByPositionID(positionA);
+		ISpieler playerB = lineup.getPlayerByPositionID(positionB);
+		lineup.setSpielerAtPosition(positionA, playerB.getSpielerID());
+		lineup.setSpielerAtPosition(positionB, playerA.getSpielerID());
+	}
+
+	void markAsSwapCandidate(SwapPositionFeature swapPositionFeature) {
+		swapCandidate = swapPositionFeature;
+	}
+
+	void unmarkSwapCandidate() {
+		swapCandidate = null;
+	}
+
+	/**
+	 * Initializes the swap feature for this player position.
+	 * 
+	 * @param swapPositionsManager
+	 *            the {@link SwapPositionsManager} to register this position to.
+	 */
+	public void addSwapCapabilityTo(SpielerPositionsPanel spielerPositionsPanel) {
+		SwapPositionFeature swapPositionFeature = new SwapPositionFeature(
+				spielerPositionsPanel, this);
+	}
+}
