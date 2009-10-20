398a0f5f9205373db232fa873c43d571e07a7a33
aik_ho <aik_ho@b8de521a-fa13-0410-8113-fc7ffabbd59a>
2009-10-20 22:34:04 +0000

    - fix font in titled borders - put font handling into HOTheme
    git-svn-id: file:///home/coren/project/backup_ho/trunk@642 b8de521a-fa13-0410-8113-fc7ffabbd59a

--- Core/src/java/de/hattrickorganizer/HO.java
+++ Core/src/java/de/hattrickorganizer/HO.java
@@ -1,28 +1,11 @@
-// %3209489045:de.hattrickorganizer%
 package de.hattrickorganizer;
 
-import java.awt.Font;
-import java.util.Enumeration;
-
-import javax.swing.UIManager;
-import javax.swing.plaf.FontUIResource;
-
-
-// %1127326364087:%
-
-/*
- * ho.java
- *
- * Created on 17. März 2003, 15:30
- */
-
 /**
- * DOCUMENT ME!
+ * Main HO starter class.
  *
  * @author thomas.werth
  */
 public class HO {
-    //~ Constructors -------------------------------------------------------------------------------
 
     /**
      * Creates a new instance of ho
@@ -31,38 +14,11 @@ public class HO {
     }
 
     /**
-     * Globally configure the font (size).
-     */
-    public static void setUIFont (javax.swing.plaf.FontUIResource f){
-    	Enumeration<Object> keys = UIManager.getDefaults().keys();
-    	while (keys.hasMoreElements()) {
-    		Object key = keys.nextElement();
-    		Object value = UIManager.get (key);
-    		if (value instanceof javax.swing.plaf.FontUIResource)
-    			UIManager.put (key, f);
-    	}
-    }
-
-    //~ Methods ------------------------------------------------------------------------------------
-
-    /**
-     * DOCUMENT ME!
+     * Main method to start a HOMainFrame.
      *
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-    	try {
-    		if (args !=null && args.length>0) {
-    			try {
-    				int size = Integer.parseInt(args[0]);
-    				FontUIResource fr = new FontUIResource("SansSerif", Font.PLAIN, size);
-    				setUIFont(fr);
-    			} catch (NumberFormatException nx) { /* nothing */ }
-    		}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
         de.hattrickorganizer.gui.HOMainFrame.main(args);
-		//MemoryCleaner mc = new MemoryCleaner();
     }
 }
