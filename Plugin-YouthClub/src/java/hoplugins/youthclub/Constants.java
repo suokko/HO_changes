4cd718da87c72ab2ce30672d17bff2d360e1ba0b
aik_ho <aik_ho@b8de521a-fa13-0410-8113-fc7ffabbd59a>
2010-01-14 21:40:47 +0000

    Add initial version of the main plugin class and it's constants.
    git-svn-id: file:///home/coren/project/backup_ho/trunk@688 b8de521a-fa13-0410-8113-fc7ffabbd59a

---
+++ Plugin-YouthClub/src/java/hoplugins/youthclub/Constants.java
@@ -0,0 +1,11 @@
+package hoplugins.youthclub;
+
+/**
+ * Constants for the YouthClub plugin.
+ */
+public interface Constants {
+
+	final public static int YOUTHCLUB_APP_ID = 1;
+	final public static String YOUTHCLUB_APP_CODE = "0084be511acd16cfa07a63db03acb48b";
+	
+}
