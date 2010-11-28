3017e72b9d142b18d7229515658725df90d36f97
aik_ho <aik_ho@b8de521a-fa13-0410-8113-fc7ffabbd59a>
2010-11-28 19:25:14 +0000

    fix buggy match type icons in the manual match selection
    git-svn-id: file:///home/coren/project/backup_ho/trunk@793 b8de521a-fa13-0410-8113-fc7ffabbd59a

--- Plugin-TeamAnalyzer/src/java/hoplugins/teamAnalyzer/ui/ManualFilterPanel.java
+++ Plugin-TeamAnalyzer/src/java/hoplugins/teamAnalyzer/ui/ManualFilterPanel.java
@@ -33,193 +33,174 @@ import javax.swing.JTable;
 import javax.swing.ScrollPaneConstants;
 import javax.swing.table.DefaultTableModel;
 
-
 /**
- * TODO Missing Class Documentation
- *
- * @author TODO Author Name
+ * 
+ * @author Draghetto
  */
 public class ManualFilterPanel extends JPanel {
-    //~ Static fields/initializers -----------------------------------------------------------------
 
 	private static final long serialVersionUID = -9029680013941604647L;
 
-	/** TODO Missing Parameter Documentation */
-    private static final Vector<String> COLUMN_NAMES = new Vector<String>(Arrays.asList(new String[]{
-	                                                                "",
-	                                                                PluginProperty.getString("RecapPanel.Game"),
-	                                                                PluginProperty.getString("Type"),
-	                                                                PluginProperty.getString("RecapPanel.Result"),
-	                                                                PluginProperty.getString("Week"),
-	                                                                Commons.getModel().getLanguageString("Season"),
-	                                                                "", ""
-	                                                            }));
+	private static final Vector<String> COLUMN_NAMES = new Vector<String>(Arrays.asList(new String[] { "",
+			PluginProperty.getString("RecapPanel.Game"), //
+			PluginProperty.getString("Type"), //
+			PluginProperty.getString("RecapPanel.Result"), //
+			PluginProperty.getString("Week"), // 
+			Commons.getModel().getLanguageString("Season"), "", "" }));
 
-    //~ Instance fields ----------------------------------------------------------------------------
+	List<Match> availableMatches = new ArrayList<Match>();
+	private DefaultTableModel tableModel;
+	private JTable table;
 
-    /** TODO Missing Parameter Documentation */
-    List<Match> availableMatches = new ArrayList<Match>();
-    private DefaultTableModel tableModel;
-    private JTable table;
+	/**
+	 * Creates a new instance of ManualFilterPanel
+	 */
+	public ManualFilterPanel() {
+		jbInit();
+	}
 
-    //~ Constructors -------------------------------------------------------------------------------
+	/**
+	 * Re-init the UI components.
+	 */
+	public void reload() {
+		tableModel = new UiFilterTableModel(new Vector<Object>(), COLUMN_NAMES);
+		table.setModel(tableModel);
+		availableMatches = MatchManager.getAllMatches();
 
-    /**
-     * Creates a new instance of ManualFilterPanel
-     */
-    public ManualFilterPanel() {
-        jbInit();
-    }
+		Vector<Object> rowData;
 
-    //~ Methods ------------------------------------------------------------------------------------
+		for (Iterator<Match> iter = availableMatches.iterator(); iter.hasNext();) {
+			List<Object> matchIds = new ArrayList<Object>();
+			Match element = iter.next();
 
-    /**
-     * TODO Missing Method Documentation
-     */
-    public void reload() {
-        tableModel = new UiFilterTableModel(new Vector<Object>(), COLUMN_NAMES);
-        table.setModel(tableModel);
-        availableMatches = MatchManager.getAllMatches();
+			rowData = new Vector<Object>();
 
-        Vector<Object> rowData;
+			boolean isAvailable = Commons.getModel().getHelper().existsMatchInDB(element.getMatchId());
+			boolean isSelected = SystemManager.getFilter().getMatches().contains("" + element.getMatchId());
 
-        for (Iterator<Match> iter = availableMatches.iterator(); iter.hasNext();) {
-            List<Object> matchIds = new ArrayList<Object>();
-            Match element = iter.next();
+			rowData.add(Boolean.valueOf(isSelected));
 
-            rowData = new Vector<Object>();
+			if (MatchUtil.isHome(element)) {
+				rowData.add(element.getAwayTeam());
+				rowData.add(Commons.getModel().getHelper().getImageIcon4Spieltyp(element.getMatchType()));
+				rowData.add(element.getHomeGoals() + " - " + element.getAwayGoals());
+			} else {
+				rowData.add("*" + element.getHomeTeam());
+				rowData.add(Commons.getModel().getHelper().getImageIcon4Spieltyp(element.getMatchType()));
+				rowData.add(element.getAwayGoals() + " - " + element.getHomeGoals());
+			}
 
-            boolean isAvailable = Commons.getModel().getHelper().existsMatchInDB(element.getMatchId());
-            boolean isSelected = SystemManager.getFilter().getMatches().contains(""
-                                                                                 + element
-                                                                                   .getMatchId());
+			rowData.add(element.getWeek() + "");
+			rowData.add(element.getSeason() + "");
 
-            rowData.add(Boolean.valueOf(isSelected));
+			if ((CHPPManager.isDownloadAllowed(element)) || isAvailable) {
+				rowData.add("true");
+				matchIds.add("");
+			} else {
+				rowData.add("false");
+				matchIds.add(element);
+			}
 
-            if (MatchUtil.isHome(element)) {
-                rowData.add(element.getAwayTeam());
-                rowData.add(Commons.getModel().getHelper().getImageIcon4Spieltyp(element
-                                                                                 .getMatchType()));
-                rowData.add(element.getHomeGoals() + " - " + element.getAwayGoals());
-            } else {
-                rowData.add("*" + element.getHomeTeam());
-                rowData.add(Commons.getModel().getHelper().getImageIcon4Spieltyp(element
-                                                                                 .getMatchType()));
-                rowData.add(element.getAwayGoals() + " - " + element.getHomeGoals());
-            }
+			rowData.add("" + element.getMatchType());
+			tableModel.addRow(rowData);
+		}
 
-            rowData.add(element.getWeek() + "");
-            rowData.add(element.getSeason() + "");
+		addTableListener();
 
-            if ((CHPPManager.isDownloadAllowed(element)) || isAvailable) {
-                rowData.add("true");
-                matchIds.add("");
-            } else {
-                rowData.add("false");
-                matchIds.add(element);
-            }
+		table.getTableHeader().getColumnModel().getColumn(0).setMaxWidth(20);
+		table.getTableHeader().getColumnModel().getColumn(0).setPreferredWidth(20);
+		table.getTableHeader().getColumnModel().getColumn(1).setPreferredWidth(90);
+		table.getTableHeader().getColumnModel().getColumn(2).setPreferredWidth(20);
+		table.getTableHeader().getColumnModel().getColumn(3).setPreferredWidth(40);
+		table.getTableHeader().getColumnModel().getColumn(4).setPreferredWidth(40);
+		table.getTableHeader().getColumnModel().getColumn(5).setPreferredWidth(40);
+		table.getTableHeader().getColumnModel().getColumn(6).setMaxWidth(0);
+		table.getTableHeader().getColumnModel().getColumn(6).setMinWidth(0);
+		table.getTableHeader().getColumnModel().getColumn(6).setPreferredWidth(0);
+		table.getTableHeader().getColumnModel().getColumn(6).setWidth(0);
+		table.getTableHeader().getColumnModel().getColumn(7).setMaxWidth(0);
+		table.getTableHeader().getColumnModel().getColumn(7).setMinWidth(0);
+		table.getTableHeader().getColumnModel().getColumn(7).setPreferredWidth(0);
+		table.getTableHeader().getColumnModel().getColumn(7).setWidth(0);
+	}
 
-            rowData.add("" + element.getMatchType());
-            tableModel.addRow(rowData);
-        }
+	/**
+	 * Set a match filter.
+	 */
+	protected void setFilter() {
+		List<String> list = new ArrayList<String>();
+		int i = 0;
 
-        addTableListener();
+		for (Iterator<Match> iter = availableMatches.iterator(); iter.hasNext();) {
+			Match element = iter.next();
+			boolean isSelected = ((Boolean) tableModel.getValueAt(i, 0)).booleanValue();
+			boolean isAvailable = Boolean.valueOf((String) tableModel.getValueAt(i, 6)).booleanValue();
 
-        table.getTableHeader().getColumnModel().getColumn(0).setMaxWidth(20);
-        table.getTableHeader().getColumnModel().getColumn(0).setPreferredWidth(20);
-        table.getTableHeader().getColumnModel().getColumn(1).setPreferredWidth(90);
-        table.getTableHeader().getColumnModel().getColumn(2).setPreferredWidth(20);
-        table.getTableHeader().getColumnModel().getColumn(3).setPreferredWidth(40);
-        table.getTableHeader().getColumnModel().getColumn(4).setPreferredWidth(40);
-        table.getTableHeader().getColumnModel().getColumn(5).setPreferredWidth(40);
-        table.getTableHeader().getColumnModel().getColumn(6).setMaxWidth(0);
-        table.getTableHeader().getColumnModel().getColumn(6).setMinWidth(0);
-        table.getTableHeader().getColumnModel().getColumn(6).setPreferredWidth(0);
-        table.getTableHeader().getColumnModel().getColumn(6).setWidth(0);
-        table.getTableHeader().getColumnModel().getColumn(7).setMaxWidth(0);
-        table.getTableHeader().getColumnModel().getColumn(7).setMinWidth(0);
-        table.getTableHeader().getColumnModel().getColumn(7).setPreferredWidth(0);
-        table.getTableHeader().getColumnModel().getColumn(7).setWidth(0);
-    }
+			if (isSelected && isAvailable) {
+				list.add("" + element.getMatchId());
+			}
 
-    /**
-     * TODO Missing Method Documentation
-     */
-    protected void setFilter() {
-        List<String> list = new ArrayList<String>();
-        int i = 0;
+			i++;
+		}
 
-        for (Iterator<Match> iter = availableMatches.iterator(); iter.hasNext();) {
-            Match element = iter.next();
-            boolean isSelected = ((Boolean) tableModel.getValueAt(i, 0)).booleanValue();
-            boolean isAvailable = Boolean.valueOf((String) tableModel.getValueAt(i, 6))
-                                         .booleanValue();
+		SystemManager.getFilter().setMatches(list);
+	}
 
-            if (isSelected && isAvailable) {
-                list.add("" + element.getMatchId());
-            }
+	/**
+	 * TODO Missing Method Documentation
+	 */
+	private void addTableListener() {
+		table.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseReleased(MouseEvent e) {
+				if (e.getClickCount() == 2) {
+					boolean downloadDone = false;
+					int row = table.getSelectedRow();
+					String status = (String) tableModel.getValueAt(row, 6);
 
-            i++;
-        }
+					if (!status.equalsIgnoreCase("true")) {
+						int id = ((Match) availableMatches.get(row)).getMatchId();
 
-        SystemManager.getFilter().setMatches(list);
-    }
+						downloadDone = Commons.getModel().getHelper().downloadMatchData(id);
+					}
 
-    /**
-     * TODO Missing Method Documentation
-     */
-    private void addTableListener() {
-        table.addMouseListener(new MouseAdapter() {
-                @Override
-				public void mouseReleased(MouseEvent e) {
-                    if (e.getClickCount() == 2) {
-                        boolean downloadDone = false;
-                        int row = table.getSelectedRow();
-                        String status = (String) tableModel.getValueAt(row, 6);
+					e.consume();
 
-                        if (!status.equalsIgnoreCase("true")) {
-                            int id = ((Match) availableMatches.get(row)).getMatchId();
+					if (downloadDone) {
+						tableModel.setValueAt("true", row, 6);
+					}
 
-                            downloadDone = Commons.getModel().getHelper().downloadMatchData(id);
-                        }
+					updateUI();
+				}
+			}
+		});
+	}
 
-                        e.consume();
+	/**
+	 * TODO Missing Method Documentation
+	 */
+	private void jbInit() {
+		JPanel main = Commons.getModel().getGUI().createImagePanel();
 
-                        if (downloadDone) {
-                            tableModel.setValueAt("true", row, 6);
-                        }
+		main.setLayout(new BorderLayout());
+		setLayout(new BorderLayout());
+		setOpaque(false);
 
-                        updateUI();
-                    }
-                }
-            });
-    }
+		Vector<Object> data = new Vector<Object>();
 
-    /**
-     * TODO Missing Method Documentation
-     */
-    private void jbInit() {
-        JPanel main = Commons.getModel().getGUI().createImagePanel();
+		tableModel = new UiFilterTableModel(data, COLUMN_NAMES);
+		table = new JTable(tableModel);
+		table.setDefaultRenderer(Object.class, new ManualFilterTableRenderer());
 
-        main.setLayout(new BorderLayout());
-        setLayout(new BorderLayout());
-        setOpaque(false);
+		JScrollPane pane = new JScrollPane(table);
 
-        Vector<Object> data = new Vector<Object>();
+		main.add(pane, BorderLayout.CENTER);
 
-        tableModel = new UiFilterTableModel(data, COLUMN_NAMES);
-        table = new JTable(tableModel);
-        table.setDefaultRenderer(Object.class, new ManualFilterTableRenderer());
+		JScrollPane scrollPane = new JScrollPane(main);
 
-        JScrollPane pane = new JScrollPane(table);
+		scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
+		scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
 
-        main.add(pane, BorderLayout.CENTER);
-        
-        JScrollPane scrollPane = new JScrollPane(main);
-
-        scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
-        scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-
-        add(scrollPane);
-    }
+		add(scrollPane);
+	}
 }
--- Plugin-TeamAnalyzer/src/java/hoplugins/teamAnalyzer/ui/RecapTableRenderer.java
+++ Plugin-TeamAnalyzer/src/java/hoplugins/teamAnalyzer/ui/RecapTableRenderer.java
@@ -10,87 +10,86 @@ import java.awt.Font;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableCellRenderer;
 
-
 /**
- * TODO Missing Class Documentation
- *
- * @author TODO Author Name
+ * Renderer for the selected matches in the TA.
+ * 
+ * @author Draghetto
  */
 public class RecapTableRenderer extends DefaultTableCellRenderer {
-    //~ Methods ------------------------------------------------------------------------------------
+	// ~ Methods
+	// ------------------------------------------------------------------------------------
 
-    /**
+	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -1496877275674136140L;
 
 	/*
-     * (non-Javadoc)
-     *
-     * @see javax.swing.table.TableCellRenderer#getTableCellRendererComponent(javax.swing.JTable,
-     *      java.lang.Object, boolean, boolean, int, int)
-     */
-    @Override
-	public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected,
-                                                   boolean hasFocus, int row, int column) {
-        super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * javax.swing.table.TableCellRenderer#getTableCellRendererComponent(javax
+	 * .swing.JTable, java.lang.Object, boolean, boolean, int, int)
+	 */
+	@Override
+	public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+		super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
 
-        // Is this 'average' or 'no matches' row?
-        if ((row == 0) && ((table.getRowCount() > 1) || (table.getValueAt(0, 1) == null))) {
-            Font nFont = new Font(this.getFont().getFontName(), Font.BOLD, this.getFont().getSize());
+		// Is this 'average' or 'no matches' row?
+		if ((row == 0) && ((table.getRowCount() > 1) || (table.getValueAt(0, 1) == null))) {
+			Font nFont = new Font(this.getFont().getFontName(), Font.BOLD, this.getFont().getSize());
 
-            this.setFont(nFont);
-            this.setBackground(UIColors.SelectedRowColor);
-            this.setIcon(null);
-        } else {
-            int matchType = 0;
-            boolean isHomeMatch = true;
+			this.setFont(nFont);
+			this.setBackground(UIColors.SelectedRowColor);
+			this.setIcon(null);
+		} else {
+			int matchType = 0;
+			boolean isHomeMatch = true;
 
-            try {
-                matchType = ((Integer) table.getValueAt(row, 20)).intValue();
-                isHomeMatch = ((Boolean) table.getValueAt(row, 21)).booleanValue();
-                this.setBackground(UIColors.getColor4Matchtyp(matchType));
-            } catch (Exception e) {
-                // Make the exception visible.
-                this.setBackground(Color.RED);
-                this.setText("!!!"); //$NON-NLS-1$
-                this.setToolTipText(e.toString());
+			try {
+				matchType = ((Integer) table.getValueAt(row, 20)).intValue();
+				isHomeMatch = ((Boolean) table.getValueAt(row, 21)).booleanValue();
+				this.setBackground(UIColors.getColor4Matchtyp(matchType));
+			} catch (Exception e) {
+				// Make the exception visible.
+				this.setBackground(Color.RED);
+				this.setText("!!!"); //$NON-NLS-1$
+				this.setToolTipText(e.toString());
 
-                return this;
-            }
+				return this;
+			}
 
-            switch (column) {
-                case 1:
+			switch (column) {
+			case 1:
 
-                    // Set icon for match type.
-                    this.setIcon(Commons.getModel().getHelper().getImageIcon4Spieltyp(matchType));
-                    this.setText(null);
+				// Set icon for match type.
+				this.setIcon(Commons.getModel().getHelper().getImageIcon4Spieltyp(matchType));
+				this.setText(null);
 
-                    StringBuffer tipText = new StringBuffer(Commons.getModel().getHelper()
-                                                                   .getNameForMatchTyp(matchType));
+				StringBuffer tipText = new StringBuffer(Commons.getModel().getHelper().getNameForMatchTyp(matchType));
 
-                    tipText.append(" - "); //$NON-NLS-1$
+				tipText.append(" - "); //$NON-NLS-1$
 
-                    if (isHomeMatch) {
-                        tipText.append(Commons.getModel().getLanguageString("Heim")); //$NON-NLS-1$
-                    } else {
-                        tipText.append(Commons.getModel().getLanguageString("Gast")); //$NON-NLS-1$
-                    }
+				if (isHomeMatch) {
+					tipText.append(Commons.getModel().getLanguageString("Heim")); //$NON-NLS-1$
+				} else {
+					tipText.append(Commons.getModel().getLanguageString("Gast")); //$NON-NLS-1$
+				}
 
-                    this.setToolTipText(tipText.toString());
+				this.setToolTipText(tipText.toString());
 
-                    break;
+				break;
 
-                default:
-                    this.setToolTipText(null);
-                    this.setIcon(null);
-            }
-        }
+			default:
+				this.setToolTipText(null);
+				this.setIcon(null);
+			}
+		}
 
-        if (isSelected) {
-            this.setBackground(UIColors.RecapRowColor);
-        }
+		if (isSelected) {
+			this.setBackground(UIColors.RecapRowColor);
+		}
 
-        return this;
-    }
+		return this;
+	}
 }
