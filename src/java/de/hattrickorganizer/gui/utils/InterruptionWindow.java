bc2eec2473666e5663961f6ca0d6e6424bbecb10
aik_ho <aik_ho@b8de521a-fa13-0410-8113-fc7ffabbd59a>
2010-11-28 15:35:18 +0000

    show icon and title in launcher screen / splash screen
    git-svn-id: file:///home/coren/project/backup_ho/trunk/Core@792 b8de521a-fa13-0410-8113-fc7ffabbd59a

--- src/java/de/hattrickorganizer/gui/utils/InterruptionWindow.java
+++ src/java/de/hattrickorganizer/gui/utils/InterruptionWindow.java
@@ -5,77 +5,80 @@ import java.awt.Color;
 import java.awt.Component;
 import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.MediaTracker;
+import java.awt.RenderingHints;
+import java.awt.Toolkit;
+import java.awt.image.FilteredImageSource;
+import java.awt.image.ImageProducer;
+import java.net.URL;
+
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
 
 import de.hattrickorganizer.gui.HOMainFrame;
 import de.hattrickorganizer.tools.HOLogger;
-
+import de.hattrickorganizer.tools.TransparentFilter;
 
 /**
- * Zentriertes Bild in der Bildschirmmitte
+ * Shows an info frame centered in the middle of the screen.
  *
  * @author Volker Fischer
  * @version 0.2a 28.08.01
  */
-public class InterruptionWindow extends javax.swing.JFrame {
-	
+public class InterruptionWindow extends JFrame {
 	private static final long serialVersionUID = -4948885175460734368L;
-	
-    //~ Instance fields ----------------------------------------------------------------------------
-
 	private Image background;
-    private String m_sInfotext = "";
-    private String m_sVersionText = HOMainFrame.getVersionString();
-
-    //~ Constructors -------------------------------------------------------------------------------
+	private String m_sInfotext = "";
+	private String m_sVersionText = HOMainFrame.getVersionString();
 
     /**
      * Creates a new InterruptionWindow object.
-     *
-     * @param component TODO Missing Constructuor Parameter Documentation
      */
-    public InterruptionWindow(Component component) {
-        try {
-            final java.net.URL resource = component.getClass().getClassLoader().getResource("gui/bilder/intro.jpg");
-            background = javax.imageio.ImageIO.read(resource);
+	public InterruptionWindow(Component component) {
+		try {
+			try {
+				setTitle("HO! " + m_sVersionText);
+				setIconImage(ImageIO.read(InterruptionWindow.class.getResourceAsStream("/gui/bilder/Logo-64px.png")));
+			} catch (Exception e) {
+				HOLogger.instance().log(getClass(), "Error setting icon: " + e);
+			}
 
-            final MediaTracker tracker = new MediaTracker(component);
-            tracker.addImage(background, 1);
+			final URL resource = component.getClass().getClassLoader().getResource("gui/bilder/intro.jpg");
+			background = ImageIO.read(resource);
 
-            try {
-                tracker.waitForAll();
-            } catch (InterruptedException ie) {
-            }
+			final MediaTracker tracker = new MediaTracker(component);
+			tracker.addImage(background, 1);
 
-            background = makeColorTransparent(background, new Color(255, 0, 0));
-            tracker.addImage(background, 1);
+			try {
+				tracker.waitForAll();
+			} catch (InterruptedException ie) {
+			}
 
-            try {
-                tracker.waitForAll();
-            } catch (InterruptedException ie) {
-            }
+			background = makeColorTransparent(background, new Color(255, 0, 0));
+			tracker.addImage(background, 1);
 
-            //background.getWidth ( null ), background.getHeight ( null ) );
-            setSize(background.getWidth(null), background.getHeight(null));
-            setLocation((getToolkit().getScreenSize().width / 2) - (getSize().width / 2),
-                        (getToolkit().getScreenSize().height / 2) - (getSize().height / 2));
+			try {
+				tracker.waitForAll();
+			} catch (InterruptedException ie) {
+			}
 
-            //repaint();
-            setUndecorated(true);
-            setVisible(true);
-        } catch (Exception e) {
-            HOLogger.instance().log(getClass(),"InterruptionWindow.<init> : " + e);
-            HOLogger.instance().log(getClass(),e);
-        }
+			// background.getWidth ( null ), background.getHeight ( null ) );
+			setSize(background.getWidth(null), background.getHeight(null));
+			setLocation((getToolkit().getScreenSize().width / 2) - (getSize().width / 2), //
+					(getToolkit().getScreenSize().height / 2) - (getSize().height / 2));
+
+			setUndecorated(true);
+			setVisible(true);
+		} catch (Exception e) {
+			HOLogger.instance().log(getClass(), "InterruptionWindow.<init> : " + e);
+			HOLogger.instance().log(getClass(), e);
+		}
     }
 
-    //~ Methods ------------------------------------------------------------------------------------
-
     /**
-     * TODO Missing Method Documentation
-     *
-     * @param text TODO Missing Method Parameter Documentation
+     * Set text info (e.g. progress).
      */
     public final void setInfoText(String text) {
         m_sInfotext = text;
@@ -83,26 +86,16 @@ public class InterruptionWindow extends javax.swing.JFrame {
     }
 
     /**
-     * TODO Missing Method Documentation
-     *
-     * @param g TODO Missing Method Parameter Documentation
+     * Manually implemented paint() method.
      */
     @Override
 	public final void paint(Graphics g) {
-        final java.awt.Graphics2D g2d = (java.awt.Graphics2D) g;
-
-        //Antialiasing einschalten
-        g2d.setRenderingHint(java.awt.RenderingHints.KEY_ANTIALIASING,
-                             java.awt.RenderingHints.VALUE_ANTIALIAS_ON);
-
-        //Hintergrundgrafik zeichnen
+        final Graphics2D g2d = (Graphics2D) g;
+        //enable antialiasing
+		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        //draw background image
         g2d.drawImage(background, 0, 0, null);
-
-        //Schriftzug
-        //                g2d.setColor ( Color.black );
-        //                g2d.setFont ( new javax.swing.plaf.FontUIResource("SansSerif",Font.BOLD,25) );
-        //                g2d.drawString ( "Hattrick Organizer", 15, 125 );
-        //Rahmen
+		// border
         g2d.drawRect(0, 0, getSize().width - 1, getSize().height - 1);
         g2d.setColor(Color.darkGray);
         g2d.drawRect(1, 1, getSize().width - 3, getSize().height - 3);
@@ -110,8 +103,7 @@ public class InterruptionWindow extends javax.swing.JFrame {
         g2d.drawRect(2, 2, getSize().width - 5, getSize().height - 5);
         g2d.setColor(Color.lightGray);
         g2d.drawRect(3, 3, getSize().width - 7, getSize().height - 7);
-
-        //Infotext
+        //infotext / progress
         g2d.setColor(Color.black);
         g2d.setFont(new javax.swing.plaf.FontUIResource("SansSerif", Font.PLAIN, 10));
         g2d.drawString(m_sInfotext, 145, 25);
@@ -119,33 +111,10 @@ public class InterruptionWindow extends javax.swing.JFrame {
     }
 
     /**
-     * Tauscht eine Farbe im Image durch eine andere
-     *
-     * @param im TODO Missing Constructuor Parameter Documentation
-     * @param original TODO Missing Constructuor Parameter Documentation
-     * @param change TODO Missing Constructuor Parameter Documentation
-     *
-     * @return TODO Missing Return Method Documentation
+     * Changes one color in the given image to transparent.
      */
-    /*private java.awt.Image changeColor(java.awt.Image im, java.awt.Color original,
-                                       java.awt.Color change) {
-        final java.awt.image.ImageProducer ip = new java.awt.image.FilteredImageSource(im.getSource(),
-                                                                                       new de.hattrickorganizer.tools.ColorChangeFilter(original,
-                                                                                                                                        change));
-        return java.awt.Toolkit.getDefaultToolkit().createImage(ip);
-    }*/
-
-    /**
-     * Macht eine Farbe in dem Bild transparent
-     *
-     * @param im TODO Missing Constructuor Parameter Documentation
-     * @param color TODO Missing Constructuor Parameter Documentation
-     *
-     * @return TODO Missing Return Method Documentation
-     */
-    private java.awt.Image makeColorTransparent(java.awt.Image im, java.awt.Color color) {
-        final java.awt.image.ImageProducer ip = new java.awt.image.FilteredImageSource(im.getSource(),
-                                                                                       new de.hattrickorganizer.tools.TransparentFilter(color));
-        return java.awt.Toolkit.getDefaultToolkit().createImage(ip);
-    }
+	private Image makeColorTransparent(Image im, Color color) {
+		final ImageProducer ip = new FilteredImageSource(im.getSource(), new TransparentFilter(color));
+		return Toolkit.getDefaultToolkit().createImage(ip);
+	}
 }
