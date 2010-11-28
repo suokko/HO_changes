f8e411e9e527fdacda07b845f60f6329147dd76f
aik_ho <aik_ho@b8de521a-fa13-0410-8113-fc7ffabbd59a>
2010-11-28 19:25:14 +0000

    fix buggy match type icons in the manual match selection
    git-svn-id: https://ho1.svn.sourceforge.net/svnroot/ho1/trunk@793 b8de521a-fa13-0410-8113-fc7ffabbd59a

--- Plugin-TeamAnalyzer/src/java/hoplugins/teamAnalyzer/ui/ManualFilterTableRenderer.java
+++ Plugin-TeamAnalyzer/src/java/hoplugins/teamAnalyzer/ui/ManualFilterTableRenderer.java
@@ -1,59 +1,49 @@
 // %2336472602:hoplugins.teamAnalyzer.ui%
 package hoplugins.teamAnalyzer.ui;
 
+import hoplugins.Commons;
+
 import java.awt.Component;
 
+import javax.swing.ImageIcon;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableCellRenderer;
 
 
 /**
- * TODO Missing Class Documentation
+ * Renderer for the manual match selection.
  *
- * @author TODO Author Name
+ * @author Draghetto
  */
 public class ManualFilterTableRenderer extends DefaultTableCellRenderer {
-    //~ Methods ------------------------------------------------------------------------------------
-
-    /**
-	 * 
-	 */
 	private static final long serialVersionUID = -685008864266149099L;
 
-	/**
-     * TODO Missing Method Documentation
-     *
-     * @param table TODO Missing Method Parameter Documentation
-     * @param value TODO Missing Method Parameter Documentation
-     * @param isSelected TODO Missing Method Parameter Documentation
-     * @param hasFocus TODO Missing Method Parameter Documentation
-     * @param row TODO Missing Method Parameter Documentation
-     * @param column TODO Missing Method Parameter Documentation
-     *
-     * @return TODO Missing Return Method Documentation
-     */
-    @Override
-	public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected,
-                                                   boolean hasFocus, int row, int column) {
-        Component cell = super.getTableCellRendererComponent(table, value, isSelected, hasFocus,
-                                                             row, column);
-        int type = 0;
+	@Override
+	public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+		super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+		int type = 0;
 
-        try {
-            type = Integer.parseInt((String) table.getValueAt(row, 7));
-        } catch (NumberFormatException e) {
-        }
+		try {
+			type = Integer.parseInt((String) table.getValueAt(row, 7));
+		} catch (NumberFormatException e) {
+		}
 
-        cell.setBackground(UIColors.getColor4Matchtyp(type));
+		setBackground(UIColors.getColor4Matchtyp(type));
+		String available = (String) table.getValueAt(row, 6);
 
-        String available = (String) table.getValueAt(row, 6);
+		if (!available.equalsIgnoreCase("true")) {
+			setEnabled(false);
+		} else {
+			setEnabled(true);
+		}
+		
+		if (value instanceof ImageIcon) {
+			this.setIcon(Commons.getModel().getHelper().getImageIcon4Spieltyp(type));
+			this.setText(null);
+		} else {
+			this.setIcon(null);
+		}
 
-        if (!available.equalsIgnoreCase("true")) {
-            cell.setEnabled(false);
-        } else {
-            cell.setEnabled(true);
-        }
-
-        return cell;
-    }
+		return this;
+	}
 }
