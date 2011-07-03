354e40fd37638839d571cc601300ba0358182680
aik_ho <aik_ho@b8de521a-fa13-0410-8113-fc7ffabbd59a>
2011-07-03 19:41:16 +0000

    bugfix: location always switched back to automatic selection when loading a saved lineup fix code style, add javadoc
    git-svn-id: file:///home/coren/project/backup_ho/trunk@921 b8de521a-fa13-0410-8113-fc7ffabbd59a

--- Core/src/java/de/hattrickorganizer/gui/model/AufstellungCBItem.java
+++ Core/src/java/de/hattrickorganizer/gui/model/AufstellungCBItem.java
@@ -1,13 +1,12 @@
 // %1683810490:de.hattrickorganizer.gui.model%
 package de.hattrickorganizer.gui.model;
 
+import de.hattrickorganizer.gui.lineup.AufstellungsVergleichHistoryPanel;
 import de.hattrickorganizer.model.Lineup;
 
 
 /**
- * TODO Missing Class Documentation
- *
- * @author TODO Author Name
+ * Named Lineup item.
  */
 public class AufstellungCBItem {
     //~ Instance fields ----------------------------------------------------------------------------
@@ -19,9 +18,6 @@ public class AufstellungCBItem {
 
     /**
      * Creates a new AufstellungCBItem object.
-     *
-     * @param text TODO Missing Constructuor Parameter Documentation
-     * @param aufstellung TODO Missing Constructuor Parameter Documentation
      */
     public AufstellungCBItem(String text, Lineup aufstellung) {
         m_sText = text;
@@ -31,18 +27,14 @@ public class AufstellungCBItem {
     //~ Methods ------------------------------------------------------------------------------------
 
     /**
-     * TODO Missing Method Documentation
-     *
-     * @return TODO Missing Return Method Documentation
+     * Check, if displayed.
      */
     public final boolean isAngezeigt() {
-        return de.hattrickorganizer.gui.lineup.AufstellungsVergleichHistoryPanel.isAngezeigt(this);
+        return AufstellungsVergleichHistoryPanel.isAngezeigt(this);
     }
 
     /**
-     * TODO Missing Method Documentation
-     *
-     * @param aufstellung TODO Missing Method Parameter Documentation
+     * Set the Lineup.
      */
     public final void setAufstellung(Lineup aufstellung) {
         m_clAufstellung = aufstellung;
@@ -58,39 +50,26 @@ public class AufstellungCBItem {
     }
 
     /**
-     * TODO Missing Method Documentation
-     *
-     * @param text TODO Missing Method Parameter Documentation
+     * Set a name.
      */
     public final void setText(String text) {
         m_sText = text;
     }
 
     /**
-     * TODO Missing Method Documentation
-     *
-     * @return TODO Missing Return Method Documentation
+     * Get the name.
      */
     public final String getText() {
         return m_sText;
     }
 
     /**
-     * TODO Missing Method Documentation
-     *
-     * @return TODO Missing Return Method Documentation
+     * Duplicate a AufstellungCBItem.
      */
     public final AufstellungCBItem duplicate() {
         return new AufstellungCBItem(this.getText(), this.getAufstellung().duplicate());
     }
 
-    /**
-     * TODO Missing Method Documentation
-     *
-     * @param obj TODO Missing Method Parameter Documentation
-     *
-     * @return TODO Missing Return Method Documentation
-     */
     @Override
 	public final boolean equals(Object obj) {
         if (obj instanceof AufstellungCBItem) {
@@ -104,11 +83,6 @@ public class AufstellungCBItem {
         return false;
     }
 
-    /**
-     * TODO Missing Method Documentation
-     *
-     * @return TODO Missing Return Method Documentation
-     */
     @Override
 	public final String toString() {
         return m_sText;
