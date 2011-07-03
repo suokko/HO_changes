6b73b07a0971fc5374aa032caf93ec8045b773a1
aik_ho <aik_ho@b8de521a-fa13-0410-8113-fc7ffabbd59a>
2011-07-03 20:11:33 +0000

    prevent Exceptions in MP3player
    git-svn-id: file:///home/coren/project/backup_ho/trunk@922 b8de521a-fa13-0410-8113-fc7ffabbd59a

--- Core/src/java/de/hattrickorganizer/gui/utils/MP3PlayerWrapper.java
+++ Core/src/java/de/hattrickorganizer/gui/utils/MP3PlayerWrapper.java
@@ -1,55 +1,56 @@
 package de.hattrickorganizer.gui.utils;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 import javazoom.jl.player.Player;
 import plugins.IMP3Player;
 import de.hattrickorganizer.tools.HOLogger;
 
-public class MP3PlayerWrapper extends Thread implements IMP3Player{
+public class MP3PlayerWrapper extends Thread implements IMP3Player {
 
-	//Startet den Player
-	Player m_clPlayer	=	null;
-	String m_sFile		=	"";
-	
+	// Startet den Player
+	private Player m_clPlayer = null;
+	private String m_sFile = "";
+
 	@Override
-	public void run()
-	{
-		if( ! m_sFile.equals( "" ) )
-		{
-			try
-			{
-		    	final java.net.URL resource = getClass().getClassLoader().getResource( m_sFile );
-		    	m_clPlayer = new Player( resource.openStream() );
-		    	m_clPlayer.play();
+	public void run() {
+		if (!m_sFile.equals("")) {
+			InputStream in = null;
+			try {
+				in = MP3PlayerWrapper.class.getResourceAsStream(m_sFile);
+				if (in != null) {
+					m_clPlayer = new Player(in);
+					m_clPlayer.play();
+				} else {
+					HOLogger.instance().log(getClass(), "MP3 resource not found: " + m_sFile);
+				}
+			} catch (Exception e) {
+				HOLogger.instance().log(getClass(), e);
+			} finally {
+				if (in != null) {
+					try {
+						in.close();
+					} catch (IOException e) {
+					}
+				}
 			}
-			catch( Exception e )
-			{
-				HOLogger.instance().log(getClass(),e);
-			}
-		}
-		else
-		{
-			System.err.println( "No soundfile set" );
+		} else {
+			System.err.println("No soundfile set");
 		}
 	}
-	
-	public void setMP3File( String file )
-	{
-		m_sFile	=	file;
+
+	public void setMP3File(String file) {
+		m_sFile = file;
 	}
-	
-	public void close()
-	{
-		try
-		{
-			if( m_clPlayer != null )
-			{
+
+	public void close() {
+		try {
+			if (m_clPlayer != null) {
 				m_clPlayer.close();
 			}
+		} catch (Exception e) {
+			HOLogger.instance().log(getClass(), e);
 		}
-		catch( Exception e )
-		{
-			HOLogger.instance().log(getClass(),e);
-		}
-		
 	}
 }
