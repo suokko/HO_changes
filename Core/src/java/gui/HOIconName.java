39d76269d9cdf4cd1a6ae120ab816c05a53b39c1
tdietz72 <tdietz72@b8de521a-fa13-0410-8113-fc7ffabbd59a>
2011-07-13 05:49:22 +0000

    Constants classes for ThemeManager-methods (getIcon() and getColor()
    git-svn-id: https://ho1.svn.sourceforge.net/svnroot/ho1/trunk@923 b8de521a-fa13-0410-8113-fc7ffabbd59a

---
+++ Core/src/java/gui/HOIconName.java
@@ -0,0 +1,87 @@
+package gui;
+
+public interface HOIconName {
+
+	public static final String[] MATCHTYPES 	= { "","MatchType1","MatchType2","MatchType3","MatchType4","MatchType5","MatchType6","MatchType7","MatchType8","MatchType9","MatchType10","MatchType11","MatchType12"};
+	public static final String[] WEATHER 		= { "weather0","weather1","weather2","weather3"};
+	public static final String[] SPECIAL 		= { "","special1","special2","special3","special4","special5"};
+	public static final String[] TEAMSMILIES 	= { "", "A-Team.png","B-Team.png", "C-Team.png", "D-Team.png", "E-Team.png", "F-Team.png" };
+	public static final String[] MANUELLSMILIES = { "", "1bigsmile.png","2smile.png", "3normal.png", "4sad.png", "5verysad.png","6clown.png", "7trainer.png", "8dollar.png", "9coach.png" };
+	
+	public static final String NO_TEAM 			= "No-Team.png";
+	public static final String TOOTHEDWHEEL 	= "toothedWheel";
+	public static final String HAND 			= "hand";
+	
+	public static final String NO_MATCH 		= "noMatch";
+	public static final String SHOW_MATCH 		= "showMatch";
+	public static final String DOWNLOAD_MATCH 	= "downloadMatch";
+	
+	public static final String WEATHER_RAIN_POS = "weather.rain.pos";
+	public static final String WEATHER_RAIN_NEG = "weather.rain.neg";
+	public static final String WEATHER_SUN_POS 	= "weather.sun.pos";
+	public static final String WEATHER_SUN_NEG 	= "weather.sun.neg";
+	
+	public static final String REDCARD 			= "redCard";
+	public static final String YELLOWCARD 		= "yellowCard";
+	public static final String TWOCARDS 		= "twoCards";
+	public static final String PATCH 			= "patch";
+	public static final String INJURED 			= "injured";
+	public static final String PATCHSMALL 		= "patch.small";
+	public static final String INJUREDSMALL 	= "injured.small";
+	
+	public static final String PRINTER 			= "printer";
+	public static final String CHECKBOXSELECTED = "checkBoxSelected";
+	public static final String CHECKBOXNOTSELECTED = "checkBoxNotSelected";
+	public static final String DISK 			= "disk";
+	public static final String LOCKED 			= "locked";
+	public static final String EMPTY 			= "empty";
+	public static final String INFO 			= "info";
+	public static final String GOTOANALYSETOP 	= "gotoAnalyseTop";
+	public static final String GOTOANALYSEBOTTOM = "gotoAnalyseBottom";
+	public static final String OFFSET 			= "offset";
+	public static final String GOTOSTATISTIK 	= "gotoStatistik";
+	public static final String TRAININGBLOCK 	= "trainingblock";
+	
+	public static final String MAXLINEUP		 = "maxLineup";
+	public static final String RELOAD 			= "reload";
+	public static final String SIMULATEMATCH 	= "simulateMatch";
+	public static final String GETLINEUP 		= "getLineup";
+	public static final String MIDLINEUPFRAME 	= "midLineupFrame";
+	public static final String MINLINEUPFRAME 	= "minLineupFrame";
+	public static final String SWAP 			= "swap";
+	public static final String SWAPPRESSED 		= "swapPressed";
+	public static final String TURN 			= "turn";
+	
+	public static final String CLEARASSIST 		= "clearAssist";
+	public static final String STARTASSIST 		= "startAssist";
+	public static final String CLEARRESERVE 	= "clearReserve";
+	
+	public static final String LOGO16 			= "logo16";
+	public static final String TRICKOT 			= "trickot";
+
+	// Highlights
+	public static final String  GOAL			= "tor";
+	public static final String  GOAL_FREEKICK 	= "tor_freistoss";
+	public static final String  GOAL_MID		= "tor_mitte";
+	public static final String  GOAL_LEFT		= "tor_links";
+	public static final String  GOAL_RIGHT		= "tor_rechts";
+	public static final String  GOAL_PENALTY	= "tor_elfmeter";
+	public static final String  GOAL_FREEKICK2	= "tor_indirect_freekick";
+	public static final String  GOAL_LONGSHOT	= "tor_longshot";
+	public static final String  GOAL_SPECIAL	= "tor_special";
+	public static final String  GOAL_COUNTER	= "tor_counter";
+	public static final String  NOGOAL			= "keintor";
+	public static final String  NOGOAL_FREEKICK	= "keintor_freistoss";
+	public static final String  NOGOAL_MID		= "keintor_mitte";
+	public static final String  NOGOAL_LEFT		= "keintor_links";
+	public static final String  NOGOAL_RIGHT	= "keintor_rechts";
+	public static final String  NOGOAL_PENALTY	= "keintor_elfmeter";
+	public static final String  NOGOAL_FREEKICK2= "keintor_indirect_freekick";
+	public static final String  NOGOAL_LONGSHOT	= "keintor_longshot";
+	public static final String  NOGOAL_SPECIAL	= "keintor_special";
+	public static final String  NOGOAL_COUNTER	= "keintor_counter";
+
+	public static final String STAR = "star";
+	public static final String STAR_GRAY = "star_gray";
+
+}
