4b6323e739d27efbf7bb83754074a9bf7735a70d
seb04 <seb04@b8de521a-fa13-0410-8113-fc7ffabbd59a>
2011-10-15 04:40:05 +0000

    Removed gktrainer
    git-svn-id: https://ho1.svn.sourceforge.net/svnroot/ho1/trunk@977 b8de521a-fa13-0410-8113-fc7ffabbd59a

--- Plugin-TrainingExperience/src/java/hoplugins/trainingExperience/ui/StaffPanel.java
+++ Plugin-TrainingExperience/src/java/hoplugins/trainingExperience/ui/StaffPanel.java
@@ -34,17 +34,12 @@ public class StaffPanel extends JPanel {
 	/** Number of Assistant ComboBox */
     private JComboBox coTrainer;
 
-//    /** Number of Keeper Assistant ComboBox */
-//    private JComboBox keeperTrainer;
 
     /** The combo boxes */
     private JComboBox trainerLevel;
 
     /** the current number of coTrainer */
     private int coTrainerNumber;
-
-//    /** the current number of keeper Trainer */
-//    private int keeperTrainerNumber;
 
     /** the current level of the coach */
     private int trainerLevelNumber;
@@ -70,15 +65,6 @@ public class StaffPanel extends JPanel {
         return coTrainerNumber;
     }
 
-//    /**
-//     * Returns the number of keeper Trainer
-//     *
-//     * @return
-//     */
-//    public int getKeeperTrainerNumber() {
-//        return keeperTrainerNumber;
-//    }
-
     /**
      * Returns the ability of your trainer
      *
@@ -92,7 +78,6 @@ public class StaffPanel extends JPanel {
      * Populate the staff with HO actual value, called after each Ho refresh command!
      */
     public void reload() {
-        //setKeeper(Commons.getModel().getVerein().getTorwartTrainer());
         setCoTrainer(Commons.getModel().getVerein().getCoTrainer());
         setTrainer(Commons.getModel().getTrainer().getTrainer() - 1);
     }
@@ -123,33 +108,6 @@ public class StaffPanel extends JPanel {
         } catch (Exception e) {
         }
     }
-
-//    /**
-//     * TODO Missing Method Documentation
-//     *
-//     * @param value TODO Missing Method Parameter Documentation
-//     */
-//    private void setKeeper(int value) {
-//        if (value > 10) {
-//            value = 10;
-//        }
-//
-//        try {
-//            keeperTrainer.setSelectedIndex(value);
-//        } catch (Exception e) {
-//        }
-//    }
-//
-//    /**
-//     * Populate the Keeper Trainer combobox
-//     */
-//    private void setKeeperAssistantComboBox() {
-//        keeperTrainer = new JComboBox();
-//
-//        for (int i = 0; i < 11; i++) {
-//            keeperTrainer.addItem(new Integer(i));
-//        }
-//    }
 
     /**
      * TODO Missing Method Documentation
@@ -205,13 +163,6 @@ public class StaffPanel extends JPanel {
                     Integer n = (Integer) coTrainer.getSelectedItem();
 
                     coTrainerNumber = n.intValue();
-
-//                    // sets number of keeper trainer i exceded 10
-//                    if ((keeperTrainerNumber + coTrainerNumber) > 10) {
-//                        keeperTrainerNumber = 10 - coTrainerNumber;
-//                        keeperTrainer.setSelectedIndex(keeperTrainerNumber);
-//                    }
-
                     // refresh player detail and prevision with the new staff settings
                     TrainingExperience.refreshPlayerDetail();
                 }
@@ -235,40 +186,10 @@ public class StaffPanel extends JPanel {
                 }
             });
 
-        // initiates the keeper trainer combo box with the actual number of keeper trainer
-//        setKeeperAssistantComboBox();
-
-//        try {
-//            keeperTrainerNumber = model.getVerein().getTorwartTrainer();
-//        } catch (RuntimeException e2) {
-//            keeperTrainerNumber = 0;
-//        }
-
-//        setKeeper(keeperTrainerNumber);
-//        keeperTrainer.addItemListener(new ItemListener() {
-//                public void itemStateChanged(ItemEvent e) {
-//                    // Sets new number of keeper trainer
-//                    Integer n = (Integer) keeperTrainer.getSelectedItem();
-//
-//                    keeperTrainerNumber = n.intValue();
-//
-//                    // sets number of co trainer if exceded 10
-//                    if ((coTrainerNumber + keeperTrainerNumber) > 10) {
-//                        coTrainerNumber = 10 - keeperTrainerNumber;
-//                        coTrainer.setSelectedIndex(coTrainerNumber);
-//                    }
-//
-//                    // refresh player detail and prevision with the new staff settings				
-//                    TrainingExperience.refreshPlayerDetail();
-//                }
-//            });
-
         main.add(new JLabel(model.getLanguageString("Trainerlevel"))); //$NON-NLS-1$
         main.add(trainerLevel);
         main.add(new JLabel(model.getLanguageString("CoTrainer"))); //$NON-NLS-1$
         main.add(coTrainer);
-//        main.add(new JLabel(model.getLanguageString("Torwarttrainer"))); //$NON-NLS-1$
-//        main.add(keeperTrainer);
         setOpaque(false);
         setLayout(new BorderLayout());
         add(new JScrollPane(main), BorderLayout.CENTER);
