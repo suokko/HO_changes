1738bb3812750e08f7d068296aebd854255d0552
seb04 <seb04@b8de521a-fa13-0410-8113-fc7ffabbd59a>
2011-10-18 09:41:49 +0000

    Removed Stamina and General Training Changed multiple, exclusive if statements to else ifs
    git-svn-id: file:///home/coren/project/backup_ho/trunk@1006 b8de521a-fa13-0410-8113-fc7ffabbd59a

--- Plugin-TrainingExperience/src/java/hoplugins/trainingExperience/constants/Trainings.java
+++ Plugin-TrainingExperience/src/java/hoplugins/trainingExperience/constants/Trainings.java
@@ -23,64 +23,44 @@ public class Trainings {
      * @return training code
      */
     public static int getTrainingCode(String selection) {
-        int type = ITeam.TA_ALLGEMEIN;
+        int type = ITeam.TA_STANDARD;
         IHOMiniModel p_IHMM_miniModel = Commons.getModel();
 
         // Compares the training name with all the training names in HO, 
         //when a match is found it returns the right code 
-        if (selection.equals(p_IHMM_miniModel.getLanguageString("training.general"))) { //$NON-NLS-1$
-            type = ITeam.TA_ALLGEMEIN;
-        }
-
-        if (selection.equals(p_IHMM_miniModel.getLanguageString("Kondition"))) { //$NON-NLS-1$
-            type = ITeam.TA_KONDITION;
-        }
-
         if (selection.equals(p_IHMM_miniModel.getLanguageString("training.set_pieces"))) { //$NON-NLS-1$
             type = ITeam.TA_STANDARD;
         }
-
-        if (selection.equals(p_IHMM_miniModel.getLanguageString("training.defending"))) { //$NON-NLS-1$
+        else if (selection.equals(p_IHMM_miniModel.getLanguageString("training.defending"))) { //$NON-NLS-1$
             type = ITeam.TA_VERTEIDIGUNG;
         }
-
-        if (selection.equals(p_IHMM_miniModel.getLanguageString("training.scoring"))) { //$NON-NLS-1$
+        else if (selection.equals(p_IHMM_miniModel.getLanguageString("training.scoring"))) { //$NON-NLS-1$
             type = ITeam.TA_CHANCEN;
         }
-
-        if (selection.equals(p_IHMM_miniModel.getLanguageString("training.crossing"))) { //$NON-NLS-1$
+        else if (selection.equals(p_IHMM_miniModel.getLanguageString("training.crossing"))) { //$NON-NLS-1$
             type = ITeam.TA_FLANKEN;
         }
-
-        if (selection.equals(p_IHMM_miniModel.getLanguageString("training.shooting"))) { //$NON-NLS-1$
+        else if (selection.equals(p_IHMM_miniModel.getLanguageString("training.shooting"))) { //$NON-NLS-1$
             type = ITeam.TA_SCHUSSTRAINING;
         }
-
-        if (selection.equals(p_IHMM_miniModel.getLanguageString("training.short_passes"))) { //$NON-NLS-1$
+        else if (selection.equals(p_IHMM_miniModel.getLanguageString("training.short_passes"))) { //$NON-NLS-1$
             type = ITeam.TA_PASSSPIEL;
         }
-
-        if (selection.equals(p_IHMM_miniModel.getLanguageString("training.playmaking"))) { //$NON-NLS-1$
+        else if (selection.equals(p_IHMM_miniModel.getLanguageString("training.playmaking"))) { //$NON-NLS-1$
             type = ITeam.TA_SPIELAUFBAU;
         }
-
-        if (selection.equals(p_IHMM_miniModel.getLanguageString("training.goalkeeping"))) { //$NON-NLS-1$
+        else if (selection.equals(p_IHMM_miniModel.getLanguageString("training.goalkeeping"))) { //$NON-NLS-1$
             type = ITeam.TA_TORWART;
         }
-
-        if (selection.equals(p_IHMM_miniModel.getLanguageString("training.through_passes"))) { //$NON-NLS-1$
+        else if (selection.equals(p_IHMM_miniModel.getLanguageString("training.through_passes"))) { //$NON-NLS-1$
             type = ITeam.TA_STEILPAESSE;
         }
-
-        if (selection.equals(p_IHMM_miniModel.getLanguageString("training.defensive_positions"))) { //$NON-NLS-1$
+        else if (selection.equals(p_IHMM_miniModel.getLanguageString("training.defensive_positions"))) { //$NON-NLS-1$
             type = ITeam.TA_ABWEHRVERHALTEN;
         }
-
-        // TODO Real name
-        if (selection.equals(p_IHMM_miniModel.getLanguageString("training.wing_attacks"))) { //$NON-NLS-1$
+        else if (selection.equals(p_IHMM_miniModel.getLanguageString("training.wing_attacks"))) { //$NON-NLS-1$
             type = ITeam.TA_EXTERNALATTACK;
         }
-
         return type;
     }
 
@@ -95,60 +75,39 @@ public class Trainings {
         IHOMiniModel p_IHMM_miniModel = Commons.getModel();
         String selectedTrain = ""; //$NON-NLS-1$
 
-        // based on the type returns the right HO property
-        if (type == ITeam.TA_ALLGEMEIN) {
-            selectedTrain = new String(p_IHMM_miniModel.getLanguageString("training.general")); //$NON-NLS-1$
-        }
-
-        if (type == ITeam.TA_KONDITION) {
-            selectedTrain = new String(p_IHMM_miniModel.getLanguageString("Kondition")); //$NON-NLS-1$
-        }
-
         if (type == ITeam.TA_STANDARD) {
             selectedTrain = new String(p_IHMM_miniModel.getLanguageString("training.set_pieces")); //$NON-NLS-1$
         }
-
-        if (type == ITeam.TA_VERTEIDIGUNG) {
+        else if (type == ITeam.TA_VERTEIDIGUNG) {
             selectedTrain = new String(p_IHMM_miniModel.getLanguageString("training.defending")); //$NON-NLS-1$
         }
-
-        if (type == ITeam.TA_CHANCEN) {
+        else if (type == ITeam.TA_CHANCEN) {
             selectedTrain = new String(p_IHMM_miniModel.getLanguageString("training.scoring")); //$NON-NLS-1$
         }
-
-        if (type == ITeam.TA_FLANKEN) {
+        else if (type == ITeam.TA_FLANKEN) {
             selectedTrain = new String(p_IHMM_miniModel.getLanguageString("training.crossing")); //$NON-NLS-1$
         }
-
-        if (type == ITeam.TA_SCHUSSTRAINING) {
+        else if (type == ITeam.TA_SCHUSSTRAINING) {
             selectedTrain = new String(p_IHMM_miniModel.getLanguageString("training.shooting")); //$NON-NLS-1$
         }
-
-        if (type == ITeam.TA_PASSSPIEL) {
+        else if (type == ITeam.TA_PASSSPIEL) {
             selectedTrain = new String(p_IHMM_miniModel.getLanguageString("training.short_passes")); //$NON-NLS-1$
         }
-
-        if (type == ITeam.TA_SPIELAUFBAU) {
+        else if (type == ITeam.TA_SPIELAUFBAU) {
             selectedTrain = new String(p_IHMM_miniModel.getLanguageString("training.playmaking")); //$NON-NLS-1$
         }
-
-        if (type == ITeam.TA_TORWART) {
+        else if (type == ITeam.TA_TORWART) {
             selectedTrain = new String(p_IHMM_miniModel.getLanguageString("training.goalkeeping")); //$NON-NLS-1$
         }
-
-        if (type == ITeam.TA_STEILPAESSE) {
+        else if (type == ITeam.TA_STEILPAESSE) {
             selectedTrain = new String(p_IHMM_miniModel.getLanguageString("training.through_passes")); //$NON-NLS-1$
         }
-
-        if (type == ITeam.TA_ABWEHRVERHALTEN) {
+        else if (type == ITeam.TA_ABWEHRVERHALTEN) {
             selectedTrain = new String(p_IHMM_miniModel.getLanguageString("training.defensive_positions")); //$NON-NLS-1$
         }
-
-        if (type == ITeam.TA_EXTERNALATTACK) {
-            selectedTrain = new String(""
-                                       + p_IHMM_miniModel.getLanguageString("training.wing_attacks")); //$NON-NLS-1$
+        else if (type == ITeam.TA_EXTERNALATTACK) {
+            selectedTrain = new String(p_IHMM_miniModel.getLanguageString("training.wing_attacks")); //$NON-NLS-1$
         }
-
         return selectedTrain;
     }
 }
