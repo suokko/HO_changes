31b5a15e75c19544fa820d836ebf144c0e8a76ad
seb04 <seb04@b8de521a-fa13-0410-8113-fc7ffabbd59a>
2011-10-18 09:36:34 +0000

    Inherit from NVPComboBox and simplified
    git-svn-id: https://ho1.svn.sourceforge.net/svnroot/ho1/trunk@1004 b8de521a-fa13-0410-8113-fc7ffabbd59a

--- Plugin-TrainingExperience/src/java/hoplugins/trainingExperience/ui/component/TrainingComboBox.java
+++ Plugin-TrainingExperience/src/java/hoplugins/trainingExperience/ui/component/TrainingComboBox.java
@@ -1,43 +1,26 @@
 // %1126721451244:hoplugins.trainingExperience.ui.component%
 package hoplugins.trainingExperience.ui.component;
 
-import hoplugins.Commons;
-
-import plugins.IHOMiniModel;
-
-import javax.swing.JComboBox;
-
+import gui.NVPComboBox;
+import hoplugins.trainingExperience.constants.Trainings;
+import plugins.ITeam;
 
 /**
  * ComboBox to edit the TrainingType
  *
  * @author <a href=mailto:draghetto@users.sourceforge.net>Massimiliano Amato</a>
+ * Seb04 - Simplified and removed General and STamina training.
  */
-public class TrainingComboBox extends JComboBox {
-    //~ Constructors -------------------------------------------------------------------------------
-
+public class TrainingComboBox extends NVPComboBox {
 	private static final long serialVersionUID = 303608674207819922L;
-
 	/**
      * Creates a new TrainingComboBox object.
      */
     public TrainingComboBox() {
         super();
-
-        IHOMiniModel p_IHMM_HOMiniModel = Commons.getModel();
-
-        addItem(p_IHMM_HOMiniModel.getLanguageString("training.general")); //$NON-NLS-1$
-        addItem(p_IHMM_HOMiniModel.getLanguageString("Kondition")); //$NON-NLS-1$
-        addItem(p_IHMM_HOMiniModel.getLanguageString("training.set_pieces")); //$NON-NLS-1$
-        addItem(p_IHMM_HOMiniModel.getLanguageString("training.defending")); //$NON-NLS-1$
-        addItem(p_IHMM_HOMiniModel.getLanguageString("training.scoring")); //$NON-NLS-1$
-        addItem(p_IHMM_HOMiniModel.getLanguageString("training.crossing")); //$NON-NLS-1$
-        addItem(p_IHMM_HOMiniModel.getLanguageString("training.shooting")); //$NON-NLS-1$
-        addItem(p_IHMM_HOMiniModel.getLanguageString("training.short_passes")); //$NON-NLS-1$
-        addItem(p_IHMM_HOMiniModel.getLanguageString("training.playmaking")); //$NON-NLS-1$
-        addItem(p_IHMM_HOMiniModel.getLanguageString("training.goalkeeping")); //$NON-NLS-1$
-        addItem(p_IHMM_HOMiniModel.getLanguageString("training.through_passes")); //$NON-NLS-1$
-        addItem(p_IHMM_HOMiniModel.getLanguageString("training.defensive_positions")); //$NON-NLS-1$
-        addItem(p_IHMM_HOMiniModel.getLanguageString("training.wing_attacks")); //$NON-NLS-1$        
+        for (int i = ITeam.TA_STANDARD; i <= ITeam.TA_EXTERNALATTACK;  i++)
+        {
+        	addItem(i, Trainings.getTrainingDescription(i));
+        }
     }
 }
