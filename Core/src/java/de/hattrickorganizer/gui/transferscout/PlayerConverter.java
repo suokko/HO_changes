120a770b21f960b564cbb42ebb73a78714e2a29d
seb04 <seb04@b8de521a-fa13-0410-8113-fc7ffabbd59a>
2011-11-01 10:40:02 +0000

    Fixed parser positions for loyalty.
    git-svn-id: file:///home/coren/project/backup_ho/trunk@1087 b8de521a-fa13-0410-8113-fc7ffabbd59a

--- Core/src/java/de/hattrickorganizer/gui/transferscout/PlayerConverter.java
+++ Core/src/java/de/hattrickorganizer/gui/transferscout/PlayerConverter.java
@@ -693,18 +693,13 @@ public class PlayerConverter {
     	player.setExperience((skillvalues.get(((Integer) (foundskills.get(2)).get(2)).intValue())).intValue());
     	player.setLeadership((skillvalues.get(((Integer) (foundskills.get(3)).get(2)).intValue())).intValue());
 
-    	player.setGoalKeeping((skillvalues.get(((Integer) (foundskills.get(4)).get(2)).intValue())).intValue());
-    	player.setDefense((skillvalues.get(((Integer) (foundskills.get(5)).get(2)).intValue())).intValue());
-    	player.setPlayMaking((skillvalues.get(((Integer) (foundskills.get(6)).get(2)).intValue())).intValue());
-    	player.setWing((skillvalues.get(((Integer) (foundskills.get(7)).get(2)).intValue())).intValue());
-    	player.setPassing((skillvalues.get(((Integer) (foundskills.get(8)).get(2)).intValue())).intValue());
-    	player.setAttack((skillvalues.get(((Integer) (foundskills.get(9)).get(2)).intValue())).intValue());
-    	player.setSetPieces((skillvalues.get(((Integer) (foundskills.get(10)).get(2)).intValue())).intValue());
-    	if (foundskills.size() > 11)
-    	{
-    		player.setLoyalty((skillvalues.get(((Integer) (foundskills.get(11)).get(2)).intValue())).intValue());
-    		player.setHomeGrown((skillvalues.get(((Integer)(foundskills.get(12)).get(2)).intValue())).intValue() == 1);
-    	}
+    	player.setGoalKeeping((skillvalues.get(((Integer) (foundskills.get(5)).get(2)).intValue())).intValue());
+    	player.setDefense((skillvalues.get(((Integer) (foundskills.get(6)).get(2)).intValue())).intValue());
+    	player.setPlayMaking((skillvalues.get(((Integer) (foundskills.get(7)).get(2)).intValue())).intValue());
+    	player.setWing((skillvalues.get(((Integer) (foundskills.get(8)).get(2)).intValue())).intValue());
+    	player.setPassing((skillvalues.get(((Integer) (foundskills.get(9)).get(2)).intValue())).intValue());
+    	player.setAttack((skillvalues.get(((Integer) (foundskills.get(10)).get(2)).intValue())).intValue());
+    	player.setSetPieces((skillvalues.get(((Integer) (foundskills.get(11)).get(2)).intValue())).intValue());
     }
 
     /**
@@ -718,17 +713,12 @@ public class PlayerConverter {
 	    player.setLeadership((skillvalues.get(((Integer) (foundskills.get(3)).get(2)).intValue())).intValue());
 
    	    player.setGoalKeeping(((Integer) skillvalues.get(((Integer) (foundskills.get(5)).get(2)).intValue())).intValue());
-	    player.setDefense(((Integer) skillvalues.get(((Integer) (foundskills.get(9)).get(2)).intValue())).intValue());
-	    player.setPlayMaking(((Integer) skillvalues.get(((Integer) (foundskills.get(6)).get(2)).intValue())).intValue());
-	    player.setWing(((Integer) skillvalues.get(((Integer) (foundskills.get(8)).get(2)).intValue())).intValue());
-	    player.setPassing(((Integer) skillvalues.get(((Integer) (foundskills.get(7)).get(2)).intValue())).intValue());
-	    player.setAttack(((Integer) skillvalues.get(((Integer) (foundskills.get(10)).get(2)).intValue())).intValue());
-	    player.setSetPieces(((Integer) skillvalues.get(((Integer) (foundskills.get(11)).get(2)).intValue())).intValue());
-	    if (foundskills.size() > 12)
-	    {
-		    player.setLoyalty(((Integer) skillvalues.get(((Integer) (foundskills.get(12)).get(2)).intValue())).intValue());
-		    player.setHomeGrown((skillvalues.get(((Integer)(foundskills.get(13)).get(2)).intValue())).intValue() == 1);
-	    }
+	    player.setDefense(((Integer) skillvalues.get(((Integer) (foundskills.get(10)).get(2)).intValue())).intValue());
+	    player.setPlayMaking(((Integer) skillvalues.get(((Integer) (foundskills.get(7)).get(2)).intValue())).intValue());
+	    player.setWing(((Integer) skillvalues.get(((Integer) (foundskills.get(9)).get(2)).intValue())).intValue());
+	    player.setPassing(((Integer) skillvalues.get(((Integer) (foundskills.get(8)).get(2)).intValue())).intValue());
+	    player.setAttack(((Integer) skillvalues.get(((Integer) (foundskills.get(11)).get(2)).intValue())).intValue());
+	    player.setSetPieces(((Integer) skillvalues.get(((Integer) (foundskills.get(12)).get(2)).intValue())).intValue());
     }
 
     public static int getPrice(String bid, String curbid) {
