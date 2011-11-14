2b6d2edb6055a51d69349d4a0deaabc4215fd12c
kruescho <kruescho@b8de521a-fa13-0410-8113-fc7ffabbd59a>
2011-11-14 20:27:48 +0000

    some javadoc
    git-svn-id: file:///home/coren/project/backup_ho/trunk/Core@1144 b8de521a-fa13-0410-8113-fc7ffabbd59a

--- src/java/de/hattrickorganizer/tools/StringUtilities.java
+++ src/java/de/hattrickorganizer/tools/StringUtilities.java
@@ -1,10 +1,28 @@
 package de.hattrickorganizer.tools;
 
+/**
+ * Utility class for various String related operations.
+ * 
+ */
 public class StringUtilities {
 
+	/**
+	 * Utility class - private constructor enforces noninstantiability.
+	 */
 	private StringUtilities() {
 	}
 
+	/**
+	 * Checks if a <code>String</code> contains digits only. This means that for
+	 * every character x in the given string <code>Character.isDigit(x)</code>
+	 * has to be <code>true</code>.
+	 * 
+	 * @param str
+	 *            the string to check.
+	 * @return <code>true</code> if the given string contains digits only,
+	 *         <code>false</code> if the given string is <code>null</code>,
+	 *         empty or contains at least one character which is not a digit.
+	 */
 	public static boolean isNumeric(String str) {
 		if (isEmpty(str)) {
 			return false;
@@ -18,6 +36,16 @@ public class StringUtilities {
 		return true;
 	}
 
+	/**
+	 * Checks if a given <code>String</code> is empty. This method is
+	 * <code>null</code>-safe which means that it will return <code>true</code>
+	 * if the given parameter is <code>null</code>.
+	 * 
+	 * @param str
+	 *            the string to check.
+	 * @return <code>true</code> if the given string is empty or
+	 *         <code>null</code>, <code>false</code> otherwise.
+	 */
 	public static boolean isEmpty(String str) {
 		return (str == null || str.length() == 0);
 	}
