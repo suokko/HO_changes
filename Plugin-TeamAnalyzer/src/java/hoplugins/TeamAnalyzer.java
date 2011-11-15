03a76d37394620dffefe9fefda6bac2a19811743
seb04 <seb04@b8de521a-fa13-0410-8113-fc7ffabbd59a>
2011-11-15 07:37:37 +0000

    Updated version number for stand alone release
    git-svn-id: file:///home/coren/project/backup_ho/trunk@1148 b8de521a-fa13-0410-8113-fc7ffabbd59a

--- Plugin-TeamAnalyzer/src/java/hoplugins/TeamAnalyzer.java
+++ Plugin-TeamAnalyzer/src/java/hoplugins/TeamAnalyzer.java
@@ -48,7 +48,7 @@ public class TeamAnalyzer implements IPlugin, IRefreshable, IOfficialPlugin {
     private final String PLUGIN_NAME = "TeamAnalyzer";
 
     /** Plugin version constant */
-    private final double PLUGIN_VERSION = 2.81;
+    private final double PLUGIN_VERSION = 2.82;
 
     /** Plugin Id */
     private final int PLUGIN_ID = 14;
