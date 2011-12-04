1e53eb9873e84870bedf8070bb835ef56ab8d247
seb04 <seb04@b8de521a-fa13-0410-8113-fc7ffabbd59a>
2011-12-04 09:43:18 +0000

    Fixed a small problem where an old constant was used for team ratings.
    git-svn-id: file:///home/coren/project/backup_ho/trunk/Core@1232 b8de521a-fa13-0410-8113-fc7ffabbd59a

--- src/java/de/hattrickorganizer/gui/statistic/SpieleStatistikPanel.java
+++ src/java/de/hattrickorganizer/gui/statistic/SpieleStatistikPanel.java
@@ -655,7 +655,7 @@ public class SpieleStatistikPanel extends ImagePanel
                 for (int j = 0; j < team.size(); j++) {
                     final MatchLineupPlayer player = (MatchLineupPlayer) team.get(j);
 
-                    if (player.getId() < ISpielerPosition.startReserves) {
+                    if (player.getId() < ISpielerPosition.substKeeper) {
                         float rating = (float) player.getRating();
 
                         if (rating > 0) {
