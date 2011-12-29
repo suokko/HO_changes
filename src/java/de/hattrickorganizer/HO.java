6690496bc907b848adb670fd444b126b20eeebf0
tdietz72 <tdietz72@b8de521a-fa13-0410-8113-fc7ffabbd59a>
2011-12-29 21:55:08 +0000

    Refactoring: Move main-method from HOMainFrame to class HO
    git-svn-id: file:///home/coren/project/backup_ho/trunk/Core@1321 b8de521a-fa13-0410-8113-fc7ffabbd59a

--- src/java/de/hattrickorganizer/HO.java
+++ src/java/de/hattrickorganizer/HO.java
@@ -1,24 +1,339 @@
 package de.hattrickorganizer;
 
+import gui.UserParameter;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.sql.Timestamp;
+import java.util.Vector;
+
+import javax.swing.JComboBox;
+import javax.swing.JOptionPane;
+
+import plugins.IPlugin;
+
+import de.hattrickorganizer.database.DBZugriff;
+import de.hattrickorganizer.gui.HOMainFrame;
+import de.hattrickorganizer.gui.SplashFrame;
+import de.hattrickorganizer.gui.model.UserColumnController;
+import de.hattrickorganizer.gui.templates.ImagePanel;
+import de.hattrickorganizer.gui.theme.ThemeManager;
+import de.hattrickorganizer.logik.GebChecker;
+import de.hattrickorganizer.logik.TrainingsManager;
+import de.hattrickorganizer.model.HOVerwaltung;
+import de.hattrickorganizer.model.User;
+import de.hattrickorganizer.net.MyConnector;
+import de.hattrickorganizer.tools.HOLogger;
+import de.hattrickorganizer.tools.LanguageFiles;
+import de.hattrickorganizer.tools.backup.BackupHelper;
+import de.hattrickorganizer.tools.extension.ExtensionListener;
+import de.hattrickorganizer.tools.extension.FileExtensionManager;
+
 /**
  * Main HO starter class.
  *
  * @author thomas.werth
  */
 public class HO {
-
+	private static Vector<IPlugin> m_vPlugins = new Vector<IPlugin>();
+	/**
+	 * After that date, the user gets a nag screen if he starts his old HO
+	 * version, set to empty string for no warning (DEVELOPMENT versions do not
+	 * show the nag screen)
+	 */
+	private static final String WARN_DATE = "2012-06-30 00:00:00.0";
+	/** Is this a development version? */
+	private static final boolean DEVELOPMENT = true;
+	public static final int SPRACHVERSION = 2; // language version
     /**
      * Creates a new instance of ho
      */
     public HO() {
     }
 
+	public static boolean isDevelopment() {
+		return DEVELOPMENT;
+	}
     /**
      * Main method to start a HOMainFrame.
      *
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        de.hattrickorganizer.gui.HOMainFrame.main(args);
+		final long start = System.currentTimeMillis();
+
+		// Schnauze!
+		// nur wenn Kein Debug
+		if ((args != null) && (args.length > 0)) {
+			String debugLvl = args[0].trim();
+
+			if (debugLvl.equalsIgnoreCase("INFO")) {
+				HOLogger.instance().setLogLevel(HOLogger.INFORMATION);
+			} else if (debugLvl.equalsIgnoreCase("DEBUG")) {
+				HOLogger.instance().setLogLevel(HOLogger.DEBUG);
+			} else if (debugLvl.equalsIgnoreCase("WARNING")) {
+				HOLogger.instance().setLogLevel(HOLogger.WARNING);
+			} else if (debugLvl.equalsIgnoreCase("ERROR")) {
+				HOLogger.instance().setLogLevel(HOLogger.ERROR);
+			}
+		}
+
+		// Set HOE file
+		// This creates a file called ho.dir in $home
+		// Do we really need this? Removed by flattermann 2009-01-18
+		// FileExtensionManager.createDirFile();
+
+		// Usermanagement Login-Dialog
+		try {
+			if (!User.getCurrentUser().isSingleUser()) {
+				JComboBox comboBox = new JComboBox(User.getAllUser().toArray());
+				int choice = JOptionPane.showConfirmDialog(null, comboBox, "Login",
+						JOptionPane.OK_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE);
+
+				if (choice == JOptionPane.OK_OPTION) {
+					User.INDEX = comboBox.getSelectedIndex();
+				} else {
+					System.exit(0);
+				}
+			}
+		} catch (Exception ex) {
+			HOLogger.instance().log(HOMainFrame.class, ex);
+		}
+
+		// //Spoofing test
+		try {
+			final BufferedReader buffy = new BufferedReader(new java.io.FileReader("ident.txt"));
+			final Vector<String> ids = new Vector<String>();
+			String tmp = "";
+
+			while (buffy.ready()) {
+				tmp = buffy.readLine();
+
+				if (!tmp.startsWith("#") && !tmp.trim().equals("")) {
+					ids.add(tmp);
+				}
+			}
+
+			buffy.close();
+
+			if (ids.size() > 0) {
+				// Math.floor(Math.random()*10)
+				MyConnector.m_sIDENTIFIER = ids.get((int) Math.floor(Math.random() * ids.size())).toString();
+			}
+		} catch (Exception e) {
+		}
+
+		// Check if this HO version is (soft) expired
+		if (!DEVELOPMENT && WARN_DATE != null && WARN_DATE.length() > 0) {
+			final Timestamp datum = new Timestamp(System.currentTimeMillis());
+
+			if (datum.after(Timestamp.valueOf(WARN_DATE))) {
+				JOptionPane.showMessageDialog(
+						null,
+						"Your HO version is very old!\nPlease download a new version at "
+								+ MyConnector.getHOSite(), "Update strongly recommended",
+						JOptionPane.WARNING_MESSAGE);
+			}
+		}
+
+		// Startbild
+		final SplashFrame interuptionsWindow = new SplashFrame();
+
+		// Backup
+		if (User.getCurrentUser().isHSQLDB()) {
+			interuptionsWindow.setInfoText(1,"Backup Database");
+			BackupHelper.backup(new File(User.getCurrentUser().getDBPath()));
+		}
+
+		// Standardparameter aus der DB holen
+		interuptionsWindow.setInfoText(2,"Initialize Database");
+		DBZugriff.instance().loadUserParameter();
+
+		// init Theme
+		try {
+			ThemeManager.instance().setCurrentTheme(UserParameter.instance().theme);
+		} catch (Exception e) {
+			HOLogger.instance().log(HOMainFrame.class, "Can´t load Theme:" + UserParameter.instance().theme);
+			JOptionPane.showMessageDialog(null, e.getMessage(),
+					"Can´t load Theme: " + UserParameter.instance().theme, JOptionPane.WARNING_MESSAGE);
+		}
+		// Init!
+		interuptionsWindow.setInfoText(3,"Initialize Data-Administration");
+
+		// Beim ersten Start Sprache erfragen
+		if (DBZugriff.instance().isFirstStart()) {
+			interuptionsWindow.setVisible(false);
+			new de.hattrickorganizer.gui.menu.option.InitOptionsDialog();
+			JOptionPane.showMessageDialog(null,
+					"To load your team data into HO! select File > Download from the main menu.",
+					"Team Data", JOptionPane.INFORMATION_MESSAGE);
+			interuptionsWindow.setVisible(true);
+		}
+
+		// Check -> Sprachdatei in Ordnung?
+		interuptionsWindow.setInfoText(4,"Check Languagefiles");
+		LanguageFiles.checkLanguageFile(UserParameter.instance().sprachDatei);
+
+		// font switch, because the default font doesn't support Georgian and
+		// Chinese characters
+		// TODO
+		
+
+		HOVerwaltung.instance().setResource(UserParameter.instance().sprachDatei);
+		interuptionsWindow.setInfoText(5,"Load latest Data");
+		HOVerwaltung.instance().loadLatestHoModel();
+		interuptionsWindow.setInfoText(6,"Load  XtraDaten");
+
+		// TableColumn
+		UserColumnController.instance().load();
+
+		// Die Währung auf die aus dem HRF setzen
+		float faktorgeld = (float) HOVerwaltung.instance().getModel().getXtraDaten().getCurrencyRate();
+
+		if (faktorgeld > -1) {
+			UserParameter.instance().faktorGeld = faktorgeld;
+		}
+
+		// Training
+		interuptionsWindow.setInfoText(7,"Initialize Training");
+
+		// Training erstellen -> dabei Trainingswochen berechnen auf Grundlage
+		// der manuellen DB Einträge
+		TrainingsManager.instance().calculateTrainings(DBZugriff.instance().getTrainingsVector());
+
+		// INIT + Dann Pluginsstarten , sonst endlos loop da instance() sich
+		// selbst aufruft!
+		interuptionsWindow.setInfoText(8,"Starting Plugins");
+		startPluginModuls(interuptionsWindow);
+
+		HOMainFrame.instance().getAufstellungsPanel().getAufstellungsPositionsPanel()
+				.exportOldLineup("Actual");
+		FileExtensionManager.extractLineup("Actual");
+		// Anzeigen
+		interuptionsWindow.setInfoText(9,"Prepare to show");
+		HOMainFrame.instance().setVisible(true);
+
+		// Startbild weg
+		interuptionsWindow.setVisible(false);
+
+		if (GebChecker.checkTWGeb()) {
+			new de.hattrickorganizer.gui.birthday.GebDialog(HOMainFrame.instance(), "birthdayTom");
+		}
+
+		if (GebChecker.checkVFGeb()) {
+			new de.hattrickorganizer.gui.birthday.GebDialog(HOMainFrame.instance(), "birthdayVolker");
+		}
+
+		new ExtensionListener().run();
+		HOLogger.instance().log(HOMainFrame.class, "Zeit:" + (System.currentTimeMillis() - start));
+
     }
+    
+	// ///////////////////////////////////////////////////////////////////////////////////////////////77
+	// helper
+	// ///////////////////////////////////////////////////////////////////////////////////////////////77
+	private static void startPluginModuls(SplashFrame interuptionWindow) {
+		try {
+			// Den Ordner mit den Plugins holen
+			final java.io.File folder = new java.io.File("hoplugins");
+			HOLogger.instance().log(HOMainFrame.class,
+					folder.getAbsolutePath() + " " + folder.exists() + " " + folder.isDirectory());
+
+			// Filter, nur class-Datein in dem Ordner interessant
+			final de.hattrickorganizer.gui.utils.ExampleFileFilter filter = new de.hattrickorganizer.gui.utils.ExampleFileFilter();
+			filter.addExtension("class");
+			filter.setDescription("Java Class File");
+			filter.setIgnoreDirectories(true);
+
+			// Alle class-Dateien in den Ordner holen
+			final java.io.File[] files = folder.listFiles(filter);
+
+			// Libs -> Alle Dateien durchlaufen
+			for (int i = 0; (files != null) && (i < files.length); i++) {
+				try {
+					// Name der Klasse erstellen und Class-Object erstellen
+					final String name = "hoplugins."
+							+ files[i].getName().substring(0, files[i].getName().lastIndexOf('.'));
+					final Class<?> fileclass = Class.forName(name);
+					// Das Class-Object definiert kein Interface ...
+					if (!fileclass.isInterface()) {
+						// ... und ist von ILib abgeleitet
+						if (plugins.ILib.class.isAssignableFrom(fileclass)) {
+							// Object davon erstellen und starten
+							final plugins.IPlugin modul = (plugins.IPlugin) fileclass.newInstance();
+
+							// Plugin im Vector gespeichert
+							m_vPlugins.add(modul);
+							HOLogger.instance().log(HOMainFrame.class,
+									" Starte " + files[i].getName() + "  (init MiniModel)");
+							interuptionWindow.setInfoText(8,"Start Plugin: " + modul.getName());
+							modul.start(de.hattrickorganizer.model.HOMiniModel.instance());
+
+							HOLogger.instance().log(HOMainFrame.class,
+									"+ " + files[i].getName() + " gestartet als lib");
+						} else {
+							HOLogger.instance().log(HOMainFrame.class,
+									"- " + files[i].getName() + " nicht von ILib abgeleitet");
+						}
+					} else {
+						HOLogger.instance().log(HOMainFrame.class,
+								"- " + files[i].getName() + " ist Interface");
+					}
+				} catch (Throwable e2) {
+					HOLogger.instance().log(HOMainFrame.class,
+							"- " + files[i].getName() + " wird übersprungen: " + e2);
+					// HOLogger.instance().log(HOMainFrame.class, e2);
+				}
+			}
+
+			// Plugins -> Alle Dateien durchlaufen
+			for (int i = 0; (files != null) && (i < files.length); i++) {
+				try {
+					// Name der Klasse erstellen und Class-Object erstellen
+					final String name = "hoplugins."
+							+ files[i].getName().substring(0, files[i].getName().lastIndexOf('.'));
+					final Class<?> fileclass = Class.forName(name);
+					// Das Class-Object definiert kein Interface ...
+					if (!fileclass.isInterface()) {
+						// ... und ist von IPlugin abgeleitet, nicht die Libs
+						// nochmal starten!
+						if (plugins.IPlugin.class.isAssignableFrom(fileclass)
+								&& !plugins.ILib.class.isAssignableFrom(fileclass)) {
+							// Object davon erstellen und starten
+							final plugins.IPlugin modul = (plugins.IPlugin) fileclass.newInstance();
+
+							// Plugin im Vector gespeichert
+							m_vPlugins.add(modul);
+							HOLogger.instance().log(HOMainFrame.class,
+									" Starte " + files[i].getName() + "  (init MiniModel)");
+							interuptionWindow.setInfoText(8,"Start Plugin: " + modul.getName());
+							modul.start(de.hattrickorganizer.model.HOMiniModel.instance());
+
+							HOLogger.instance().log(HOMainFrame.class,
+									"+ " + files[i].getName() + " gestartet");
+						} else {
+							HOLogger.instance().log(HOMainFrame.class,
+									"- " + files[i].getName() + " nicht von IPlugin abgeleitet");
+						}
+					} else {
+						HOLogger.instance().log(HOMainFrame.class,
+								"- " + files[i].getName() + " ist Interface");
+					}
+				} catch (Throwable e2) {
+					HOLogger.instance().log(HOMainFrame.class,
+							"- " + files[i].getName() + " wird übersprungen: " + e2);
+					// HOLogger.instance().log(HOMainFrame.class, e2);
+				}
+			}
+		} catch (Exception e) {
+			HOLogger.instance().log(HOMainFrame.class, e);
+		}
+	}
+	
+	/**
+	 * Returns the Vector with the started Plugins
+	 */
+	public static Vector<IPlugin> getPlugins() {
+		return m_vPlugins;
+	}
 }
