b83011a0a1cd5637fc4690ed9eacb5a842e51901
tdietz72 <tdietz72@b8de521a-fa13-0410-8113-fc7ffabbd59a>
2011-12-30 21:40:34 +0000

    a little cleanup remove jl1.0.jar for playing MP3s
    git-svn-id: file:///home/coren/project/backup_ho/trunk@1322 b8de521a-fa13-0410-8113-fc7ffabbd59a

--- Core/src/java/plugins/IHelper.java
+++ Core/src/java/plugins/IHelper.java
@@ -551,11 +551,6 @@ public interface IHelper {
      */
     public void showMessage(Component parent, String message, String titel, int typ);
 
-    /**
-     * Returns an IMP3Player. Set the mp3-filename and path with setMP3File( String )
-     * and start the playback with run(). You can end the playback with close()
-     */
-    public IMP3Player getMP3Player();
     
     //--------------------------------------------------------------------------
 
