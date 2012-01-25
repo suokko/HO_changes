1e23c034467a272454816adacbf0c3792c2c0880
kruescho <kruescho@b8de521a-fa13-0410-8113-fc7ffabbd59a>
2012-01-25 15:21:21 +0000

    RatingPanel: refactoring
    git-svn-id: file:///home/coren/project/backup_ho/trunk@1382 b8de521a-fa13-0410-8113-fc7ffabbd59a

--- Core/src/java/de/hattrickorganizer/gui/lineup2/RatingPanel.java
+++ Core/src/java/de/hattrickorganizer/gui/lineup2/RatingPanel.java
@@ -1,5 +1,7 @@
 package de.hattrickorganizer.gui.lineup2;
 
+import gui.HOColorName;
+
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Font;
@@ -7,21 +9,20 @@ import java.awt.GridLayout;
 import java.text.NumberFormat;
 
 import javax.swing.BorderFactory;
-import javax.swing.JComponent;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
 
-import de.hattrickorganizer.gui.templates.ColorLabelEntry;
+import de.hattrickorganizer.gui.theme.ThemeManager;
 import de.hattrickorganizer.tools.Helper;
 import de.hattrickorganizer.tools.PlayerHelper;
 
 public class RatingPanel extends JPanel {
 
 	private static final long serialVersionUID = -8083628255407967880L;
-	private JLabel topLeftLabel = new JLabel("", SwingConstants.LEFT);
-	private ColorLabelEntry topLeftMain;
-	private ColorLabelEntry topLeftCompare;
+	private JLabel ratingTextLabel;
+	private JLabel ratingValueLabel;
+	private JLabel ratingCompareLabel;
 	private NumberFormat numberFormat;
 	private double rating;
 
@@ -34,47 +35,66 @@ public class RatingPanel extends JPanel {
 		initComponents();
 	}
 
+	public void setRating(double value) {
+		this.ratingTextLabel.setText(getNameForSkill(value));
+		this.ratingValueLabel.setText(this.numberFormat.format(value));
+		updateRatingCompareLabel(value - this.rating);
+		this.rating = value;
+	}
+
+	public void setToolTip(String toolTip) {
+		this.ratingTextLabel.setToolTipText(toolTip);
+		this.ratingValueLabel.setToolTipText(toolTip);
+		this.ratingCompareLabel.setToolTipText(toolTip);
+	}
+
 	private void initComponents() {
 		setBackground(Color.WHITE);
 		setLayout(new BorderLayout());
 
 		JPanel temppanel = new JPanel(new GridLayout(1, 2));
 		temppanel.setOpaque(true);
-		this.topLeftMain = new ColorLabelEntry("", Color.BLACK, Color.WHITE, SwingConstants.RIGHT);
-		this.topLeftMain.setFontStyle(Font.BOLD);
-		JComponent tempcomponent = this.topLeftMain.getComponent(false);
-		tempcomponent.setOpaque(true);
-		temppanel.add(tempcomponent);
-		this.topLeftCompare = new ColorLabelEntry("", Color.BLACK, Color.WHITE,
-				SwingConstants.CENTER);
-		tempcomponent = this.topLeftCompare.getComponent(false);
-		tempcomponent.setOpaque(true);
-		temppanel.add(tempcomponent);
+		this.ratingValueLabel = new JLabel();
+		this.ratingValueLabel.setBackground(Color.WHITE);
+		this.ratingValueLabel.setFont(this.ratingValueLabel.getFont().deriveFont(Font.BOLD));
+		this.ratingValueLabel.setOpaque(true);
+		this.ratingValueLabel.setHorizontalAlignment(SwingConstants.RIGHT);
+		temppanel.add(this.ratingValueLabel);
+
+		this.ratingCompareLabel = new JLabel();
+		this.ratingCompareLabel.setBackground(Color.WHITE);
+		this.ratingCompareLabel.setOpaque(true);
+		this.ratingCompareLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		temppanel.add(this.ratingCompareLabel);
 
 		JPanel innerpanel = new JPanel(new GridLayout(2, 1));
 		innerpanel.setBackground(Color.white);
 		innerpanel.setBorder(BorderFactory.createLineBorder(Color.white));
-		innerpanel.add(this.topLeftLabel);
+
+		this.ratingTextLabel = new JLabel("", SwingConstants.LEFT);
+		Font font = this.ratingTextLabel.getFont();
+		this.ratingTextLabel.setFont(font.deriveFont(font.getSize2D() - 1f));
+		this.ratingTextLabel.setOpaque(true);
+		innerpanel.add(this.ratingTextLabel);
 		innerpanel.add(temppanel);
 		add(innerpanel, BorderLayout.CENTER);
-
-		this.topLeftLabel.setFont(this.topLeftLabel.getFont().deriveFont(
-				this.topLeftLabel.getFont().getSize2D() - 1f));
-		this.topLeftLabel.setOpaque(true);
-		this.topLeftCompare.setSpezialNumber(0f, false);
 	}
 
-	public void setRating(double value) {
-		this.topLeftLabel.setText(getNameForSkill(value));
-		this.topLeftMain.setText(this.numberFormat.format(value));
-		this.topLeftCompare.setSpezialNumber((float) (value - this.rating), false);
-		this.rating = value;
-	}
-
-	public void setToolTip(String toolTip) {
-		this.topLeftLabel.setToolTipText(toolTip);
-		this.topLeftMain.setToolTipText(toolTip);
-		this.topLeftCompare.setToolTipText(toolTip);
+	private void updateRatingCompareLabel(double diff) {
+		Color color = null;
+		String sign;
+		if (diff < 0) {
+			color = ThemeManager.getColor(HOColorName.TABLEENTRY_DECLINE_FG);
+			sign = "-";
+		} else if (diff > 0) {
+			color = ThemeManager.getColor(HOColorName.TABLEENTRY_IMPROVEMENT_FG);
+			sign = "+";
+		} else {
+			color = ThemeManager.getColor(HOColorName.TABLEENTRY_FG);
+			sign = "";
+		}
+		this.ratingCompareLabel.setForeground(color);
+		this.ratingCompareLabel.setText(sign + this.numberFormat.format(diff));
 	}
 
 	private String getNameForSkill(double skill) {
