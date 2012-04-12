eb7e0940836d8117d2725489b28511dbf6248479
seb04 <seb04@b8de521a-fa13-0410-8113-fc7ffabbd59a>
2012-04-12 10:49:38 +0000

    First version of minute based training
    git-svn-id: https://ho1.svn.sourceforge.net/svnroot/ho1/trunk@1507 b8de521a-fa13-0410-8113-fc7ffabbd59a

---
+++ Core/src/java/ho/core/training/TrainingPlayer.java
@@ -0,0 +1,83 @@
+package ho.core.training;
+
+public class TrainingPlayer {
+	private int pMinutesPlayedAsGK = 0;
+	private int pMinutesPlayedAsWB = 0;
+	private int pMinutesPlayedAsCD = 0;
+	private int pMinutesPlayedAsW = 0;
+	private int pMinutesPlayedAsIM = 0;
+	private int pMinutesPlayedAsFW = 0;
+	private int pMinutesPlayedAsSP = 0;
+	private int pTotalMinutesPlayed = 0;
+
+	public TrainingPlayer()
+	{
+	}
+	public void setMinutesPlayedAsGK(int minutes)
+	{
+		pMinutesPlayedAsGK = minutes;
+		pTotalMinutesPlayed += minutes;
+	}
+	public int getMinutesPlayedAsGK()
+	{
+		return pMinutesPlayedAsGK;
+	}
+	public void setMinutesPlayedAsWB(int minutes)
+	{
+		pMinutesPlayedAsWB = minutes;
+		pTotalMinutesPlayed += minutes;
+	}
+	public int getMinutesPlayedAsWB()
+	{
+		return pMinutesPlayedAsWB;
+	}
+	public void setMinutesPlayedAsCD(int minutes)
+	{
+		pMinutesPlayedAsCD = minutes;
+		pTotalMinutesPlayed += minutes;
+	}
+	public int getMinutesPlayedAsCD()
+	{
+		return pMinutesPlayedAsCD;
+	}
+	public void setMinutesPlayedAsW(int minutes)
+	{
+		pMinutesPlayedAsW = minutes;
+		pTotalMinutesPlayed += minutes;
+	}
+	public int getMinutesPlayedAsW()
+	{
+		return pMinutesPlayedAsW;
+	}
+	public void setMinutesPlayedAsIM(int minutes)
+	{
+		pMinutesPlayedAsIM = minutes;
+		pTotalMinutesPlayed += minutes;
+	}
+	public int getMinutesPlayedAsIM()
+	{
+		return pMinutesPlayedAsIM;
+	}
+	public void setMinutesPlayedAsFW(int minutes)
+	{
+		pMinutesPlayedAsFW = minutes;
+		pTotalMinutesPlayed += minutes;
+	}
+	public int getMinutesPlayedAsFW()
+	{
+		return pMinutesPlayedAsFW;
+	}
+	public void setMinutesPlayedAsSP(int minutes)
+	{
+		pMinutesPlayedAsSP = minutes;
+		pTotalMinutesPlayed += minutes;
+	}
+	public int getMinutesPlayedAsSP()
+	{
+		return pMinutesPlayedAsSP;
+	}
+	public boolean PlayerHasPlayed()
+	{
+		return pTotalMinutesPlayed > 0;
+	}
+}
