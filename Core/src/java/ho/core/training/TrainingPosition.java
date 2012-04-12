ad2c1020cfa736f2f235766bc6d493ac63d38b6d
seb04 <seb04@b8de521a-fa13-0410-8113-fc7ffabbd59a>
2012-04-12 10:49:38 +0000

    First version of minute based training
    git-svn-id: file:///home/coren/project/backup_ho/trunk@1507 b8de521a-fa13-0410-8113-fc7ffabbd59a

---
+++ Core/src/java/ho/core/training/TrainingPosition.java
@@ -0,0 +1,11 @@
+package ho.core.training;
+
+public class TrainingPosition {
+	public static final int keeper = 0;
+    public static final int wingBack = 1;
+    public static final int centralDefender = 2;
+    public static final int winger = 3;
+    public static final int innerMidfielder = 4;
+    public static final int forward = 5;
+    public static final int setPiece = 6;
+}
