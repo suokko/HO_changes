5940221c113eafe4a7bbe7c3116d827792ecea75
kruescho <kruescho@b8de521a-fa13-0410-8113-fc7ffabbd59a>
2012-06-10 15:16:01 +0000

    missing @Override annotations added
    git-svn-id: file:///home/coren/project/backup_ho/trunk/Core@1571 b8de521a-fa13-0410-8113-fc7ffabbd59a

--- src/java/ho/module/lineup/AustellungSpielerTable.java
+++ src/java/ho/module/lineup/AustellungSpielerTable.java
@@ -18,282 +18,302 @@ import ho.module.playerOverview.PlayerTable;
 import javax.swing.JTable;
 import javax.swing.table.TableColumnModel;
 
-
-
 /**
  * TODO Missing Class Documentation
- *
+ * 
  * @author TODO Author Name
  */
 public final class AustellungSpielerTable extends JTable implements java.awt.event.MouseListener,
-                                                                    java.awt.event.KeyListener,
-                                                                    ho.core.gui.Refreshable,
-                                                                    PlayerTable
-{
-	
+		java.awt.event.KeyListener, ho.core.gui.Refreshable, PlayerTable {
+
 	private static final long serialVersionUID = -8295456454328467793L;
-	
-    //~ Instance fields ----------------------------------------------------------------------------
 
-	
-	//TableSorter sorter;
-    private LineupColumnModel m_clTableModel;
-    private TableSorter m_clTableSorter;
+	// ~ Instance fields
+	// ----------------------------------------------------------------------------
 
-    //~ Constructors -------------------------------------------------------------------------------
+	// TableSorter sorter;
+	private LineupColumnModel m_clTableModel;
+	private TableSorter m_clTableSorter;
 
-    //private DragSource                  m_clDragsource  =   null;
-    protected AustellungSpielerTable() {
-        super();
+	// ~ Constructors
+	// -------------------------------------------------------------------------------
 
-        initModel();
-        setDefaultRenderer(Boolean.class,new BooleanTableCellRenderer());
-        setDefaultRenderer(Object.class,new HODefaultTableCellRenderer());
-        setSelectionBackground(HODefaultTableCellRenderer.SELECTION_BG);
-        setBackground(ColorLabelEntry.BG_STANDARD);
-        addMouseListener(this);
-        RefreshManager.instance().registerRefreshable(this);
-    }
+	// private DragSource m_clDragsource = null;
+	protected AustellungSpielerTable() {
+		super();
 
-    //~ Methods ------------------------------------------------------------------------------------
+		initModel();
+		setDefaultRenderer(Boolean.class, new BooleanTableCellRenderer());
+		setDefaultRenderer(Object.class, new HODefaultTableCellRenderer());
+		setSelectionBackground(HODefaultTableCellRenderer.SELECTION_BG);
+		setBackground(ColorLabelEntry.BG_STANDARD);
+		addMouseListener(this);
+		RefreshManager.instance().registerRefreshable(this);
+	}
 
-    /**
-     * TODO Missing Method Documentation
-     *
-     * @param spielerid TODO Missing Method Parameter Documentation
-     */
-    @Override
+	// ~ Methods
+	// ------------------------------------------------------------------------------------
+
+	/**
+	 * TODO Missing Method Documentation
+	 * 
+	 * @param spielerid
+	 *            TODO Missing Method Parameter Documentation
+	 */
+	@Override
 	public void setSpieler(int spielerid) {
-        final int index = m_clTableSorter.getRow4Spieler(spielerid);
+		final int index = m_clTableSorter.getRow4Spieler(spielerid);
 
-        if (index >= 0) {
-            this.setRowSelectionInterval(index, index);
-        }
-    }
-    
-    @Override
+		if (index >= 0) {
+			this.setRowSelectionInterval(index, index);
+		}
+	}
+
+	@Override
 	public Spieler getSpieler(int row) {
-    	return this.m_clTableSorter.getSpieler(row);
-    }
+		return this.m_clTableSorter.getSpieler(row);
+	}
 
-    /**
-     * TODO Missing Method Documentation
-     *
-     * @param keyEvent TODO Missing Method Parameter Documentation
-     */
-    public void keyPressed(java.awt.event.KeyEvent keyEvent) {
-    }
+	/**
+	 * TODO Missing Method Documentation
+	 * 
+	 * @param keyEvent
+	 *            TODO Missing Method Parameter Documentation
+	 */
+	@Override
+	public void keyPressed(java.awt.event.KeyEvent keyEvent) {
+	}
 
-    /**
-     * TODO Missing Method Documentation
-     *
-     * @param keyEvent TODO Missing Method Parameter Documentation
-     */
-    public void keyReleased(java.awt.event.KeyEvent keyEvent) {
-        if (this.getSelectedRow() > -1) {
-            m_clTableModel.setSpielberechtigung();
-        }
-    }
+	/**
+	 * TODO Missing Method Documentation
+	 * 
+	 * @param keyEvent
+	 *            TODO Missing Method Parameter Documentation
+	 */
+	@Override
+	public void keyReleased(java.awt.event.KeyEvent keyEvent) {
+		if (this.getSelectedRow() > -1) {
+			m_clTableModel.setSpielberechtigung();
+		}
+	}
 
-    /**
-     * TODO Missing Method Documentation
-     *
-     * @param keyEvent TODO Missing Method Parameter Documentation
-     */
-    public void keyTyped(java.awt.event.KeyEvent keyEvent) {
-    }
+	/**
+	 * TODO Missing Method Documentation
+	 * 
+	 * @param keyEvent
+	 *            TODO Missing Method Parameter Documentation
+	 */
+	@Override
+	public void keyTyped(java.awt.event.KeyEvent keyEvent) {
+	}
 
-    //---------------Listener--------------------------------------    
-    public void mouseClicked(java.awt.event.MouseEvent mouseEvent) {
-    }
+	// ---------------Listener--------------------------------------
+	@Override
+	public void mouseClicked(java.awt.event.MouseEvent mouseEvent) {
+	}
 
-    /**
-     * TODO Missing Method Documentation
-     *
-     * @param mouseEvent TODO Missing Method Parameter Documentation
-     */
-    public void mouseEntered(java.awt.event.MouseEvent mouseEvent) {
-    }
+	/**
+	 * TODO Missing Method Documentation
+	 * 
+	 * @param mouseEvent
+	 *            TODO Missing Method Parameter Documentation
+	 */
+	@Override
+	public void mouseEntered(java.awt.event.MouseEvent mouseEvent) {
+	}
 
-    /**
-     * TODO Missing Method Documentation
-     *
-     * @param mouseEvent TODO Missing Method Parameter Documentation
-     */
-    public void mouseExited(java.awt.event.MouseEvent mouseEvent) {
-    }
+	/**
+	 * TODO Missing Method Documentation
+	 * 
+	 * @param mouseEvent
+	 *            TODO Missing Method Parameter Documentation
+	 */
+	@Override
+	public void mouseExited(java.awt.event.MouseEvent mouseEvent) {
+	}
 
-    /**
-     * TODO Missing Method Documentation
-     *
-     * @param mouseEvent TODO Missing Method Parameter Documentation
-     */
-    public void mousePressed(java.awt.event.MouseEvent mouseEvent) {
-    }
+	/**
+	 * TODO Missing Method Documentation
+	 * 
+	 * @param mouseEvent
+	 *            TODO Missing Method Parameter Documentation
+	 */
+	@Override
+	public void mousePressed(java.awt.event.MouseEvent mouseEvent) {
+	}
 
-    /**
-     * TODO Missing Method Documentation
-     *
-     * @param mouseEvent TODO Missing Method Parameter Documentation
-     */
-    public void mouseReleased(java.awt.event.MouseEvent mouseEvent) {
-        if (this.getSelectedRow() > -1) {
-            m_clTableModel.setSpielberechtigung();
-        }
-    }
+	/**
+	 * TODO Missing Method Documentation
+	 * 
+	 * @param mouseEvent
+	 *            TODO Missing Method Parameter Documentation
+	 */
+	@Override
+	public void mouseReleased(java.awt.event.MouseEvent mouseEvent) {
+		if (this.getSelectedRow() > -1) {
+			m_clTableModel.setSpielberechtigung();
+		}
+	}
 
-    /**
-     * TODO Missing Method Documentation
-     */
-    public void reInit() {
-        initModel();
+	/**
+	 * TODO Missing Method Documentation
+	 */
+	@Override
+	public void reInit() {
+		initModel();
 
-        repaint();
-    }
+		repaint();
+	}
 
-    /**
-     * TODO Missing Method Documentation
-     */
-    public void reInitModel() {
-        ((LineupColumnModel) (this.getSorter()).getModel()).reInitData();
-    }
+	/**
+	 * TODO Missing Method Documentation
+	 */
+	public void reInitModel() {
+		((LineupColumnModel) (this.getSorter()).getModel()).reInitData();
+	}
 
-    /**
-     * TODO Missing Method Documentation
-     */
-    public void refresh() {
-        reInitModel();
-        repaint(); // TODO fire TableModelEvent instead
-    }
+	/**
+	 * TODO Missing Method Documentation
+	 */
+	@Override
+	public void refresh() {
+		reInitModel();
+		repaint(); // TODO fire TableModelEvent instead
+	}
 
-    /**
-     * Breite der BestPosSpalte zurückgeben
-     *
-     * @return TODO Missing Return Method Documentation
-     */
-    protected int getBestPosWidth() {
-        return getColumnModel().getColumn(getColumnModel().getColumnIndex(Integer.valueOf(3))).getWidth();
-    }
+	/**
+	 * Breite der BestPosSpalte zurückgeben
+	 * 
+	 * @return TODO Missing Return Method Documentation
+	 */
+	protected int getBestPosWidth() {
+		return getColumnModel().getColumn(getColumnModel().getColumnIndex(Integer.valueOf(3)))
+				.getWidth();
+	}
 
-    /**
-     * Gibt die Spalte für die Sortierung zurück
-     *
-     * @return TODO Missing Return Method Documentation
-     */
-    protected int getSortSpalte() {
-    	switch (ho.core.model.UserParameter.instance().standardsortierung) {
-        case ho.core.model.UserParameter.SORT_NAME:
-            return m_clTableModel.getPositionInArray( UserColumnFactory.NAME );
+	/**
+	 * Gibt die Spalte für die Sortierung zurück
+	 * 
+	 * @return TODO Missing Return Method Documentation
+	 */
+	protected int getSortSpalte() {
+		switch (ho.core.model.UserParameter.instance().standardsortierung) {
+		case ho.core.model.UserParameter.SORT_NAME:
+			return m_clTableModel.getPositionInArray(UserColumnFactory.NAME);
 
-        case ho.core.model.UserParameter.SORT_BESTPOS:
-            return m_clTableModel.getPositionInArray( UserColumnFactory.BEST_POSITION );
+		case ho.core.model.UserParameter.SORT_BESTPOS:
+			return m_clTableModel.getPositionInArray(UserColumnFactory.BEST_POSITION);
 
-        case ho.core.model.UserParameter.SORT_AUFGESTELLT:
-            return m_clTableModel.getPositionInArray( UserColumnFactory.LINUP );
+		case ho.core.model.UserParameter.SORT_AUFGESTELLT:
+			return m_clTableModel.getPositionInArray(UserColumnFactory.LINUP);
 
-        case ho.core.model.UserParameter.SORT_GRUPPE:
-            return m_clTableModel.getPositionInArray( UserColumnFactory.GROUP );
+		case ho.core.model.UserParameter.SORT_GRUPPE:
+			return m_clTableModel.getPositionInArray(UserColumnFactory.GROUP);
 
-        case ho.core.model.UserParameter.SORT_BEWERTUNG:
-            return m_clTableModel.getPositionInArray( UserColumnFactory.RATING );
+		case ho.core.model.UserParameter.SORT_BEWERTUNG:
+			return m_clTableModel.getPositionInArray(UserColumnFactory.RATING);
 
-        default:
-            return m_clTableModel.getPositionInArray( UserColumnFactory.BEST_POSITION );
+		default:
+			return m_clTableModel.getPositionInArray(UserColumnFactory.BEST_POSITION);
 
-        }
-    }
+		}
+	}
 
-    /**
-     * TODO Missing Method Documentation
-     *
-     * @return TODO Missing Return Method Documentation
-     */
-    protected TableSorter getSorter() {
-        return m_clTableSorter;
-    }
+	/**
+	 * TODO Missing Method Documentation
+	 * 
+	 * @return TODO Missing Return Method Documentation
+	 */
+	protected TableSorter getSorter() {
+		return m_clTableSorter;
+	}
 
-    /**
-     * DOCUMENT ME!
-     *
-     * @return int[spaltenanzahl][2] mit 0=ModelIndex und 1=ViewIndex
-     */
-    protected int[][] getSpaltenreihenfolge() {
-        final int[][] reihenfolge = new int[m_clTableModel.getColumnCount()][2];
+	/**
+	 * DOCUMENT ME!
+	 * 
+	 * @return int[spaltenanzahl][2] mit 0=ModelIndex und 1=ViewIndex
+	 */
+	protected int[][] getSpaltenreihenfolge() {
+		final int[][] reihenfolge = new int[m_clTableModel.getColumnCount()][2];
 
-        for (int i = 0; i < m_clTableModel.getColumnCount(); i++) {
-            // Modelindex
-            reihenfolge[i][0] = i;
+		for (int i = 0; i < m_clTableModel.getColumnCount(); i++) {
+			// Modelindex
+			reihenfolge[i][0] = i;
 
-            //ViewIndex
-            reihenfolge[i][1] = convertColumnIndexToView(i);
-        }
+			// ViewIndex
+			reihenfolge[i][1] = convertColumnIndexToView(i);
+		}
 
-        return reihenfolge;
-    }
+		return reihenfolge;
+	}
 
-    public final void saveColumnOrder(){
-    	final UserColumn[] columns = m_clTableModel.getDisplayedColumns();
-    	final TableColumnModel tableColumnModel = getColumnModel();
-    	for (int i = 0; i < columns.length; i++) {
-    		columns[i].setIndex(convertColumnIndexToView(i));
-    		columns[i].setPreferredWidth(tableColumnModel.getColumn(convertColumnIndexToView(i)).getWidth());
-    	}
-    	m_clTableModel.setCurrentValueToColumns(columns);
-    	DBManager.instance().saveHOColumnModel(m_clTableModel);
-    }
-    
-    /**
-     * Initialisiert das Model
-     */
-    private void initModel() {
-        setOpaque(false);
+	public final void saveColumnOrder() {
+		final UserColumn[] columns = m_clTableModel.getDisplayedColumns();
+		final TableColumnModel tableColumnModel = getColumnModel();
+		for (int i = 0; i < columns.length; i++) {
+			columns[i].setIndex(convertColumnIndexToView(i));
+			columns[i].setPreferredWidth(tableColumnModel.getColumn(convertColumnIndexToView(i))
+					.getWidth());
+		}
+		m_clTableModel.setCurrentValueToColumns(columns);
+		DBManager.instance().saveHOColumnModel(m_clTableModel);
+	}
 
-        if (m_clTableModel == null) {
-            m_clTableModel = UserColumnController.instance().getLineupModel();//();
-            
-            m_clTableModel.setValues(HOVerwaltung.instance().getModel().getAllSpieler());                                                                                      
-            m_clTableSorter = new TableSorter(m_clTableModel, m_clTableModel.getPositionInArray(UserColumnFactory.ID), getSortSpalte());
+	/**
+	 * Initialisiert das Model
+	 */
+	private void initModel() {
+		setOpaque(false);
 
-            final ho.core.gui.comp.table.ToolTipHeader header = new ho.core.gui.comp.table.ToolTipHeader(getColumnModel());
-            header.setToolTipStrings(m_clTableModel.getTooltips());
-            header.setToolTipText("");
-            setTableHeader(header);
+		if (m_clTableModel == null) {
+			m_clTableModel = UserColumnController.instance().getLineupModel();// ();
 
-            setModel(m_clTableSorter);
+			m_clTableModel.setValues(HOVerwaltung.instance().getModel().getAllSpieler());
+			m_clTableSorter = new TableSorter(m_clTableModel,
+					m_clTableModel.getPositionInArray(UserColumnFactory.ID), getSortSpalte());
 
-            final TableColumnModel columnModel = getColumnModel();
+			final ho.core.gui.comp.table.ToolTipHeader header = new ho.core.gui.comp.table.ToolTipHeader(
+					getColumnModel());
+			header.setToolTipStrings(m_clTableModel.getTooltips());
+			header.setToolTipText("");
+			setTableHeader(header);
 
-            for (int i = 0; i < m_clTableModel.getColumnCount(); i++) {
-                columnModel.getColumn(i).setIdentifier(new Integer(i));
-            }
+			setModel(m_clTableSorter);
 
-            int[][] targetColumn = m_clTableModel.getColumnOrder();//gui.UserParameter.instance().aufstellungsspaltenreihenfolge;
+			final TableColumnModel columnModel = getColumnModel();
 
-            //Reihenfolge -> nach [][1] sortieren
-            targetColumn = Helper.sortintArray(targetColumn, 1);
+			for (int i = 0; i < m_clTableModel.getColumnCount(); i++) {
+				columnModel.getColumn(i).setIdentifier(new Integer(i));
+			}
 
-            if (targetColumn != null) {
-                for (int i = 0; i < targetColumn.length; i++) {
-                    this.moveColumn(getColumnModel().getColumnIndex(Integer.valueOf(targetColumn[i][0])),
-                                    targetColumn[i][1]);
-                }
-            }
+			int[][] targetColumn = m_clTableModel.getColumnOrder();// gui.UserParameter.instance().aufstellungsspaltenreihenfolge;
 
-            m_clTableSorter.addMouseListenerToHeaderInTable(this);
-            m_clTableModel.setColumnsSize(getColumnModel());
-        } else {
-            //Werte neu setzen
-            m_clTableModel.setValues(HOVerwaltung.instance().getModel().getAllSpieler());
-            m_clTableSorter.reallocateIndexes();
-        }
+			// Reihenfolge -> nach [][1] sortieren
+			targetColumn = Helper.sortintArray(targetColumn, 1);
 
-        setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
-       
-       setSelectionMode(0);
-        setRowSelectionAllowed(true);
+			if (targetColumn != null) {
+				for (int i = 0; i < targetColumn.length; i++) {
+					this.moveColumn(
+							getColumnModel().getColumnIndex(Integer.valueOf(targetColumn[i][0])),
+							targetColumn[i][1]);
+				}
+			}
 
-        m_clTableSorter.initsort();
+			m_clTableSorter.addMouseListenerToHeaderInTable(this);
+			m_clTableModel.setColumnsSize(getColumnModel());
+		} else {
+			// Werte neu setzen
+			m_clTableModel.setValues(HOVerwaltung.instance().getModel().getAllSpieler());
+			m_clTableSorter.reallocateIndexes();
+		}
 
-    }
-    
+		setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+
+		setSelectionMode(0);
+		setRowSelectionAllowed(true);
+
+		m_clTableSorter.initsort();
+
+	}
+
 }
