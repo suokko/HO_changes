310a0a1583a080c490632abaf87ff6c4fb6afe8e
kruescho <kruescho@b8de521a-fa13-0410-8113-fc7ffabbd59a>
2012-06-10 14:56:47 +0000

    refactoring: implementation of table selection synchronization simplified
    git-svn-id: https://ho1.svn.sourceforge.net/svnroot/ho1/trunk@1569 b8de521a-fa13-0410-8113-fc7ffabbd59a

--- Core/src/java/ho/module/lineup/LineupPanel.java
+++ Core/src/java/ho/module/lineup/LineupPanel.java
@@ -5,6 +5,7 @@ import ho.core.gui.HOMainFrame;
 import ho.core.gui.theme.HOIconName;
 import ho.core.gui.theme.ThemeManager;
 import ho.core.model.player.Spieler;
+import ho.module.playerOverview.PlayerTable;
 import ho.module.playerOverview.SpielerUebersichtNamenTable;
 
 import java.awt.BorderLayout;
@@ -12,18 +13,17 @@ import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.event.AdjustmentEvent;
 import java.awt.event.AdjustmentListener;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseListener;
 
 import javax.swing.JPanel;
 import javax.swing.JScrollBar;
 import javax.swing.JScrollPane;
 import javax.swing.JSplitPane;
 import javax.swing.JTabbedPane;
+import javax.swing.JTable;
 import javax.swing.JViewport;
 import javax.swing.ScrollPaneConstants;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 
 /**
  * Panel zum Darstellen aller SpielerPositionen
@@ -229,7 +229,8 @@ public class LineupPanel extends ho.core.gui.comp.panel.ImagePanel {
 		final JScrollBar bar = scrollpane.getVerticalScrollBar();
 		final JScrollBar bar2 = scrollpane2.getVerticalScrollBar();
 		// setVisibile(false) does not have an effect, so we set the size to
-		// false we can' disable the scrollbar with VERTICAL_SCROLLBAR_NEVER
+		// false
+		// we can't disable the scrollbar with VERTICAL_SCROLLBAR_NEVER
 		// because this will disable mouse wheel scrolling
 		bar.setPreferredSize(new Dimension(0, 0));
 
@@ -254,100 +255,38 @@ public class LineupPanel extends ho.core.gui.comp.panel.ImagePanel {
 	}
 
 	private void addListeners() {
-		// TODO use an interface implemented by both table classes to eliminate
-		// if/else blocks (and code duplication) here
 
-		MouseListener ml = new MouseAdapter() {
+		ListSelectionListener lsl = new ListSelectionListener() {
+
 			@Override
-			public void mouseClicked(java.awt.event.MouseEvent mouseEvent) {
-				if (mouseEvent.getSource().equals(m_jtAufstellungSpielerTable)) {
-					int row = m_jtAufstellungSpielerTable.getSelectedRow();
-					m_jtAufstellungSpielerTableName.setRowSelectionInterval(row, row);
-
-					Spieler spieler = m_jtAufstellungSpielerTable.getSorter().getSpieler(row);
-					if (spieler != null) {
-						HOMainFrame.instance().setActualSpieler(spieler.getSpielerID());
-					}
-				} else if (mouseEvent.getSource().equals(m_jtAufstellungSpielerTableName)) {
-					int row = m_jtAufstellungSpielerTableName.getSelectedRow();
-					m_jtAufstellungSpielerTable.setRowSelectionInterval(row, row);
-
-					Spieler spieler = m_jtAufstellungSpielerTableName.getSorter().getSpieler(row);
-					if (spieler != null) {
-						HOMainFrame.instance().setActualSpieler(spieler.getSpielerID());
-					}
+			public void valueChanged(ListSelectionEvent e) {
+				if (e.getSource() == m_jtAufstellungSpielerTable.getSelectionModel()) {
+					synchronizeSelection(m_jtAufstellungSpielerTable);
+				} else if (e.getSource() == m_jtAufstellungSpielerTableName.getSelectionModel()) {
+					synchronizeSelection(m_jtAufstellungSpielerTableName);
 				}
 			}
 
-			@Override
-			public void mouseReleased(java.awt.event.MouseEvent mouseEvent) {
-				if (mouseEvent.getSource().equals(m_jtAufstellungSpielerTable)) {
-					int row = m_jtAufstellungSpielerTable.getSelectedRow();
-					m_jtAufstellungSpielerTableName.setRowSelectionInterval(row, row);
+			private void synchronizeSelection(JTable sourceTable) {
+				JTable targetTable;
+				if (sourceTable == m_jtAufstellungSpielerTable) {
+					targetTable = m_jtAufstellungSpielerTableName;
+				} else {
+					targetTable = m_jtAufstellungSpielerTable;
+				}
 
-					Spieler spieler = m_jtAufstellungSpielerTable.getSorter().getSpieler(row);
-					if (spieler != null) {
-						HOMainFrame.instance().setActualSpieler(spieler.getSpielerID());
-					}
-				} else if (mouseEvent.getSource().equals(m_jtAufstellungSpielerTableName)) {
-					int row = m_jtAufstellungSpielerTableName.getSelectedRow();
-					m_jtAufstellungSpielerTable.setRowSelectionInterval(row, row);
-
-					Spieler spieler = m_jtAufstellungSpielerTableName.getSorter().getSpieler(row);
-					if (spieler != null) {
-						HOMainFrame.instance().setActualSpieler(spieler.getSpielerID());
-					}
+				int row = sourceTable.getSelectedRow();
+				if (targetTable.getSelectedRow() != row) {
+					targetTable.setRowSelectionInterval(row, row);
+				}
+				Spieler spieler = ((PlayerTable) sourceTable).getSpieler(row);
+				if (spieler != null) {
+					HOMainFrame.instance().setActualSpieler(spieler.getSpielerID());
 				}
 			}
 		};
-		this.m_jtAufstellungSpielerTable.addMouseListener(ml);
-		this.m_jtAufstellungSpielerTableName.addMouseListener(ml);
 
-		KeyListener kl = new KeyAdapter() {
-			@Override
-			public void keyReleased(java.awt.event.KeyEvent keyEvent) {
-				if (keyEvent.getSource().equals(m_jtAufstellungSpielerTable)) {
-					int row = m_jtAufstellungSpielerTable.getSelectedRow();
-					m_jtAufstellungSpielerTableName.setRowSelectionInterval(row, row);
-
-					Spieler spieler = m_jtAufstellungSpielerTable.getSorter().getSpieler(row);
-					if (spieler != null) {
-						HOMainFrame.instance().setActualSpieler(spieler.getSpielerID());
-					}
-				} else if (keyEvent.getSource().equals(m_jtAufstellungSpielerTableName)) {
-					int row = m_jtAufstellungSpielerTableName.getSelectedRow();
-					m_jtAufstellungSpielerTable.setRowSelectionInterval(row, row);
-
-					Spieler spieler = m_jtAufstellungSpielerTableName.getSorter().getSpieler(row);
-					if (spieler != null) {
-						HOMainFrame.instance().setActualSpieler(spieler.getSpielerID());
-					}
-				}
-			}
-
-			// ---Listener zum Markieren der beiden Tabellen
-			@Override
-			public final void keyPressed(java.awt.event.KeyEvent keyEvent) {
-				if (keyEvent.getSource().equals(m_jtAufstellungSpielerTable)) {
-					int row = m_jtAufstellungSpielerTable.getSelectedRow();
-					m_jtAufstellungSpielerTableName.setRowSelectionInterval(row, row);
-
-					Spieler spieler = m_jtAufstellungSpielerTable.getSorter().getSpieler(row);
-					if (spieler != null) {
-						HOMainFrame.instance().setActualSpieler(spieler.getSpielerID());
-					}
-				} else if (keyEvent.getSource().equals(m_jtAufstellungSpielerTableName)) {
-					int row = m_jtAufstellungSpielerTableName.getSelectedRow();
-					m_jtAufstellungSpielerTable.setRowSelectionInterval(row, row);
-
-					Spieler spieler = m_jtAufstellungSpielerTableName.getSorter().getSpieler(row);
-					if (spieler != null) {
-						HOMainFrame.instance().setActualSpieler(spieler.getSpielerID());
-					}
-				}
-			}
-		};
-		this.m_jtAufstellungSpielerTable.addKeyListener(kl);
-		this.m_jtAufstellungSpielerTableName.addKeyListener(kl);
+		this.m_jtAufstellungSpielerTable.getSelectionModel().addListSelectionListener(lsl);
+		this.m_jtAufstellungSpielerTableName.getSelectionModel().addListSelectionListener(lsl);
 	}
 }
