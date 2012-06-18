06f179d29ef9f173768c69425cc62bb99afd7330
kruescho <kruescho@b8de521a-fa13-0410-8113-fc7ffabbd59a>
2012-06-18 20:31:30 +0000

    some changes for plausibility checks (for problems, an explanation will be displayed)
    git-svn-id: file:///home/coren/project/backup_ho/trunk@1641 b8de521a-fa13-0410-8113-fc7ffabbd59a

--- Core/src/java/ho/module/lineup/substitution/plausibility/PlausibilityCheck.java
+++ Core/src/java/ho/module/lineup/substitution/plausibility/PlausibilityCheck.java
@@ -1,17 +1,65 @@
 package ho.module.lineup.substitution.plausibility;
 
+import ho.core.model.HOVerwaltung;
+import ho.core.model.player.Spieler;
+import ho.core.model.player.SpielerPosition;
 import ho.module.lineup.Lineup;
+import ho.module.lineup.substitution.LanguageStringLookup;
+import ho.module.lineup.substitution.model.MatchOrderType;
 import ho.module.lineup.substitution.model.Substitution;
 
 public class PlausibilityCheck {
-	
+
 	public static Problem checkForProblem(Lineup lineup, Substitution substitution) {
-		if (!lineup.isSpielerAufgestellt(substitution.getPlayerIn())) {
+		// first, check if players in lineup. note: when NEW_BEHAVIOUR, there is
+		// only one player involved
+		if (substitution.getOrderType() != MatchOrderType.NEW_BEHAVIOUR
+				&& !lineup.isSpielerAufgestellt(substitution.getPlayerIn())) {
 			return Error.PLAYERIN_NOT_IN_LINEUP;
 		} else if (!lineup.isSpielerAufgestellt(substitution.getPlayerOut())) {
 			return Error.PLAYEROUT_NOT_IN_LINEUP;
 		}
+
+		// when NEW_BEHAVIOUR, check that behaviour is not the same like at the
+		// beginning of the match
+		if (substitution.getOrderType() == MatchOrderType.NEW_BEHAVIOUR) {
+			SpielerPosition pos = lineup.getPositionBySpielerId(substitution.getPlayerOut());
+			if (pos.getTaktik() == substitution.getBehaviour()) {
+				return Uncertainty.SAME_TACTIC;
+			}
+		}
 		return null;
 	}
 
+	public static String getComment(Problem problem, Substitution substitution) {
+		if (problem instanceof Error) {
+			switch ((Error) problem) {
+			case PLAYERIN_NOT_IN_LINEUP:
+				return HOVerwaltung.instance().getLanguageString(
+						"subs.plausibility.playerIn.notInLineup",
+						getPlayerIn(substitution).getName());
+			case PLAYEROUT_NOT_IN_LINEUP:
+				return HOVerwaltung.instance().getLanguageString(
+						"subs.plausibility.playerOut.notInLineup",
+						getPlayerOut(substitution).getName());
+			}
+		} else if (problem instanceof Uncertainty) {
+			switch ((Uncertainty) problem) {
+			case SAME_TACTIC:
+				return HOVerwaltung.instance().getLanguageString("subs.plausibility.sameTactic",
+						getPlayerOut(substitution).getName(),
+						LanguageStringLookup.getBehaviour(substitution.getBehaviour()));
+			}
+		}
+
+		return null;
+	}
+
+	private static Spieler getPlayerIn(Substitution substitution) {
+		return HOVerwaltung.instance().getModel().getSpieler(substitution.getPlayerIn());
+	}
+
+	private static Spieler getPlayerOut(Substitution substitution) {
+		return HOVerwaltung.instance().getModel().getSpieler(substitution.getPlayerOut());
+	}
 }
