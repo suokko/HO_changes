45a2cec03c9e71121edf8805bb75b8a1721c5e8e
kruescho <kruescho@b8de521a-fa13-0410-8113-fc7ffabbd59a>
2012-06-20 19:05:35 +0000

    problem with plausibility check fixed
    git-svn-id: file:///home/coren/project/backup_ho/trunk@1661 b8de521a-fa13-0410-8113-fc7ffabbd59a

--- Core/src/java/ho/module/lineup/substitution/plausibility/Error.java
+++ Core/src/java/ho/module/lineup/substitution/plausibility/Error.java
@@ -2,6 +2,9 @@ package ho.module.lineup.substitution.plausibility;
 
 
 public enum Error implements Problem {
+	NEWBEHAVIOUR_PLAYER_MISSING,
+	SUBSTITUTION_PLAYER_MISSING,
+	POSITIONSWAP_PLAYER_MISSING,
 	PLAYERIN_NOT_IN_LINEUP,
 	PLAYEROUT_NOT_IN_LINEUP;
 }
