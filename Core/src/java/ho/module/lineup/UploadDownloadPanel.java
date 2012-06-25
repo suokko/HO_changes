da54fce019509cf1fc8257a0a995acdc52d5045f
kruescho <kruescho@b8de521a-fa13-0410-8113-fc7ffabbd59a>
2012-06-25 20:17:09 +0000

    use JTable instead of JList for the list of matches to download/upload
    git-svn-id: file:///home/coren/project/backup_ho/trunk@1674 b8de521a-fa13-0410-8113-fc7ffabbd59a

--- Core/src/java/ho/module/lineup/UploadDownloadPanel.java
+++ Core/src/java/ho/module/lineup/UploadDownloadPanel.java
@@ -2,12 +2,17 @@ package ho.module.lineup;
 
 import ho.core.db.DBManager;
 import ho.core.gui.HOMainFrame;
+import ho.core.gui.theme.HOIconName;
+import ho.core.gui.theme.ThemeManager;
 import ho.core.model.HOVerwaltung;
 import ho.core.model.match.MatchKurzInfo;
+import ho.core.model.match.MatchType;
 import ho.core.net.OnlineWorker;
+import ho.core.util.GUIUtilities;
 import ho.core.util.HOLogger;
 import ho.core.util.XMLUtils;
 
+import java.awt.Component;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
@@ -17,13 +22,19 @@ import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.swing.Icon;
 import javax.swing.JButton;
-import javax.swing.JList;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 import javax.swing.ListSelectionModel;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
+import javax.swing.table.AbstractTableModel;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.TableColumn;
 
 import org.w3c.dom.Document;
 import org.xml.sax.SAXException;
@@ -31,61 +42,81 @@ import org.xml.sax.SAXException;
 public class UploadDownloadPanel extends JPanel {
 
 	private static final long serialVersionUID = -5314050322847463180L;
-	private JList matchList;
+	private JTable matchesTable;
+	private JButton uploadButton;
+	private JButton downloadButton;
+	private JButton refreshButton;
 
 	public UploadDownloadPanel() {
 		initComponents();
+		addListeners();
 	}
 
 	private void initComponents() {
+
+		this.refreshButton = new JButton("refresh list");
+		this.downloadButton = new JButton("download lineup");
+		this.downloadButton.setEnabled(false);
+		this.uploadButton = new JButton("upload lineup");
+		this.uploadButton.setEnabled(false);
+
+		JPanel buttonPanel = new JPanel(new GridBagLayout());
+		GridBagConstraints gbc = new GridBagConstraints();
+		gbc.insets = new Insets(10, 8, 4, 10);
+		buttonPanel.add(this.refreshButton, gbc);
+
+		gbc.gridy = 1;
+		gbc.insets = new Insets(4, 8, 4, 10);
+		buttonPanel.add(this.uploadButton, gbc);
+
+		gbc.gridy = 2;
+		gbc.insets = new Insets(4, 8, 10, 10);
+		gbc.weightx = 1.0;
+		buttonPanel.add(this.downloadButton, gbc);
+
+		GUIUtilities.equalizeComponentSizes(this.refreshButton, this.uploadButton,
+				this.downloadButton);
+
 		MatchKurzInfo[] matches = DBManager.instance().getMatchesKurzInfo(
 				HOVerwaltung.instance().getModel().getBasics().getTeamId());
 
 		Timestamp today = new Timestamp(System.currentTimeMillis());
-		List<ListElement> listElements = new ArrayList<ListElement>();
+		List<MatchKurzInfo> data = new ArrayList<MatchKurzInfo>();
 		for (MatchKurzInfo match : matches) {
 			if (match.getMatchDateAsTimestamp().after(today)) {
-				listElements.add(new ListElement(match));
+				data.add(match);
 			}
 		}
 
-		final JButton uploadButton = new JButton("upload");
-		uploadButton.setEnabled(false);
-		uploadButton.addActionListener(new ActionListener() {
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				upload();
-			}
-		});
-
-		this.matchList = new JList(listElements.toArray());
-		this.matchList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-		this.matchList.addListSelectionListener(new ListSelectionListener() {
-
-			@Override
-			public void valueChanged(ListSelectionEvent e) {
-				if (!e.getValueIsAdjusting()) {
-					uploadButton.setEnabled(matchList.getSelectedIndex() != -1);
-				}
-			}
-		});
+		MatchesTableModel model = new MatchesTableModel(data);
+		this.matchesTable = new JTable();
+		this.matchesTable.setModel(model);
+		this.matchesTable.setAutoCreateRowSorter(true);
+		this.matchesTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		TableColumn matchTypeColumn = this.matchesTable.getColumnModel().getColumn(1);
+		matchTypeColumn.setCellRenderer(new MatchTypeCellRenderer());
+		matchTypeColumn.setMaxWidth(25);
 
 		setLayout(new GridBagLayout());
-		GridBagConstraints gbc = new GridBagConstraints();
+		gbc = new GridBagConstraints();
 		gbc.insets = new Insets(10, 10, 10, 10);
-		add(matchList, gbc);
+		gbc.fill = GridBagConstraints.BOTH;
+		gbc.weightx = 1.0;
+		gbc.weighty = 1.0;
+		add(new JScrollPane(this.matchesTable), gbc);
 
+		gbc = new GridBagConstraints();
+		gbc.insets = new Insets(10, 10, 10, 10);
 		gbc.gridx = 1;
-		add(uploadButton, gbc);
+		gbc.anchor = GridBagConstraints.NORTH;
+		add(buttonPanel, gbc);
 	}
 
 	private void upload() {
-		ListElement elem = (ListElement) this.matchList.getSelectedValue();
-		int matchId = elem.getMatch().getMatchID();
+		MatchKurzInfo match = getSelectedMatch();
 
 		OnlineWorker ow = new OnlineWorker();
-		String result = ow.uploadMatchOrder(matchId, elem.getMatch().getMatchTyp(), HOVerwaltung
+		String result = ow.uploadMatchOrder(match.getMatchID(), match.getMatchTyp(), HOVerwaltung
 				.instance().getModel().getAufstellung());
 
 		int messageType = JOptionPane.PLAIN_MESSAGE;
@@ -120,21 +151,109 @@ public class UploadDownloadPanel extends JPanel {
 				.getLanguageString("lineup.upload.title"), messageType);
 	}
 
-	private class ListElement {
-		private MatchKurzInfo match;
+	private MatchKurzInfo getSelectedMatch() {
+		int tableIndex = this.matchesTable.getSelectedRow();
+		if (tableIndex != -1) {
+			int modelIndex = this.matchesTable.convertRowIndexToModel(tableIndex);
+			return ((MatchesTableModel) this.matchesTable.getModel()).getMatch(modelIndex);
+		}
+		return null;
+	}
 
-		public ListElement(MatchKurzInfo match) {
-			this.match = match;
+	private void addListeners() {
+		this.uploadButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				upload();
+			}
+		});
+
+		this.matchesTable.getSelectionModel().addListSelectionListener(new ListSelectionListener() {
+
+			@Override
+			public void valueChanged(ListSelectionEvent e) {
+				if (!e.getValueIsAdjusting()) {
+					boolean enableButtons = matchesTable.getSelectedRow() != -1;
+					uploadButton.setEnabled(enableButtons);
+					downloadButton.setEnabled(enableButtons);
+				}
+			}
+		});
+	}
+
+	private class MatchesTableModel extends AbstractTableModel {
+
+		private static final long serialVersionUID = 7917970964575188677L;
+		private List<MatchKurzInfo> data;
+		private String[] columns = { "Datum", "", "Heim", "Gast", "ID" };
+
+		public MatchesTableModel() {
+			this.data = new ArrayList<MatchKurzInfo>();
 		}
 
-		public MatchKurzInfo getMatch() {
-			return this.match;
+		public MatchesTableModel(List<MatchKurzInfo> list) {
+			this.data = new ArrayList<MatchKurzInfo>(list);
+		}
+
+		public MatchKurzInfo getMatch(int row) {
+			System.out.println(row);
+			return this.data.get(row);
+		}
+
+		public void setData(List<MatchKurzInfo> list) {
+			this.data = new ArrayList<MatchKurzInfo>(list);
 		}
 
 		@Override
-		public String toString() {
-			return this.match.getMatchDate() + " - " + this.match.getHeimName() + " - "
-					+ this.match.getGastName();
+		public int getRowCount() {
+			return data.size();
+		}
+
+		@Override
+		public int getColumnCount() {
+			return columns.length;
+		}
+
+		@Override
+		public String getColumnName(int column) {
+			return columns[column];
+		}
+
+		@Override
+		public Object getValueAt(int rowIndex, int columnIndex) {
+			MatchKurzInfo match = this.data.get(rowIndex);
+			switch (columnIndex) {
+			case 0:
+				return match.getMatchDate();
+			case 1:
+				return match.getMatchTyp();
+			case 2:
+				return match.getHeimName();
+			case 3:
+				return match.getGastName();
+			case 4:
+				return match.getMatchID();
+
+			}
+			return null;
+		}
+	}
+
+	private class MatchTypeCellRenderer extends DefaultTableCellRenderer {
+
+		private static final long serialVersionUID = -6068887874289410058L;
+
+		@Override
+		public Component getTableCellRendererComponent(JTable table, Object value,
+				boolean isSelected, boolean hasFocus, int row, int column) {
+			JLabel component = (JLabel) super.getTableCellRendererComponent(table, value,
+					isSelected, hasFocus, row, column);
+			component.setText(null);
+			MatchType type = (MatchType) value;
+			Icon icon = ThemeManager.getIcon(HOIconName.MATCHTYPES[type.getIconArrayIndex()]);
+			component.setIcon(icon);
+			return component;
 		}
 	}
 }
