a265dc45b0691c34641ad1e5c138cff233131ac9
kruescho <kruescho@b8de521a-fa13-0410-8113-fc7ffabbd59a>
2012-07-02 20:32:01 +0000

    new tab for penalty shooters
    git-svn-id: file:///home/coren/project/backup_ho/trunk@1717 b8de521a-fa13-0410-8113-fc7ffabbd59a

---
+++ Core/src/java/ho/module/lineup/penalties/PenaltyShootersView.java
@@ -0,0 +1,9 @@
+package ho.module.lineup.penalties;
+
+import javax.swing.JPanel;
+
+public class PenaltyShootersView extends JPanel {
+
+	private static final long serialVersionUID = -5089904466636200088L;
+
+}
