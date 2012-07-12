ca3f9e49d8816009b733edb6f85173708eb3ab2a
kruescho <kruescho@b8de521a-fa13-0410-8113-fc7ffabbd59a>
2012-07-12 08:38:05 +0000

    small refactoring
    git-svn-id: file:///home/coren/project/backup_ho/trunk/Core@1755 b8de521a-fa13-0410-8113-fc7ffabbd59a

--- src/java/ho/core/gui/comp/tabbedPane/HOTabbedPane.java
+++ src/java/ho/core/gui/comp/tabbedPane/HOTabbedPane.java
@@ -12,51 +12,52 @@ import javax.swing.UIManager;
 public final class HOTabbedPane extends JTabbedPane {
 
 	private static final long serialVersionUID = 1L;
-	
-	public HOTabbedPane(){
+
+	public HOTabbedPane() {
 		super();
 	}
-	
+
 	public void addTab(String title, Component component, boolean closeIcon) {
 		int xOffset = 0;
 		super.addTab(title, component);
-		if(closeIcon) {
-			// Nimbus skin is the only one to put the icon on the right, which we need to move away from title text
+		if (closeIcon) {
+			// Nimbus skin is the only one to put the icon on the right, which
+			// we need to move away from title text
 			if (UIManager.getLookAndFeel().getName().equalsIgnoreCase("Nimbus")) {
 				xOffset = 6;
 			}
-			setIconAt(getComponentCount()-1, new TabCloseIcon(this, xOffset));
+			setIconAt(getComponentCount() - 1, new TabCloseIcon(this, xOffset));
 		}
 	}
 
-	public void showTab(int moduleId){
+	public void showTab(int moduleId) {
 		IModule module = ModuleManager.instance().getModule(moduleId);
-		if(module.hasMainTab()){
+		if (module.hasMainTab()) {
 			int index = indexOfTab(module.getDescription());
-			if(index == -1){
-				addTab(module.getDescription(),module.createTabPanel(),true);
-				setSelectedIndex(getTabCount()-1);
-			}
-			else
+			if (index == -1) {
+				addTab(module.getDescription(), module.createTabPanel(), true);
+				setSelectedIndex(getTabCount() - 1);
+			} else {
 				setSelectedIndex(index);
+			}
 		}
 	}
-	
-	public JPanel getModulePanel(int moduleId){
+
+	public JPanel getModulePanel(int moduleId) {
 		IModule module = ModuleManager.instance().getModule(moduleId);
-		if(module.hasMainTab()){
+		if (module.hasMainTab()) {
 			int index = indexOfTab(module.getDescription());
-			if(index == -1){
-				addTab(module.getDescription(),module.createTabPanel(),true);
-				setSelectedIndex(getTabCount()-1);
-				index = getTabCount()-1;
+			if (index == -1) {
+				addTab(module.getDescription(), module.createTabPanel(), true);
+				setSelectedIndex(getTabCount() - 1);
+				index = getTabCount() - 1;
 			}
-			return (JPanel)getComponent(index);
+			return (JPanel) getComponent(index);
 		}
 		return null;
 	}
-	
-	public boolean isModuleTabVisible(int moduleId){
+
+	public boolean isModuleTabVisible(int moduleId) {
 		IModule module = ModuleManager.instance().getModule(moduleId);
 		return indexOfTab(module.getDescription()) != -1;
 	}
