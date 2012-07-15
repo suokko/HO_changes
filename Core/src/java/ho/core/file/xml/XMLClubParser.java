d5221a481231b9cc2882e9ed65a6daa88dc0be98
kruescho <kruescho@b8de521a-fa13-0410-8113-fc7ffabbd59a>
2012-07-15 10:45:31 +0000

    XMLClubParser is a static utility class now; some refactoring
    git-svn-id: file:///home/coren/project/backup_ho/trunk@1808 b8de521a-fa13-0410-8113-fc7ffabbd59a

--- Core/src/java/ho/core/file/xml/XMLClubParser.java
+++ Core/src/java/ho/core/file/xml/XMLClubParser.java
@@ -6,147 +6,40 @@
  */
 package ho.core.file.xml;
 
-
 import ho.core.model.misc.Verein;
 import ho.core.util.HOLogger;
-
+import java.util.Map;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
-
-
 /**
- * DOCUMENT ME!
  *
  * @author thomas.werth
  */
 public class XMLClubParser {
-    //~ Constructors -------------------------------------------------------------------------------
 
-    /**
-     * Creates a new instance of XMLClubParser
-     */
-    public XMLClubParser() {
+	/**
+	 * Utility class - private constructor enforces noninstantiability.
+	 */
+    private XMLClubParser() {
     }
+    
 
-    //~ Methods ------------------------------------------------------------------------------------
-
-    /////////////////////////////////////////////////////////////////////////////////    
-    //parse public
-    ////////////////////////////////////////////////////////////////////////////////    
-    public final Verein parseClub(String dateiname) {
-    	return parseVerein(XMLManager.parseFile(dateiname));
-    }
-
-    /**
-     * TODO Missing Method Documentation
-     *
-     * @param datei TODO Missing Method Parameter Documentation
-     *
-     * @return TODO Missing Return Method Documentation
-     */
-    public final Verein parseClub(java.io.File datei) {
-        return parseVerein(XMLManager.parseFile(datei));
-    }
-
-    /**
-     * TODO Missing Method Documentation
-     *
-     * @param inputStream TODO Missing Method Parameter Documentation
-     *
-     * @return TODO Missing Return Method Documentation
-     */
-    public final java.util.Hashtable<?, ?> parseClubFromString(String inputStream) {
+    public static Map<String, String> parseClubFromString(String inputStream) {
         return parseDetails(XMLManager.parseString(inputStream));
     }
 
-    /**
-     * erstellt das MAtchlineup Objekt
-     *
-     * @param doc TODO Missing Constructuor Parameter Documentation
-     *
-     * @return TODO Missing Return Method Documentation
-     */
-    protected final java.util.Hashtable<?, ?> parseDetails(Document doc) {
-        Element ele = null;
-        Element root = null;
-        ho.core.file.xml.MyHashtable club = new ho.core.file.xml.MyHashtable();
-
+    public static Verein parseVerein(Document doc) {
         if (doc == null) {
-            return club;
+            return null;
         }
 
-        //Tabelle erstellen
-        root = doc.getDocumentElement();
-
-        try {
-            //Daten füllen
-            ele = (Element) root.getElementsByTagName("FetchedDate").item(0);
-            club.put("FetchedDate", XMLManager.getFirstChildNodeValue(ele));
-
-            //Root wechseln
-            root = (Element) root.getElementsByTagName("Team").item(0);
-            ele = (Element) root.getElementsByTagName("TeamID").item(0);
-            club.put("TeamID", XMLManager.getFirstChildNodeValue(ele));
-            ele = (Element) root.getElementsByTagName("TeamName").item(0);
-            club.put("TeamName", XMLManager.getFirstChildNodeValue(ele));
-
-            //nochmal root wechseln
-            root = (Element) doc.getDocumentElement().getElementsByTagName("Specialists").item(0);
-            ele = (Element) root.getElementsByTagName("Doctors").item(0);
-            club.put("Doctors", XMLManager.getFirstChildNodeValue(ele));
-            ele = (Element) root.getElementsByTagName("PressSpokesmen").item(0);
-            club.put("PressSpokesmen", XMLManager.getFirstChildNodeValue(ele));
-            ele = (Element) root.getElementsByTagName("AssistantTrainers").item(0);
-            club.put("AssistantTrainers", XMLManager.getFirstChildNodeValue(ele));
-            ele = (Element) root.getElementsByTagName("Physiotherapists").item(0);
-            club.put("Physiotherapists", XMLManager.getFirstChildNodeValue(ele));
-            ele = (Element) root.getElementsByTagName("Psychologists").item(0);
-            club.put("Psychologists", XMLManager.getFirstChildNodeValue(ele));
-
-            //und nochmal root wechseln
-            root = (Element) doc.getDocumentElement().getElementsByTagName("YouthSquad").item(0);
-            ele = (Element) root.getElementsByTagName("Investment").item(0);
-            club.put("Investment", XMLManager.getFirstChildNodeValue(ele));
-            ele = (Element) root.getElementsByTagName("HasPromoted").item(0);
-            club.put("HasPromoted", XMLManager.getFirstChildNodeValue(ele));
-            ele = (Element) root.getElementsByTagName("YouthLevel").item(0);
-            club.put("YouthLevel", XMLManager.getFirstChildNodeValue(ele));
-        } catch (Exception e) {
-            HOLogger.instance().log(getClass(),"XMLClubParser.createVerein Exception gefangen: " + e);
-            HOLogger.instance().log(getClass(),e);
-            club = null;
-        }
-
-        return club;
-    }
-
-    /////////////////////////////////////////////////////////////////////////////////    
-    //Parser Helper private
-    ////////////////////////////////////////////////////////////////////////////////    
-
-    /**
-     * erstellt das MAtchlineup Objekt
-     *
-     * @param doc TODO Missing Constructuor Parameter Documentation
-     *
-     * @return TODO Missing Return Method Documentation
-     */
-    protected final Verein parseVerein(Document doc) {
-        Element ele = null;
-        Element root = null;
         Verein club = new Verein();
-
-        if (doc == null) {
-            return club;
-        }
-
-        //Tabelle erstellen
-        root = doc.getDocumentElement();
-
+        
         try {
+        	Element root = doc.getDocumentElement();
             //Daten füllen
-            ele = (Element) root.getElementsByTagName("FetchedDate").item(0);
+        	Element  ele = (Element) root.getElementsByTagName("FetchedDate").item(0);
             club.setDateFromString(ele.getFirstChild().getNodeValue());
 
             //Root wechseln
@@ -178,11 +71,59 @@ public class XMLClubParser {
             ele = (Element) root.getElementsByTagName("YouthLevel").item(0);
             club.setJugend(Integer.parseInt(ele.getFirstChild().getNodeValue()));
         } catch (Exception e) {
-            HOLogger.instance().log(getClass(),"XMLClubParser.createVerein Exception gefangen: " + e);
-            HOLogger.instance().log(getClass(),e);
+            HOLogger.instance().log(XMLClubParser.class,e);
             club = null;
         }
 
         return club;
+    }
+    
+    private static Map<String, String> parseDetails(Document doc) {
+        Map<String, String> map = new MyHashtable();
+
+        if (doc == null) {
+            return map;
+        }
+
+        try {
+        	Element root = doc.getDocumentElement();
+            //Daten füllen
+        	Element ele = (Element) root.getElementsByTagName("FetchedDate").item(0);
+            map.put("FetchedDate", XMLManager.getFirstChildNodeValue(ele));
+
+            //Root wechseln
+            root = (Element) root.getElementsByTagName("Team").item(0);
+            ele = (Element) root.getElementsByTagName("TeamID").item(0);
+            map.put("TeamID", XMLManager.getFirstChildNodeValue(ele));
+            ele = (Element) root.getElementsByTagName("TeamName").item(0);
+            map.put("TeamName", XMLManager.getFirstChildNodeValue(ele));
+
+            //nochmal root wechseln
+            root = (Element) doc.getDocumentElement().getElementsByTagName("Specialists").item(0);
+            ele = (Element) root.getElementsByTagName("Doctors").item(0);
+            map.put("Doctors", XMLManager.getFirstChildNodeValue(ele));
+            ele = (Element) root.getElementsByTagName("PressSpokesmen").item(0);
+            map.put("PressSpokesmen", XMLManager.getFirstChildNodeValue(ele));
+            ele = (Element) root.getElementsByTagName("AssistantTrainers").item(0);
+            map.put("AssistantTrainers", XMLManager.getFirstChildNodeValue(ele));
+            ele = (Element) root.getElementsByTagName("Physiotherapists").item(0);
+            map.put("Physiotherapists", XMLManager.getFirstChildNodeValue(ele));
+            ele = (Element) root.getElementsByTagName("Psychologists").item(0);
+            map.put("Psychologists", XMLManager.getFirstChildNodeValue(ele));
+
+            //und nochmal root wechseln
+            root = (Element) doc.getDocumentElement().getElementsByTagName("YouthSquad").item(0);
+            ele = (Element) root.getElementsByTagName("Investment").item(0);
+            map.put("Investment", XMLManager.getFirstChildNodeValue(ele));
+            ele = (Element) root.getElementsByTagName("HasPromoted").item(0);
+            map.put("HasPromoted", XMLManager.getFirstChildNodeValue(ele));
+            ele = (Element) root.getElementsByTagName("YouthLevel").item(0);
+            map.put("YouthLevel", XMLManager.getFirstChildNodeValue(ele));
+        } catch (Exception e) {
+            HOLogger.instance().log(XMLClubParser.class,e);
+            map = null;
+        }
+
+        return map;
     }
 }
