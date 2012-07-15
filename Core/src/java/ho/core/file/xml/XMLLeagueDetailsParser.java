94baba83fbbdf33fcdf70ff8b67dee16d242f016
kruescho <kruescho@b8de521a-fa13-0410-8113-fc7ffabbd59a>
2012-07-15 09:23:19 +0000

    XMLLeagueDetailsParser is a static utility class now; some refactoring
    git-svn-id: file:///home/coren/project/backup_ho/trunk@1805 b8de521a-fa13-0410-8113-fc7ffabbd59a

--- Core/src/java/ho/core/file/xml/XMLLeagueDetailsParser.java
+++ Core/src/java/ho/core/file/xml/XMLLeagueDetailsParser.java
@@ -6,109 +6,78 @@
  */
 package ho.core.file.xml;
 
-
 import ho.core.util.HOLogger;
 
-import java.util.Hashtable;
+import java.util.Map;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
 
-
-
 /**
- * DOCUMENT ME!
- *
+ * 
  * @author thomas.werth
  */
 public class XMLLeagueDetailsParser {
-    //~ Constructors -------------------------------------------------------------------------------
 
-    /**
-     * Creates a new instance of xmlLeagueDetailsParser
-     */
-    public XMLLeagueDetailsParser() {
-    }
+	/**
+	 * Utility class - private constructor enforces noninstantiability.
+	 */
+	private XMLLeagueDetailsParser() {
+	}
 
-    //~ Methods ------------------------------------------------------------------------------------
+	public static Map<String, String> parseLeagueDetailsFromString(String str, String teamID) {
+		return parseDetails(XMLManager.parseString(str), teamID);
+	}
 
-    /////////////////////////////////////////////////////////////////////////////////    
-    //parse public
-    ////////////////////////////////////////////////////////////////////////////////    
-    public final Hashtable<?, ?> parseLeagueDetailsFromString(String inputStream, String teamID) {
-        Document doc = null;
+	private static Map<String, String> parseDetails(Document doc, String teamID) {
+		Map<String, String> map = new MyHashtable();
 
-        doc = XMLManager.parseString(inputStream);
+		if (doc == null) {
+			return map;
+		}
 
-        return parseDetails(doc, teamID);
-    }
+		// Tabelle erstellen
+		Element root = doc.getDocumentElement();
 
-    /////////////////////////////////////////////////////////////////////////////////    
-    //Parser Helper private
-    ////////////////////////////////////////////////////////////////////////////////       
+		try {
+			// Daten füllen
+			Element ele = (Element) root.getElementsByTagName("LeagueLevelUnitName").item(0);
+			map.put("LeagueLevelUnitName", XMLManager.getFirstChildNodeValue(ele));
 
-    /**
-     * erstellt das MAtchlineup Objekt
-     *
-     * @param doc TODO Missing Constructuor Parameter Documentation
-     * @param teamID TODO Missing Constructuor Parameter Documentation
-     *
-     * @return TODO Missing Return Method Documentation
-     */
-    protected final Hashtable<?, ?> parseDetails(Document doc, String teamID) {
-        Element ele = null;
-        Element root = null;
-        final ho.core.file.xml.MyHashtable hash = new ho.core.file.xml.MyHashtable();
-        NodeList list = null;
+			// Einträge adden
+			NodeList list = root.getElementsByTagName("Team");
 
-        if (doc == null) {
-            return hash;
-        }
+			for (int i = 0; (list != null) && (i < list.getLength()); i++) {
+				root = (Element) list.item(i);
+				// Team suchen
+				if (XMLManager.getFirstChildNodeValue(
+						(Element) root.getElementsByTagName("TeamID").item(0)).equals(teamID)) {
 
-        //Tabelle erstellen
-        root = doc.getDocumentElement();
+					// Land
+					ele = (Element) root.getElementsByTagName("TeamID").item(0);
+					map.put("TeamID", XMLManager.getFirstChildNodeValue(ele));
+					ele = (Element) root.getElementsByTagName("Position").item(0);
+					map.put("Position", XMLManager.getFirstChildNodeValue(ele));
+					ele = (Element) root.getElementsByTagName("TeamName").item(0);
+					map.put("TeamName", XMLManager.getFirstChildNodeValue(ele));
+					ele = (Element) root.getElementsByTagName("Matches").item(0);
+					map.put("Matches", XMLManager.getFirstChildNodeValue(ele));
+					ele = (Element) root.getElementsByTagName("GoalsFor").item(0);
+					map.put("GoalsFor", XMLManager.getFirstChildNodeValue(ele));
+					ele = (Element) root.getElementsByTagName("GoalsAgainst").item(0);
+					map.put("GoalsAgainst", XMLManager.getFirstChildNodeValue(ele));
+					ele = (Element) root.getElementsByTagName("Points").item(0);
+					map.put("Points", XMLManager.getFirstChildNodeValue(ele));
 
-        try {
-            //Daten füllen            
-            //root  =   (Element) root.getElementsByTagName ( "Team" ).item (0);    
-            ele = (Element) root.getElementsByTagName("LeagueLevelUnitName").item(0);
-            hash.put("LeagueLevelUnitName", (XMLManager.getFirstChildNodeValue(ele)));
+					// fertig
+					break;
+				}
+			}
+		} catch (Exception e) {
+			HOLogger.instance().log(XMLLeagueDetailsParser.class, e);
+		}
 
-            //Einträge adden
-            list = root.getElementsByTagName("Team");
-
-            for (int i = 0; (list != null) && (i < list.getLength()); i++) {
-                //Team suchen
-                if (XMLManager.getFirstChildNodeValue((Element) ((Element) list.item(i)).getElementsByTagName("TeamID")
-                                                                 .item(0)).equals(teamID)) {
-                    root = (Element) list.item(i);
-
-                    //Land
-                    ele = (Element) root.getElementsByTagName("TeamID").item(0);
-                    hash.put("TeamID", (XMLManager.getFirstChildNodeValue(ele)));
-                    ele = (Element) root.getElementsByTagName("Position").item(0);
-                    hash.put("Position", (XMLManager.getFirstChildNodeValue(ele)));
-                    ele = (Element) root.getElementsByTagName("TeamName").item(0);
-                    hash.put("TeamName", (XMLManager.getFirstChildNodeValue(ele)));
-                    ele = (Element) root.getElementsByTagName("Matches").item(0);
-                    hash.put("Matches", (XMLManager.getFirstChildNodeValue(ele)));
-                    ele = (Element) root.getElementsByTagName("GoalsFor").item(0);
-                    hash.put("GoalsFor", (XMLManager.getFirstChildNodeValue(ele)));
-                    ele = (Element) root.getElementsByTagName("GoalsAgainst").item(0);
-                    hash.put("GoalsAgainst", (XMLManager.getFirstChildNodeValue(ele)));
-                    ele = (Element) root.getElementsByTagName("Points").item(0);
-                    hash.put("Points", (XMLManager.getFirstChildNodeValue(ele)));
-
-                    //fertig
-                    break;
-                }
-            }
-        } catch (Exception e) {
-            HOLogger.instance().log(getClass(),"XMLTeamDetailsParser.parseDetails Exception gefangen: " + e);
-            HOLogger.instance().log(getClass(),e);
-        }
-
-        return hash;
-    }
+		return map;
+	}
 }
