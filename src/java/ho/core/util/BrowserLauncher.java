36b1b6f7595aa6f8ae274c780e58649d25cd5e6b
kruescho <kruescho@b8de521a-fa13-0410-8113-fc7ffabbd59a>
2012-07-20 22:58:48 +0000

    new method to open url in browser
    git-svn-id: file:///home/coren/project/backup_ho/trunk/Core@1843 b8de521a-fa13-0410-8113-fc7ffabbd59a

--- src/java/ho/core/util/BrowserLauncher.java
+++ src/java/ho/core/util/BrowserLauncher.java
@@ -6,6 +6,8 @@ import ho.core.net.MyConnector;
 
 import java.awt.Color;
 import java.awt.Component;
+import java.awt.Desktop;
+import java.awt.Desktop.Action;
 import java.awt.event.InputEvent;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
@@ -15,6 +17,8 @@ import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
@@ -774,5 +778,39 @@ public class BrowserLauncher {
 					"Browser not found",
 					JOptionPane.INFORMATION_MESSAGE);
 	    }
+	}
+	
+	public static void open(String urlString) throws IOException, URISyntaxException {
+		if (Desktop.isDesktopSupported()
+				&& Desktop.getDesktop().isSupported(Action.BROWSE)) {
+			Desktop.getDesktop().browse(new URI(urlString));
+		} else {
+			// taken from
+			// http://www.mkyong.com/java/open-browser-in-java-windows-or-linux/
+			String os = System.getProperty("os.name").toLowerCase();
+			Runtime rt = Runtime.getRuntime();
+			if (os.indexOf("win") >= 0) {
+				// this doesn't support showing urls in the form
+				// of "page.html#nameLink"
+				rt.exec("rundll32 url.dll,FileProtocolHandler " + urlString);
+			} else if (os.indexOf("mac") >= 0) {
+				rt.exec("open " + urlString);
+			} else if (os.indexOf("nix") >= 0 || os.indexOf("nux") >= 0) {
+				// Do a best guess on unix until we get a platform
+				// independent way.
+				// Build a list of browsers to try, in this order.
+				String[] browsers = { "epiphany", "firefox", "mozilla", "konqueror",
+						"netscape", "opera", "links", "lynx" };
+
+				// Build a command string which looks like
+				// "browser1 "url" || browser2 "url" ||..."
+				StringBuffer cmd = new StringBuffer();
+				for (int i = 0; i < browsers.length; i++)
+					cmd.append((i == 0 ? "" : " || ") + browsers[i] + " \"" + urlString
+							+ "\" ");
+
+				rt.exec(new String[] { "sh", "-c", cmd.toString() });
+			}
+		}
 	}
 }
