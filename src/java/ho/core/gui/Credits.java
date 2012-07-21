5d5c92210d80f8f56367b622c3ca9a1478533a53
kruescho <kruescho@b8de521a-fa13-0410-8113-fc7ffabbd59a>
2012-07-21 10:32:52 +0000

    added message for fatcow to the credits
    git-svn-id: file:///home/coren/project/backup_ho/trunk/Core@1847 b8de521a-fa13-0410-8113-fc7ffabbd59a

--- src/java/ho/core/gui/Credits.java
+++ src/java/ho/core/gui/Credits.java
@@ -21,52 +21,77 @@ import javax.swing.JPanel;
 
 public class Credits {
 
+	private static final Color LINK_COLOR = new Color(6, 69, 173);
+	
 	public static void showCredits(Component parent) {
 		StringBuilder hoCredits = new StringBuilder(200);
 		hoCredits.append("Hattrick Organizer ").append(HO.VERSION).append("\n\n");
 		hoCredits.append(HOVerwaltung.instance().getLanguageString("MenuCredits"));
 		hoCredits.append("\n\n");
 
+		String fugueURL = "http://p.yusukekamiyamane.com/";
 		JPanel fuguePanel = new JPanel(new GridBagLayout());
 		GridBagConstraints gbc = new GridBagConstraints();
 		gbc.anchor = GridBagConstraints.NORTHWEST;
-		fuguePanel.add(new JLabel("Some icons by "), gbc);
+		fuguePanel.add(new JLabel("Some icons by Yusuke Kamiyamane, "), gbc);
 		gbc.gridx = 1;
-		final JLabel linkLabel = new JLabel("Yusuke Kamiyamane.");
-		linkLabel.setForeground(new Color(6, 69, 173));
+		JLabel linkLabel = new JLabel(fugueURL + ".");
+		linkLabel.setForeground(LINK_COLOR);
+		linkLabel.addMouseListener(new LinkListener(fugueURL));
 		fuguePanel.add(linkLabel, gbc);
 		gbc.gridx = 2;
 		gbc.fill = GridBagConstraints.HORIZONTAL;
 		gbc.weightx = 1.0;
 		fuguePanel.add(new JLabel(" All rights reserved."), gbc);
-
-		linkLabel.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseEntered(MouseEvent e) {
-				linkLabel.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-			}
-
-			@Override
-			public void mouseExited(MouseEvent e) {
-				linkLabel.setCursor(Cursor.getDefaultCursor());
-			}
-
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				try {
-					BrowserLauncher.openURL("http://p.yusukekamiyamane.com/");
-				} catch (IOException ex) {
-					HOLogger.instance().log(Credits.class, ex);
-				} catch (URISyntaxException ex) {
-					HOLogger.instance().log(Credits.class, ex);
-				}
-			}
-		});
-
-		Object[] components = { hoCredits.toString(), fuguePanel };
+		
+		String fatcowURL = "http://www.fatcow.com/free-icons";
+		JPanel fatcowPanel = new JPanel(new GridBagLayout());
+		gbc = new GridBagConstraints();
+		gbc.anchor = GridBagConstraints.NORTHWEST;
+		fatcowPanel.add(new JLabel("Thx to Fatcow Hosting for this great Icon set! Check it out: "), gbc);
+		gbc.gridx = 1;
+		linkLabel = new JLabel(fatcowURL);
+		linkLabel.setForeground(LINK_COLOR);
+		linkLabel.addMouseListener(new LinkListener(fatcowURL));
+		gbc.gridx = 2;
+		gbc.fill = GridBagConstraints.HORIZONTAL;
+		gbc.weightx = 1.0;
+		fatcowPanel.add(linkLabel, gbc);
+		
+		Object[] components = { hoCredits.toString(), fuguePanel, fatcowPanel };
 
 		JOptionPane.showMessageDialog(parent, components, HOVerwaltung.instance()
 				.getLanguageString("MenuCreditsChoice"), JOptionPane.INFORMATION_MESSAGE);
+	}
+
+	private static class LinkListener extends MouseAdapter {
+
+		private final String url;
+
+		public LinkListener(String url) {
+			this.url = url;
+		}
+
+		@Override
+		public void mouseEntered(MouseEvent e) {
+			((Component) e.getSource()).setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		}
+
+		@Override
+		public void mouseExited(MouseEvent e) {
+			((Component) e.getSource()).setCursor(Cursor.getDefaultCursor());
+		}
+
+		@Override
+		public void mouseClicked(MouseEvent e) {
+			try {
+				BrowserLauncher.openURL(url);
+			} catch (IOException ex) {
+				HOLogger.instance().log(Credits.class, ex);
+			} catch (URISyntaxException ex) {
+				HOLogger.instance().log(Credits.class, ex);
+			}
+		}
 	}
 
 }
