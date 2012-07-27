3b182f29c167418cd1127442135f2f78afa0de04
kruescho <kruescho@b8de521a-fa13-0410-8113-fc7ffabbd59a>
2012-07-27 21:06:22 +0000

    new class HyperLinkLabel
    git-svn-id: file:///home/coren/project/backup_ho/trunk/Core@1875 b8de521a-fa13-0410-8113-fc7ffabbd59a

--- src/java/ho/core/gui/Credits.java
+++ src/java/ho/core/gui/Credits.java
@@ -1,27 +1,18 @@
 package ho.core.gui;
 
 import ho.HO;
+import ho.core.gui.comp.HyperLinkLabel;
 import ho.core.model.HOVerwaltung;
-import ho.core.util.BrowserLauncher;
-import ho.core.util.HOLogger;
 
-import java.awt.Color;
 import java.awt.Component;
-import java.awt.Cursor;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.io.IOException;
-import java.net.URISyntaxException;
 
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class Credits {
-
-	private static final Color LINK_COLOR = new Color(6, 69, 173);
 
 	public static void showCredits(Component parent) {
 		StringBuilder hoCredits = new StringBuilder(200);
@@ -35,9 +26,7 @@ public class Credits {
 		gbc.anchor = GridBagConstraints.NORTHWEST;
 		fuguePanel.add(new JLabel("Some icons by Yusuke Kamiyamane, "), gbc);
 		gbc.gridx = 1;
-		JLabel linkLabel = new JLabel(fugueURL + ".");
-		linkLabel.setForeground(LINK_COLOR);
-		linkLabel.addMouseListener(new LinkListener(fugueURL));
+		JLabel linkLabel = new HyperLinkLabel(fugueURL + ".", fugueURL);
 		fuguePanel.add(linkLabel, gbc);
 		gbc.gridx = 2;
 		gbc.fill = GridBagConstraints.HORIZONTAL;
@@ -50,9 +39,7 @@ public class Credits {
 		gbc.anchor = GridBagConstraints.NORTHWEST;
 		fatcowPanel.add(new JLabel("Some icons by Fatcow Hosting, "), gbc);
 		gbc.gridx = 1;
-		linkLabel = new JLabel(fatcowURL + ".");
-		linkLabel.setForeground(LINK_COLOR);
-		linkLabel.addMouseListener(new LinkListener(fatcowURL));
+		linkLabel = new HyperLinkLabel(fatcowURL + ".", fatcowURL);
 		fatcowPanel.add(linkLabel, gbc);
 		gbc.gridx = 2;
 		gbc.fill = GridBagConstraints.HORIZONTAL;
@@ -64,35 +51,4 @@ public class Credits {
 		JOptionPane.showMessageDialog(parent, components, HOVerwaltung.instance()
 				.getLanguageString("MenuCreditsChoice"), JOptionPane.INFORMATION_MESSAGE);
 	}
-
-	private static class LinkListener extends MouseAdapter {
-
-		private final String url;
-
-		public LinkListener(String url) {
-			this.url = url;
-		}
-
-		@Override
-		public void mouseEntered(MouseEvent e) {
-			((Component) e.getSource()).setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-		}
-
-		@Override
-		public void mouseExited(MouseEvent e) {
-			((Component) e.getSource()).setCursor(Cursor.getDefaultCursor());
-		}
-
-		@Override
-		public void mouseClicked(MouseEvent e) {
-			try {
-				BrowserLauncher.openURL(url);
-			} catch (IOException ex) {
-				HOLogger.instance().log(Credits.class, ex);
-			} catch (URISyntaxException ex) {
-				HOLogger.instance().log(Credits.class, ex);
-			}
-		}
-	}
-
 }
