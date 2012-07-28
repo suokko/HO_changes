e0860772f7d6430b091a5d6c163aa88b5a11866c
werder0405 <werder0405@b8de521a-fa13-0410-8113-fc7ffabbd59a>
2012-07-28 15:22:48 +0000

    Rework language strings
    git-svn-id: https://ho1.svn.sourceforge.net/svnroot/ho1/trunk@1885 b8de521a-fa13-0410-8113-fc7ffabbd59a

--- Core/src/java/ho/core/constants/TrainingType.java
+++ Core/src/java/ho/core/constants/TrainingType.java
@@ -15,23 +15,23 @@ public final class TrainingType {
 	public static final int THROUGH_PASSES 		= 10;
 	public static final int DEF_POSITIONS 		= 11;
     public static final int WING_ATTACKS 		= 12;
-    
-    public static CBItem[] ITEMS = { 
-		new CBItem(HOVerwaltung.instance().getLanguageString("training.set_pieces"), SET_PIECES),
-		new CBItem(HOVerwaltung.instance().getLanguageString("training.defending"), DEFENDING),
-		new CBItem(HOVerwaltung.instance().getLanguageString("training.scoring"), SCORING),
-		new CBItem(HOVerwaltung.instance().getLanguageString("training.crossing"), CROSSING_WINGER),
-		new CBItem(HOVerwaltung.instance().getLanguageString("training.shooting"), SHOOTING),
-		new CBItem(HOVerwaltung.instance().getLanguageString("training.short_passes"), SHORT_PASSES),
-		new CBItem(HOVerwaltung.instance().getLanguageString("training.playmaking"), PLAYMAKING),
-		new CBItem(HOVerwaltung.instance().getLanguageString("training.goalkeeping"), GOALKEEPING),
-		new CBItem(HOVerwaltung.instance().getLanguageString("training.through_passes"), THROUGH_PASSES),
-		new CBItem(HOVerwaltung.instance().getLanguageString("training.defensive_positions"), DEF_POSITIONS),
-		new CBItem(HOVerwaltung.instance().getLanguageString("training.wing_attacks"), WING_ATTACKS)
+
+    public static CBItem[] ITEMS = {
+		new CBItem(HOVerwaltung.instance().getLanguageString("ls.team.trainingtype.setpieces"), SET_PIECES),
+		new CBItem(HOVerwaltung.instance().getLanguageString("ls.team.trainingtype.defending"), DEFENDING),
+		new CBItem(HOVerwaltung.instance().getLanguageString("ls.team.trainingtype.scoring"), SCORING),
+		new CBItem(HOVerwaltung.instance().getLanguageString("ls.team.trainingtype.crossing"), CROSSING_WINGER),
+		new CBItem(HOVerwaltung.instance().getLanguageString("ls.team.trainingtype.shooting"), SHOOTING),
+		new CBItem(HOVerwaltung.instance().getLanguageString("ls.team.trainingtype.shortpasses"), SHORT_PASSES),
+		new CBItem(HOVerwaltung.instance().getLanguageString("ls.team.trainingtype.playmaking"), PLAYMAKING),
+		new CBItem(HOVerwaltung.instance().getLanguageString("ls.team.trainingtype.goalkeeping"), GOALKEEPING),
+		new CBItem(HOVerwaltung.instance().getLanguageString("ls.team.trainingtype.throughpasses"), THROUGH_PASSES),
+		new CBItem(HOVerwaltung.instance().getLanguageString("ls.team.trainingtype.defensivepositions"), DEF_POSITIONS),
+		new CBItem(HOVerwaltung.instance().getLanguageString("ls.team.trainingtype.wingattacks"), WING_ATTACKS)
 	};
-    
+
     private TrainingType(){};
-    
+
     public static String toString(int trainingType){
     	if(trainingType >= SET_PIECES && trainingType <= WING_ATTACKS)
     		return ITEMS[trainingType-SET_PIECES].getText();
