f46b157ea996e3d20cd175cd4c84174de04d87cd
suokkos <suokkos@b8de521a-fa13-0410-8113-fc7ffabbd59a>
2012-08-07 18:12:17 +0000

    Revert "Have different teamAnalyzer settings for each game type"
    blaghaid: Not really wanted feature even tough older HO had separate default
    configuration for cup and league.
    
    I agree after hearing the arguments so the commit 1914 is removed here.
    
    This reverts commit c13591fefc63bdce041ee77163a20ebad503025b.
    
    git-svn-id: file:///home/coren/project/backup_ho/trunk/Core@1916 b8de521a-fa13-0410-8113-fc7ffabbd59a

--- src/java/ho/module/teamAnalyzer/ui/TeamAnalyzerPanel.java
+++ src/java/ho/module/teamAnalyzer/ui/TeamAnalyzerPanel.java
@@ -23,7 +23,7 @@ import javax.swing.JSplitPane;
 public class TeamAnalyzerPanel extends JPanel implements IRefreshable{
 
     /** The filters */
-    public static Filter filter = null;
+    public static Filter filter = new Filter();
 
 	private static final long serialVersionUID = 1L;
 	private JButton simButton = new JButton(HOVerwaltung.instance().getLanguageString("Simulate"));
@@ -34,8 +34,6 @@ public class TeamAnalyzerPanel extends JPanel implements IRefreshable{
 	
 	public TeamAnalyzerPanel() {
 		SystemManager.initialize(this);
-		if (filter == null)
-			filter = new Filter();
 		initialize();
 		simButton.addActionListener(new SimButtonListener(mainPanel.getMyTeamLineupPanel(),
         		mainPanel.getOpponentTeamLineupPanel(), recapPanel));
