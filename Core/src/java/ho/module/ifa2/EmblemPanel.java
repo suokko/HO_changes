a9715cc00b408583c0b3a6a6feba5cabf069038f
kruescho <kruescho@b8de521a-fa13-0410-8113-fc7ffabbd59a>
2012-08-14 10:39:12 +0000

    refactoring EmblemPanel
    git-svn-id: https://ho1.svn.sourceforge.net/svnroot/ho1/branches/ifa-fix@1942 b8de521a-fa13-0410-8113-fc7ffabbd59a

--- Core/src/java/ho/module/ifa2/EmblemPanel.java
+++ Core/src/java/ho/module/ifa2/EmblemPanel.java
@@ -1,23 +1,29 @@
 package ho.module.ifa2;
 
+import ho.core.model.HOVerwaltung;
+import ho.core.util.StringUtils;
+import ho.module.ifa.ImageFileFilter;
+
 import java.awt.Color;
-import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
-import java.awt.Insets;
+import java.awt.Window;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 import javax.swing.ImageIcon;
 import javax.swing.JComponent;
+import javax.swing.JFileChooser;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
 
 public class EmblemPanel extends JPanel {
-	private EmblemPanel emblemPanel;
-	private GridBagConstraints constraints = new GridBagConstraints();
-	private PluginIfaPanel pluginIfaPanel;
+
+	private static final long serialVersionUID = 5771493941186321587L;
 	private FlagPanel flagPanel;
-	private JLabel logo;
+	private JLabel logoLabel;
 	private int flagWidth = 8;
 	private String headerText = "";
 	private boolean header = true;
@@ -27,57 +33,53 @@ public class EmblemPanel extends JPanel {
 	private int brightness = 50;
 	private String imagePath = "";
 
-	public EmblemPanel(PluginIfaPanel pluginIfaPanel, FlagLabel[] flagLabels,
-			int countriesPlayedIn) {
-		this.pluginIfaPanel = pluginIfaPanel;
+	public EmblemPanel(FlagLabel[] flagLabels, int countriesPlayedIn) {
 		this.flagPanel = new FlagPanel(flagLabels, countriesPlayedIn);
 		initialize();
-		this.emblemPanel = this;
 	}
 
 	private void initialize() {
 		setLayout(new GridBagLayout());
 		setBackground(Color.white);
-		if (this.logo == null) {
-			this.logo = new JLabel("Klick here to load your Emblem");
-			this.logo.setPreferredSize(new Dimension(100, 100));
-			this.logo.setVerticalAlignment(0);
-			this.logo.setHorizontalAlignment(0);
-			this.logo.setBackground(Color.white);
-			this.logo.addMouseListener(new GlobalActionsListener(
-					this.pluginIfaPanel));
+		if (this.logoLabel == null) {
+			this.logoLabel = new JLabel(HOVerwaltung.instance().getLanguageString(
+					"ifa.loadEmblem.clickHere"));
+			this.logoLabel.setPreferredSize(new Dimension(100, 100));
+			this.logoLabel.setVerticalAlignment(0);
+			this.logoLabel.setHorizontalAlignment(0);
+			this.logoLabel.setBackground(Color.white);
+			this.logoLabel.addMouseListener(new MouseAdapter() {
+				@Override
+				public void mouseReleased(MouseEvent arg0) {
+					loadEmblem();
+				}
+			});
 		}
 
-		this.constraints.fill = 1;
-		this.constraints.anchor = 10;
-		this.constraints.insets = new Insets(0, 0, 0, 0);
-		this.constraints.weightx = 100.0D;
-		this.constraints.weighty = 100.0D;
-		add(this.logo, this.constraints, 0, 0, 1, 1);
-		this.constraints.fill = 0;
-		this.constraints.anchor = 10;
-		this.constraints.weightx = 0.0D;
-		this.constraints.weighty = 0.0D;
-		add(this.flagPanel, this.constraints, 0, 1, 1, 1);
-	}
-
-	private void add(Component c, GridBagConstraints constraints, int x, int y,
-			int w, int h) {
-		constraints.gridx = x;
-		constraints.gridy = y;
-		constraints.gridwidth = w;
-		constraints.gridheight = h;
-		add(c, constraints);
+		GridBagConstraints constraints = new GridBagConstraints();
+		constraints.fill = GridBagConstraints.BOTH;
+		constraints.anchor = GridBagConstraints.CENTER;
+		constraints.weightx = 1.0;
+		constraints.weighty = 1.0;
+		constraints.gridx = 0;
+		constraints.gridy = 0;
+		add(this.logoLabel, constraints);
+		constraints.fill = GridBagConstraints.NONE;
+		constraints.weightx = 0.0;
+		constraints.weighty = 0.0;
+		constraints.gridy = 1;
+		add(this.flagPanel, constraints);
 	}
 
 	public void setLogo(ImageIcon image) {
-		this.logo.setIcon(image);
+		this.logoLabel.setIcon(image);
 		if (image != null) {
-			this.logo.setText("");
-			this.logo.setPreferredSize(new Dimension(image.getIconWidth(),
-					image.getIconHeight()));
+			this.logoLabel.setText("");
+			this.logoLabel.setPreferredSize(new Dimension(image.getIconWidth(), image
+					.getIconHeight()));
 		} else {
-			this.logo.setText("Klick here to load your Emblem");
+			this.logoLabel.setText(HOVerwaltung.instance().getLanguageString(
+					"ifa.loadEmblem.clickHere"));
 			this.imagePath = "";
 		}
 	}
@@ -92,17 +94,21 @@ public class EmblemPanel extends JPanel {
 		this.flagPanel.setHeaderVisible(this.header);
 		this.flagPanel.setFooterVisible(this.footer);
 		this.flagPanel.setHeader(this.headerText);
-		add(this.flagPanel, this.constraints, 0, 1, 1, 1);
+		GridBagConstraints gbc = new GridBagConstraints();
+		gbc.gridy = 0;
+		gbc.gridy = 1;
+		add(this.flagPanel, gbc);
 	}
 
 	public JLabel getLogo() {
-		return this.logo;
+		return this.logoLabel;
 	}
 
 	public JComponent getImage() {
-		if (this.logo.getIcon() == null)
+		if (this.logoLabel.getIcon() == null) {
 			return this.flagPanel;
-		return this.emblemPanel;
+		}
+		return this;
 	}
 
 	public void setFooter(boolean footer) {
@@ -141,8 +147,9 @@ public class EmblemPanel extends JPanel {
 	}
 
 	public String getImagePath() {
-		if (this.imagePath.equals(""))
+		if (StringUtils.isEmpty(this.imagePath)) {
 			return "\"\"";
+		}
 		return this.imagePath;
 	}
 
@@ -172,5 +179,26 @@ public class EmblemPanel extends JPanel {
 
 	public void setRoundly(boolean roundly) {
 		this.roundly = roundly;
+	}
+
+	private void loadEmblem() {
+		JFileChooser fileChooser = new JFileChooser();
+		fileChooser.setFileFilter(new ImageFileFilter(new String[] { "jpg", "gif", "png" }));
+		fileChooser.setAcceptAllFileFilterUsed(false);
+		fileChooser.setMultiSelectionEnabled(false);
+		Window parent = SwingUtilities.getWindowAncestor(this);
+		if (fileChooser.showOpenDialog(parent) == JFileChooser.APPROVE_OPTION) {
+			String path = fileChooser.getSelectedFile().getPath();
+			setImagePath(path);
+			ImageIcon image = null;
+			if (path != null) {
+				image = new ImageIcon(path);
+			}
+			setLogo(image);
+		} else {
+			setLogo(null);
+		}
+		validate();
+		repaint();
 	}
 }
