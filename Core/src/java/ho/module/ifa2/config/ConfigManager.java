ad05236047c7cde048d6155c3b1bf5ff34daeb35
kruescho <kruescho@b8de521a-fa13-0410-8113-fc7ffabbd59a>
2012-08-15 11:09:09 +0000

    read settings from MODULE_CONFIGURATION table
    git-svn-id: file:///home/coren/project/backup_ho/branches/ifa-fix@1946 b8de521a-fa13-0410-8113-fc7ffabbd59a

--- Core/src/java/ho/module/ifa2/config/ConfigManager.java
+++ Core/src/java/ho/module/ifa2/config/ConfigManager.java
@@ -13,106 +13,106 @@ import java.net.URL;
 
 public class ConfigManager {
 
-	public static String getDefaultFromConfig(String tagName) {
-		if (tagName.equalsIgnoreCase("visitedWidth"))
-			return "8";
-		if (tagName.equalsIgnoreCase("hostedWidth"))
-			return "8";
-		if (tagName.equalsIgnoreCase("visitedHeaderShow"))
-			return "true";
-		if (tagName.equalsIgnoreCase("visitedFooterShow"))
-			return "true";
-		if (tagName.equalsIgnoreCase("hostedHeaderShow"))
-			return "true";
-		if (tagName.equalsIgnoreCase("hostedFooterShow"))
-			return "true";
-		if (tagName.equalsIgnoreCase("visitedRoundly"))
-			return "false";
-		if (tagName.equalsIgnoreCase("visitedGrey"))
-			return "true";
-		if (tagName.equalsIgnoreCase("visitedBrightness"))
-			return "50";
-		if (tagName.equalsIgnoreCase("hostedRoundly"))
-			return "false";
-		if (tagName.equalsIgnoreCase("hostedGrey"))
-			return "true";
-		if (tagName.equalsIgnoreCase("hostedBrightness"))
-			return "50";
-		if (tagName.equalsIgnoreCase("visitedHeader"))
-			return "Visited";
-		if (tagName.equalsIgnoreCase("hostedHeader"))
-			return "Hosted";
-		if (tagName.equalsIgnoreCase("visitedPathEmblem"))
-			return "";
-		if (tagName.equalsIgnoreCase("hostedPathEmblem"))
-			return "";
-		if (tagName.equalsIgnoreCase("gifAnimated"))
-			return "false";
-		if (tagName.equalsIgnoreCase("gifDelay")) {
-			return "5.0";
-		}
-		return "";
-	}
-
-	public static String getTextFromConfig(String tagName) {
-		try {
-			URL url = ImageDesignPanel.class.getResource("config/config.txt");
-			FileInputStream in = new FileInputStream(new File(url.getPath()));
-			LineNumberReader lineReader = new LineNumberReader(
-					new InputStreamReader(in));
-			String retVal = "";
-
-			while (retVal != null) {
-				retVal = lineReader.readLine();
-				String[] tmp = retVal.split("=");
-				if (tmp[0].equalsIgnoreCase(tagName)) {
-					return tmp[1];
-				}
-
-			}
-
-			lineReader.close();
-			in.close();
-		} catch (Exception e) {
-			return getDefaultFromConfig(tagName);
-		}
-		return "";
-	}
-
-	public static void saveConfig(ImageDesignPanel imageDesignPanel)
-			throws Exception {
-		File file = new File(System.getProperty("user.dir") + File.separator
-				+ "hoplugins" + File.separator + "pluginIFA" + File.separator
-				+ "config" + File.separator + "config.txt");
-		if (!file.exists())
-			file.createNewFile();
-		FileOutputStream fos = new FileOutputStream(file);
-		PrintWriter pw = new PrintWriter(fos);
-
-		EmblemPanel visitedEmblemPanel = imageDesignPanel.getEmblemPanel(true);
-		EmblemPanel hostedEmblemPanel = imageDesignPanel.getEmblemPanel(false);
-		pw.println("visitedWidth=" + visitedEmblemPanel.getFlagWidth());
-		pw.println("hostedWidth=" + hostedEmblemPanel.getFlagWidth());
-		pw.println("visitedHeaderShow=" + visitedEmblemPanel.isHeader());
-		pw.println("visitedFooterShow=" + visitedEmblemPanel.isFooter());
-		pw.println("visitedBrightness=" + visitedEmblemPanel.getBrightness());
-		pw.println("visitedGrey=" + visitedEmblemPanel.isGrey());
-		pw.println("visitedRoundly=" + visitedEmblemPanel.isRoundly());
-		pw.println("hostedBrightness=" + hostedEmblemPanel.getBrightness());
-		pw.println("hostedGrey=" + hostedEmblemPanel.isGrey());
-		pw.println("hostedRoundly=" + hostedEmblemPanel.isRoundly());
-		pw.println("hostedHeaderShow=" + hostedEmblemPanel.isHeader());
-		pw.println("hostedFooterShow=" + hostedEmblemPanel.isFooter());
-		pw.println("visitedHeader=" + visitedEmblemPanel.getHeaderText());
-		pw.println("hostedHeader=" + hostedEmblemPanel.getHeaderText());
-		pw.println("visitedPathEmblem=" + visitedEmblemPanel.getImagePath());
-		pw.println("hostedPathEmblem=" + hostedEmblemPanel.getImagePath());
-		pw.println("gifAnimated=" + imageDesignPanel.isAnimGif());
-		pw.println("gifDelay="
-				+ imageDesignPanel.getDelaySpinner().getValue().toString());
-
-		pw.close();
-		fos.flush();
-		fos.close();
-	}
+//	public static String getDefaultFromConfig(String tagName) {
+//		if (tagName.equalsIgnoreCase("visitedWidth"))
+//			return "8";
+//		if (tagName.equalsIgnoreCase("hostedWidth"))
+//			return "8";
+//		if (tagName.equalsIgnoreCase("visitedHeaderShow"))
+//			return "true";
+//		if (tagName.equalsIgnoreCase("visitedFooterShow"))
+//			return "true";
+//		if (tagName.equalsIgnoreCase("hostedHeaderShow"))
+//			return "true";
+//		if (tagName.equalsIgnoreCase("hostedFooterShow"))
+//			return "true";
+//		if (tagName.equalsIgnoreCase("visitedRoundly"))
+//			return "false";
+//		if (tagName.equalsIgnoreCase("visitedGrey"))
+//			return "true";
+//		if (tagName.equalsIgnoreCase("visitedBrightness"))
+//			return "50";
+//		if (tagName.equalsIgnoreCase("hostedRoundly"))
+//			return "false";
+//		if (tagName.equalsIgnoreCase("hostedGrey"))
+//			return "true";
+//		if (tagName.equalsIgnoreCase("hostedBrightness"))
+//			return "50";
+//		if (tagName.equalsIgnoreCase("visitedHeader"))
+//			return "Visited";
+//		if (tagName.equalsIgnoreCase("hostedHeader"))
+//			return "Hosted";
+//		if (tagName.equalsIgnoreCase("visitedPathEmblem"))
+//			return "";
+//		if (tagName.equalsIgnoreCase("hostedPathEmblem"))
+//			return "";
+//		if (tagName.equalsIgnoreCase("gifAnimated"))
+//			return "false";
+//		if (tagName.equalsIgnoreCase("gifDelay")) {
+//			return "5.0";
+//		}
+//		return "";
+//	}
+//
+//	public static String getTextFromConfig(String tagName) {
+//		try {
+//			URL url = ImageDesignPanel.class.getResource("config/config.txt");
+//			FileInputStream in = new FileInputStream(new File(url.getPath()));
+//			LineNumberReader lineReader = new LineNumberReader(
+//					new InputStreamReader(in));
+//			String retVal = "";
+//
+//			while (retVal != null) {
+//				retVal = lineReader.readLine();
+//				String[] tmp = retVal.split("=");
+//				if (tmp[0].equalsIgnoreCase(tagName)) {
+//					return tmp[1];
+//				}
+//
+//			}
+//
+//			lineReader.close();
+//			in.close();
+//		} catch (Exception e) {
+//			return getDefaultFromConfig(tagName);
+//		}
+//		return "";
+//	}
+//
+//	public static void saveConfig(ImageDesignPanel imageDesignPanel)
+//			throws Exception {
+//		File file = new File(System.getProperty("user.dir") + File.separator
+//				+ "hoplugins" + File.separator + "pluginIFA" + File.separator
+//				+ "config" + File.separator + "config.txt");
+//		if (!file.exists())
+//			file.createNewFile();
+//		FileOutputStream fos = new FileOutputStream(file);
+//		PrintWriter pw = new PrintWriter(fos);
+//
+//		EmblemPanel visitedEmblemPanel = imageDesignPanel.getEmblemPanel(true);
+//		EmblemPanel hostedEmblemPanel = imageDesignPanel.getEmblemPanel(false);
+//		pw.println("visitedWidth=" + visitedEmblemPanel.getFlagWidth());
+//		pw.println("hostedWidth=" + hostedEmblemPanel.getFlagWidth());
+//		pw.println("visitedHeaderShow=" + visitedEmblemPanel.isHeader());
+//		pw.println("visitedFooterShow=" + visitedEmblemPanel.isFooter());
+//		pw.println("visitedBrightness=" + visitedEmblemPanel.getBrightness());
+//		pw.println("visitedGrey=" + visitedEmblemPanel.isGrey());
+//		pw.println("visitedRoundly=" + visitedEmblemPanel.isRoundly());
+//		pw.println("hostedBrightness=" + hostedEmblemPanel.getBrightness());
+//		pw.println("hostedGrey=" + hostedEmblemPanel.isGrey());
+//		pw.println("hostedRoundly=" + hostedEmblemPanel.isRoundly());
+//		pw.println("hostedHeaderShow=" + hostedEmblemPanel.isHeader());
+//		pw.println("hostedFooterShow=" + hostedEmblemPanel.isFooter());
+//		pw.println("visitedHeader=" + visitedEmblemPanel.getHeaderText());
+//		pw.println("hostedHeader=" + hostedEmblemPanel.getHeaderText());
+//		pw.println("visitedPathEmblem=" + visitedEmblemPanel.getImagePath());
+//		pw.println("hostedPathEmblem=" + hostedEmblemPanel.getImagePath());
+//		pw.println("gifAnimated=" + imageDesignPanel.isAnimGif());
+//		pw.println("gifDelay="
+//				+ imageDesignPanel.getDelaySpinner().getValue().toString());
+//
+//		pw.close();
+//		fos.flush();
+//		fos.close();
+//	}
 }
