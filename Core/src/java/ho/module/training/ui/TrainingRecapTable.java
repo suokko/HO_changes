d57de65b9bd3194aaded995a0e68e981762c2b7d
suokkos <suokkos@b8de521a-fa13-0410-8113-fc7ffabbd59a>
2012-08-20 16:56:09 +0000

    Visualise and sort training speed in training prediction
    To make it easier to follow training speed in prediction will be sorted
    based on training speed. Also background color is selected differently
    for players that get full training, half training or osmosis training.
    
    git-svn-id: file:///home/coren/project/backup_ho/trunk@1983 b8de521a-fa13-0410-8113-fc7ffabbd59a

--- Core/src/java/ho/module/training/ui/TrainingRecapTable.java
+++ Core/src/java/ho/module/training/ui/TrainingRecapTable.java
@@ -1,10 +1,17 @@
 // %1126721452104:hoplugins.trainingExperience.ui%
 package ho.module.training.ui;
 
+import ho.core.gui.theme.HOColorName;
+import ho.core.gui.theme.ThemeManager;
+
+import java.awt.Color;
+import java.awt.Component;
+
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.ListSelectionModel;
 import javax.swing.ScrollPaneConstants;
+import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.TableColumnModel;
 
 
@@ -23,6 +30,39 @@ public class TrainingRecapTable extends JScrollPane {
 	private JTable fixed;
     private JTable scroll;
 
+    /**
+     * Fixed table rendere to add special background colors depending on training speed
+     */
+    private class FixedTrainingRecapRenderer extends DefaultTableCellRenderer {
+        private static final long serialVersionUID = 7666144967819145974L;
+
+        /* (non-Javadoc)
+         * @see javax.swing.table.TableCellRenderer#getTableCellRendererComponent(javax.swing.JTable, java.lang.Object, boolean, boolean, int, int)
+         */
+        @Override
+        public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected,
+                                                       boolean hasFocus, int row, int column) {
+            super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+
+            if (isSelected) {
+                return this;
+            }
+
+            int speed = Integer.parseInt((String) table.getValueAt(row, 2));
+            Color bg_color; 
+            // Speed range is 16 to 125
+            if (speed > (125+50)/2) {
+                bg_color = ThemeManager.getColor(HOColorName.PLAYER_SKILL_SPECIAL_BG);
+            } else if (speed > (50+16)/2) {
+                bg_color = ThemeManager.getColor(HOColorName.PLAYER_SKILL_BG);
+            } else {
+                bg_color = ThemeManager.getColor(HOColorName.TABLEENTRY_BG);
+            }
+
+            setBackground(bg_color);
+            return this;
+        }
+    }
     //~ Constructors -------------------------------------------------------------------------------
 
     /**
@@ -62,10 +102,16 @@ public class TrainingRecapTable extends JScrollPane {
         fixed.getColumnModel().getColumn(0).setMaxWidth(120);
         fixed.getColumnModel().getColumn(0).setMinWidth(120);
         fixed.getColumnModel().getColumn(0).setWidth(120);
+        fixed.getColumnModel().getColumn(2).setMaxWidth(0);
+        fixed.getColumnModel().getColumn(2).setMinWidth(0);
+        fixed.getColumnModel().getColumn(2).setPreferredWidth(0);
 
         //  Add the fixed table to the scroll pane
         fixed.setPreferredScrollableViewportSize(fixed.getPreferredSize());
         setRowHeaderView(fixed);
+
+        fixed.setDefaultRenderer(Object.class, new FixedTrainingRecapRenderer());
+
         setCorner(ScrollPaneConstants.UPPER_LEFT_CORNER, fixed.getTableHeader());
     }
 
