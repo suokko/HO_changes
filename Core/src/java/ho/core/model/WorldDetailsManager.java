9691a3fe00cc47c216994b0e6e19bfba0ddb1099
kruescho <kruescho@b8de521a-fa13-0410-8113-fc7ffabbd59a>
2012-10-04 09:12:37 +0000

    new method: WorldDetailLeague getWorldDetailLeagueByCountryId(Integer countryId)
    git-svn-id: https://ho1.svn.sourceforge.net/svnroot/ho1/trunk@2174 b8de521a-fa13-0410-8113-fc7ffabbd59a

--- Core/src/java/ho/core/model/WorldDetailsManager.java
+++ Core/src/java/ho/core/model/WorldDetailsManager.java
@@ -8,54 +8,58 @@ public class WorldDetailsManager {
 
 	private static WorldDetailsManager WMANAGER = null;
 	private WorldDetailLeague[] leagues;
-	private HashMap<Integer,WorldDetailLeague> countryMap = new HashMap<Integer,WorldDetailLeague>();
-	private HashMap<Integer,WorldDetailLeague> leagueMap = new HashMap<Integer,WorldDetailLeague>();
+	private HashMap<Integer, WorldDetailLeague> countryMap = new HashMap<Integer, WorldDetailLeague>();
+	private HashMap<Integer, WorldDetailLeague> leagueMap = new HashMap<Integer, WorldDetailLeague>();
 	private int totalUsers;
-	
-	public static WorldDetailsManager instance(){
-		if(WMANAGER == null){
+
+	public static WorldDetailsManager instance() {
+		if (WMANAGER == null) {
 			WMANAGER = new WorldDetailsManager();
 		}
 		return WMANAGER;
 	}
-	
-	private WorldDetailsManager(){
+
+	private WorldDetailsManager() {
 		initialize();
 	}
-	
+
 	private void initialize() {
 		leagues = DBManager.instance().getAllWorldDetailLeagues();
 		leagueMap.clear();
 		countryMap.clear();
 		for (int i = 0; i < leagues.length; i++) {
 			totalUsers += leagues[i].getActiveUsers();
-			countryMap.put(Integer.valueOf(leagues[i].getCountryId()),leagues[i]);
-			leagueMap.put(Integer.valueOf(leagues[i].getLeagueId()),leagues[i]);
+			countryMap.put(Integer.valueOf(leagues[i].getCountryId()), leagues[i]);
+			leagueMap.put(Integer.valueOf(leagues[i].getLeagueId()), leagues[i]);
 		}
 	}
-	
-	public void refresh(){
+
+	public void refresh() {
 		initialize();
 	}
-	
-	public int size(){
+
+	public int size() {
 		return leagues.length;
 	}
-	
-	public String getNameByCountryId(int countryId){
+
+	public String getNameByCountryId(int countryId) {
 		return countryMap.get(Integer.valueOf(countryId)).getCountryName();
 	}
 
-	public WorldDetailLeague getWorldDetailLeagueByLeagueId(Integer leagueId){
+	public WorldDetailLeague getWorldDetailLeagueByLeagueId(Integer leagueId) {
 		return leagueMap.get(leagueId);
+	}
+
+	public WorldDetailLeague getWorldDetailLeagueByCountryId(Integer countryId) {
+		return countryMap.get(countryId);
 	}
 
 	public final WorldDetailLeague[] getLeagues() {
 		return leagues;
 	}
-	
-	public final int getTotalUsers(){
+
+	public final int getTotalUsers() {
 		return totalUsers;
 	}
-	
+
 }
