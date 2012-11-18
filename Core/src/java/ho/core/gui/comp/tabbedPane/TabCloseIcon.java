b2c3004f2ae5533c7d45c33715e2fed44a6ee59f
kruescho <kruescho@b8de521a-fa13-0410-8113-fc7ffabbd59a>
2012-11-18 18:11:51 +0000

    Ticket #170: confirmation message when closing tabs
    git-svn-id: https://ho1.svn.sourceforge.net/svnroot/ho1/trunk@2303 b8de521a-fa13-0410-8113-fc7ffabbd59a

--- Core/src/java/ho/core/gui/comp/tabbedPane/TabCloseIcon.java
+++ Core/src/java/ho/core/gui/comp/tabbedPane/TabCloseIcon.java
@@ -1,8 +1,8 @@
 package ho.core.gui.comp.tabbedPane;
 
-
 import ho.core.gui.theme.HOIconName;
 import ho.core.gui.theme.ThemeManager;
+import ho.core.model.HOVerwaltung;
 
 import java.awt.Component;
 import java.awt.Graphics;
@@ -11,59 +11,64 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
 import javax.swing.Icon;
+import javax.swing.JOptionPane;
 import javax.swing.JTabbedPane;
-
 
 final class TabCloseIcon implements Icon {
 	private final Icon mIcon = ThemeManager.getIcon(HOIconName.TABBEDPANE_CLOSE);
 	private transient Rectangle mPosition = null;
 	private int xOffset = 0;
-	
-	TabCloseIcon(final JTabbedPane mTabbedPane, int xOffset){
+	private final JTabbedPane mTabbedPane;
+
+	TabCloseIcon(final JTabbedPane mTabbedPane, int xOffset) {
 		this.xOffset = xOffset;
-		mTabbedPane.addMouseListener( new MouseAdapter()
-		{
-			@Override 
-			public void mouseReleased( MouseEvent e )
-			{
-				// asking for isConsumed is *very* important, otherwise more than one tab might get closed!
-				if ( !e.isConsumed()  &&   mPosition.contains( e.getX(), e.getY() ) )
-				{
-					final int index = mTabbedPane.getSelectedIndex();
-					mTabbedPane.remove( index );
+		this.mTabbedPane = mTabbedPane;
+		this.mTabbedPane.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseReleased(MouseEvent e) {
+				// asking for isConsumed is *very* important, otherwise more
+				// than one tab might get closed!
+				if (!e.isConsumed() && mPosition.contains(e.getX(), e.getY())) {
+					closeTab();
 					e.consume();
 				}
 			}
 		});
 	}
-	
-	
+
 	/**
-	 * when painting, remember last position painted.
-	 * add extra xOffset pixels to allow for Nimbus skin
+	 * when painting, remember last position painted. add extra xOffset pixels
+	 * to allow for Nimbus skin
 	 */
 	@Override
-	public void paintIcon(Component c, Graphics g, int x, int y){
-		mPosition = new Rectangle( x + xOffset, y, getIconWidth(), getIconHeight() );
-		mIcon.paintIcon(c, g, x + xOffset, y );
+	public void paintIcon(Component c, Graphics g, int x, int y) {
+		mPosition = new Rectangle(x + xOffset, y, getIconWidth(), getIconHeight());
+		mIcon.paintIcon(c, g, x + xOffset, y);
 	}
-	
-	
+
 	/**
 	 * just delegate
 	 */
 	@Override
-	public int getIconWidth(){
+	public int getIconWidth() {
 		return mIcon.getIconWidth();
 	}
-	
+
 	/**
 	 * just delegate
 	 */
 	@Override
-	public int getIconHeight(){
+	public int getIconHeight() {
 		return mIcon.getIconHeight();
 	}
-	
-}
 
+	private void closeTab() {
+		String title = HOVerwaltung.instance().getLanguageString("tab.close.confirm.title");
+		String message = HOVerwaltung.instance().getLanguageString("tab.close.confirm.msg");
+		if (JOptionPane.showConfirmDialog(mTabbedPane, message, title, JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION) {
+			int index = mTabbedPane.getSelectedIndex();
+			mTabbedPane.remove(index);
+		}
+	}
+
+}
