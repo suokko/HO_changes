13182a03d61e69ea3d839c4a07ca23e6b415fab1
kruescho <kruescho@b8de521a-fa13-0410-8113-fc7ffabbd59a>
2013-01-05 15:15:20 +0000

    unused code removed
    git-svn-id: https://ho1.svn.sourceforge.net/svnroot/ho1/trunk@2384 b8de521a-fa13-0410-8113-fc7ffabbd59a

--- Core/src/java/ho/module/teamAnalyzer/manager/PlayerDataManager.java
+++ Core/src/java/ho/module/teamAnalyzer/manager/PlayerDataManager.java
@@ -6,54 +6,51 @@ import java.util.Iterator;
 import java.util.List;
 
 public class PlayerDataManager {
-    //~ Static fields/initializers -----------------------------------------------------------------
-    public static final int AVAILABLE = 0;
-    public static final int INJURED = 1;
-    public static final int SUSPENDED = 2;
-    public static final int SOLD = 3;
 
+	public static final int AVAILABLE = 0;
+	public static final int INJURED = 1;
+	public static final int SUSPENDED = 2;
+	public static final int SOLD = 3;
 
-    //~ Methods ------------------------------------------------------------------------------------
-    public static PlayerInfo getLatestPlayerInfo(int playerId) {
-        PlayerInfo info = DBManager.instance().getTAPlayerInfo(playerId);
+	public static PlayerInfo getLatestPlayerInfo(int playerId) {
+		PlayerInfo info = DBManager.instance().getTAPlayerInfo(playerId);
 
-        if (info.getPlayerId() == 0) {
-            info = DBManager.instance().getTAPreviousPlayerInfo(playerId);
-        }
+		if (info.getPlayerId() == 0) {
+			info = DBManager.instance().getTAPreviousPlayerInfo(playerId);
+		}
 
-        return info;
-    }
+		return info;
+	}
 
-    public static PlayerInfo getPlayerInfo(int id, int week, int season) {
-        return DBManager.instance().getTAPlayerInfo(id, week, season);
-    }
+	public static PlayerInfo getPlayerInfo(int id, int week, int season) {
+		return DBManager.instance().getTAPlayerInfo(id, week, season);
+	}
 
-    public static PlayerInfo getPlayerInfo(int id) {
-        return DBManager.instance().getTAPlayerInfo(id);
-    }
+	public static PlayerInfo getPlayerInfo(int id) {
+		return DBManager.instance().getTAPlayerInfo(id);
+	}
 
-    public static PlayerInfo getPreviousPlayerInfo(int playerId) {
-        return DBManager.instance().getTAPreviousPlayerInfo(playerId);
-    }
+	public static void update(List<PlayerInfo> players) {
+		for (Iterator<PlayerInfo> iter = players.iterator(); iter.hasNext();) {
+			PlayerInfo parsedPlayer = iter.next();
+			setPlayer(parsedPlayer);
+		}
+	}
 
-    public static void update(List<PlayerInfo> players) {
-        for (Iterator<PlayerInfo> iter = players.iterator(); iter.hasNext();) {
-            PlayerInfo parsedPlayer = iter.next();
-            setPlayer(parsedPlayer);
-        }
-    }
+	private static void setPlayer(PlayerInfo info) {
+		if (info.getPlayerId() == 0) {
+			return;
+		}
 
-    private static void setPlayer(PlayerInfo info) {
-        if (info.getPlayerId() == 0) {
-            return;
-        }
+		PlayerInfo actual = DBManager.instance().getTAPlayerInfo(info.getPlayerId());
+		if (info.getPlayerId() == 217381328) {
+			System.out.println();
+		}
 
-        PlayerInfo actual = DBManager.instance().getTAPlayerInfo(info.getPlayerId());
-
-        if (actual.getPlayerId() == 0) {
-        	DBManager.instance().addTAPlayerInfo(info);
-        } else {
-        	DBManager.instance().updateTAPlayerInfo(info);
-        }
-    }
+		if (actual.getPlayerId() == 0) {
+			DBManager.instance().addTAPlayerInfo(info);
+		} else {
+			DBManager.instance().updateTAPlayerInfo(info);
+		}
+	}
 }
