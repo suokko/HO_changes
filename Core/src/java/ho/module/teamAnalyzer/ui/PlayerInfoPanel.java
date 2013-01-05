7eda8a8f8ce670298ccdc71b002adc7c32fb49f7
kruescho <kruescho@b8de521a-fa13-0410-8113-fc7ffabbd59a>
2013-01-05 15:12:23 +0000

    unused code removed
    git-svn-id: https://ho1.svn.sourceforge.net/svnroot/ho1/trunk@2383 b8de521a-fa13-0410-8113-fc7ffabbd59a

--- Core/src/java/ho/module/teamAnalyzer/ui/PlayerInfoPanel.java
+++ Core/src/java/ho/module/teamAnalyzer/ui/PlayerInfoPanel.java
@@ -20,127 +20,87 @@ import javax.swing.SwingConstants;
 import javax.swing.border.EtchedBorder;
 
 public class PlayerInfoPanel extends JPanel {
-    //~ Instance fields ----------------------------------------------------------------------------
+
 	private static final long serialVersionUID = 5338615169669058973L;
-    protected JLabel ageLabel = new JLabel("");
-    protected JLabel expLabel = new JLabel("");
-    protected JLabel formLabel = new JLabel("");
-    protected JLabel tsiLabel = new JLabel("");
+	protected JLabel ageLabel = new JLabel("");
+	protected JLabel expLabel = new JLabel("");
+	protected JLabel formLabel = new JLabel("");
+	protected JLabel tsiLabel = new JLabel("");
 
-    //~ Constructors -------------------------------------------------------------------------------
+	public PlayerInfoPanel() {
+		this.setLayout(new GridBagLayout());
 
-    public PlayerInfoPanel() {
-        super();
+		addInfo(HOVerwaltung.instance().getLanguageString("ls.player.age"), ageLabel, 0, 0);
+		addInfo(HOVerwaltung.instance().getLanguageString("ls.player.tsi"), tsiLabel, 0, 1);
+		addInfo(HOVerwaltung.instance().getLanguageString("ls.player.form"), formLabel, 1, 0);
+		addInfo(HOVerwaltung.instance().getLanguageString("ls.player.short_experience"), expLabel,
+				1, 1);
+	}
 
-        this.setLayout(new GridBagLayout());
+	public void setValue(PlayerInfo oldPlayer) {
+		clearData();
+		expLabel.setText(PlayerAbility.getNameForSkill(oldPlayer.getExperience(), false));
+		tsiLabel.setText("" + oldPlayer.getTSI());
+		ageLabel.setText("" + oldPlayer.getAge());
+		formLabel.setText(PlayerAbility.getNameForSkill(oldPlayer.getForm(), false));
 
-        addInfo(HOVerwaltung.instance().getLanguageString("ls.player.age"), ageLabel, 0, 0);
-        addInfo(HOVerwaltung.instance().getLanguageString("ls.player.tsi"), tsiLabel, 0, 1);
-        addInfo(HOVerwaltung.instance().getLanguageString("ls.player.form"), formLabel, 1, 0);
-        addInfo(HOVerwaltung.instance().getLanguageString("ls.player.short_experience"), expLabel, 1, 1);
-    }
+		PlayerInfo actual = PlayerDataManager.getPlayerInfo(oldPlayer.getPlayerId());
 
-    //~ Methods ------------------------------------------------------------------------------------
+		if (actual.getForm() == 0) {
+			return;
+		}
+	}
 
-    public void setValue(PlayerInfo oldPlayer) {
-        clearData();
-        expLabel.setText(PlayerAbility.getNameForSkill(oldPlayer.getExperience(),
-                                                                        false));
-        tsiLabel.setText("" + oldPlayer.getTSI());
-        ageLabel.setText("" + oldPlayer.getAge());
-        formLabel.setText(PlayerAbility.getNameForSkill(oldPlayer.getForm(), false));
+	public void clearData() {
+		ageLabel.setText("");
+		formLabel.setText("");
+		expLabel.setText("");
+		tsiLabel.setText("");
+		expLabel.setIcon(null);
+		formLabel.setIcon(null);
+		tsiLabel.setToolTipText("");
+	}
 
-        PlayerInfo actual = PlayerDataManager.getPlayerInfo(oldPlayer.getPlayerId());
+	private GridBagConstraints getConstraint(int gridx, int gridy, int gridwidth, int gridheight) {
+		GridBagConstraints c = new GridBagConstraints();
 
-        if (actual.getForm() == 0) {
-            return;
-        }
+		c.gridx = gridx;
+		c.gridy = gridy;
+		c.gridwidth = gridwidth;
+		c.gridheight = gridheight;
+		c.weightx = 0.5D;
+		c.weighty = 0.5D;
+		c.anchor = GridBagConstraints.CENTER;
+		c.fill = GridBagConstraints.BOTH;
+		c.insets = new Insets(0, 0, 0, 0);
+		c.ipadx = 0;
+		c.ipady = 0;
 
-//        if (HelperWrapper.instance().isDevVersion()) {
-//            expLabel.setIcon( ImageUtilities.getImageIcon4Veraenderung(actual.getExperience()
-//                                                                         - oldPlayer.getExperience(),true));
-//            formLabel.setIcon( ImageUtilities.getImageIcon4Veraenderung(actual.getForm()
-//                                                                         - oldPlayer.getForm(),true));
-//
-//            int diff = actual.getTSI() - oldPlayer.getTSI();
-//            String desc = "";
-//
-//            if (diff > 0) {
-//                desc = "+";
-//            }
-//
-//            desc = desc + diff;
-//            tsiLabel.setToolTipText(desc);
-//        }
-    }
+		return c;
+	}
 
-    /**
-     *
-     */
-    public void clearData() {
-        ageLabel.setText("");
-        formLabel.setText("");
-        expLabel.setText("");
-        tsiLabel.setText("");
-        expLabel.setIcon(null);
-        formLabel.setIcon(null);
-        tsiLabel.setToolTipText("");
-    }
+	private void addInfo(String title, JLabel label, int x, int y) {
+		GridBagConstraints c = getConstraint(x, y, 1, 1);
 
-    /**
-     * @param gridx
-     * @param gridy
-     * @param gridwidth
-     * @param gridheight
-     *
-     * @return constraint
-     */
-    private GridBagConstraints getConstraint(int gridx, int gridy, int gridwidth, int gridheight) {
-        GridBagConstraints c = new GridBagConstraints();
+		JLabel titleLabel = new JLabel(title);
 
-        c.gridx = gridx;
-        c.gridy = gridy;
-        c.gridwidth = gridwidth;
-        c.gridheight = gridheight;
-        c.weightx = 0.5D;
-        c.weighty = 0.5D;
-        c.anchor = GridBagConstraints.CENTER;
-        c.fill = GridBagConstraints.BOTH;
-        c.insets = new Insets(0, 0, 0, 0);
-        c.ipadx = 0;
-        c.ipady = 0;
+		titleLabel.setOpaque(false);
+		titleLabel.setHorizontalAlignment(SwingConstants.LEFT);
+		titleLabel.setHorizontalTextPosition(SwingConstants.LEFT);
 
-        return c;
-    }
+		label.setOpaque(false);
+		label.setHorizontalAlignment(SwingConstants.RIGHT);
+		label.setHorizontalTextPosition(SwingConstants.RIGHT);
 
-    /**
-     * @param title
-     * @param label
-     * @param x
-     * @param y
-     */
-    private void addInfo(String title, JLabel label, int x, int y) {
-        GridBagConstraints c = getConstraint(x, y, 1, 1);
+		JPanel panel = new ImagePanel();
 
-        JLabel titleLabel = new JLabel(title);
+		panel.setOpaque(false);
+		panel.setLayout(new BorderLayout());
+		panel.setBorder(javax.swing.BorderFactory.createEtchedBorder(EtchedBorder.LOWERED));
 
-        titleLabel.setOpaque(false);
-        titleLabel.setHorizontalAlignment(SwingConstants.LEFT);
-        titleLabel.setHorizontalTextPosition(SwingConstants.LEFT);
+		panel.add(titleLabel, BorderLayout.WEST);
+		panel.add(label, BorderLayout.CENTER);
 
-        label.setOpaque(false);
-        label.setHorizontalAlignment(SwingConstants.RIGHT);
-        label.setHorizontalTextPosition(SwingConstants.RIGHT);
-
-        JPanel panel =new ImagePanel();
-
-        panel.setOpaque(false);
-        panel.setLayout(new BorderLayout());
-        panel.setBorder(javax.swing.BorderFactory.createEtchedBorder(EtchedBorder.LOWERED));
-
-        panel.add(titleLabel, BorderLayout.WEST);
-        panel.add(label, BorderLayout.CENTER);
-
-        this.add(panel, c);
-    }
+		this.add(panel, c);
+	}
 }
